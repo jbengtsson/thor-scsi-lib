0761fb3d3e67273b400c6dcc92c2b19d2e0425da
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2021-12-20 12:06:14 +0100

    A compatability layer a bit closer to former TRACY
    Not recommended for new projects!

---
+++ thor/inc/thor_scsi/compat/compat.h
@@ -0,0 +1,26 @@
+#ifndef _THOR_SCSI_COMPAT_H_
+#define _THOR_SCSI_COMPAT_H_ 1
+/**
+
+   Provide a compatability layer to the former TRACY API
+   Compatability is only provided as so far as it does not touch dangerous code
+
+   Warning:
+        If writing new code, please use the thor_scsi API. This compatability
+	will only be supported in a limited fashion. Please consider to uprade
+	your code!
+
+
+ */
+#include <thor_scsi/compat/globals.h>
+#include <thor_scsi/compat/std_headers.h>
+#include <thor_scsi/thor_scsi.h>
+#include <thor_scsi/compat/namespaces.h>
+
+#endif /*  _THOR_SCSI_COMPAT_H_ */
+/*
+ * Local Variables:
+ * mode: c++
+ * c-file-style: "python"
+ * End:
+ */
