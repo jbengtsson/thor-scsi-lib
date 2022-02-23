f29819b44dae2e1cd14cd09a5834cde6881fcf2d
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-02-23 12:13:31 +0100

    Reorgansing header files: follow up commit
    cmake now traversiing new build tree
    cmake directory:
        collection of cmake utils
        gpp warning collections in include file
    
    core: added test for transform

---
+++ cmake/gpp_warnings.cmake
@@ -0,0 +1,46 @@
+
+if(CMAKE_COMPILER_IS_GNUCXX)
+  add_definitions(
+  	-Wcast-align
+	-Wcast-qual
+	-Wctor-dtor-privacy
+	-Wdisabled-optimization
+	-Wformat=2
+	-Winit-self
+	-Wlogical-op
+	-Wnoexcept
+	# -Wold-style-cast
+	-Woverloaded-virtual
+	-Wredundant-decls
+	# -Wshadow
+	-Wsign-promo
+	-Wstrict-null-sentinel
+	-Wstrict-overflow=5
+	-Wswitch-default
+	-Wundef
+	-Wno-unused
+   	-Wmissing-declarations
+   	#-Weffc++
+	#-Wpadding
+)
+  # has to be removed at a later stage
+  # guess a move ctor and use in code will provide
+  # some speed up
+  # add_definitions(-Werror)
+
+  add_definitions(
+  -Wno-deprecated-copy
+    -Wno-error=deprecated-copy
+  )
+  add_definitions(
+  -Wno-Werror=all
+   -Wmissing-declarations
+  -Wno-error=unused-result
+  -Wno-error=sign-compare
+  -Wno-error=vla -Wno-error=old-style-cast
+  -Wno-error=shadow
+  -Wno-error=strict-overflow
+  -Wno-error=sign-promo
+  -Wno-error=format-nonliteral
+  -Wno-error=switch-default)
+endif()
