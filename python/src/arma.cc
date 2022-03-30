68aad5758ef277e5b882569fa5b0cbe3beeb679c
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-03-30 20:07:56 +0200

    src/arma: trying to get clang satisfied

--- python/src/arma.cc
+++ python/src/arma.cc
@@ -17,17 +17,19 @@ void py_thor_scsi_init_arma(py::module &m)
 				 })
 		//.def("__str__", &PyArma::pstr)
 		.def_buffer([](arma::mat &mat) -> py::buffer_info {
+				    size_t n_cols = static_cast<size_t>(mat.n_cols);
 				    py::buffer_info r;
 				    r.ptr = mat.memptr();         /* Pointer to buffer */
 				    r.itemsize = sizeof(double); /* Size of one scalar */
 				    r.format = py::format_descriptor<double>::format(); /* Python struct-style format descriptor */
 				    r.ndim = 2;
-				    r.shape = { static_cast<long int>(mat.n_rows),
-						static_cast<long int>(mat.n_cols)
+				    r.shape = { static_cast<py::ssize_t>(mat.n_rows),
+						static_cast<py::ssize_t>(mat.n_cols)
 				    };/* Number of dimensions */
-				    r.strides = {            /* Strides (in bytes) for each index */
-					    sizeof(double),
-					    sizeof(double) * static_cast<size_t>(mat.n_cols)};
+				    r.strides = {
+					    /* Strides (in bytes) for each index */
+					    static_cast<py::ssize_t>(sizeof(double)),
+					    static_cast<py::ssize_t>(sizeof(double) * n_cols)};
 				    return r;
 			    });
 }
