a0de8d2d43630b3dd88fe5b521a0d206005ff5d5
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-05-12 12:14:15 +0200

    Cleanup: removing unused files (see pedon tree if still required)
    Removing directories:
    
        * gnuplot
        * projects
        * thor

--- thor/inc_on_hold/thor_scsi/obsolete/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef _THOR_SCSI_CONFIG_H_
-#define _THOR_SCSI_CONFIG_H_
-/**
- * Todo:
- *     Check if file should be merged with core/config.h
- */
-#endif
--- thor/inc_on_hold/thor_scsi/thor_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef _THOR_SCSI_CONFIG_H_
-#define _THOR_SCSI_CONFIG_H_ 1
-// Define if linear or arbitrary order TPSA.
-#include <tps/config.>
-#ifndef NO_TPSA
-#error "NO_TPSA must be defined "
-#endif
-#endif /* _THOR_SCSI_CONFIG_H_ */
