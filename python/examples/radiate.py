3f4abfc39020acb5d7cf114fa4fb6f3e5c47a8ff
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-09-22 18:06:25 +0200

    [Task] Develop radiation
    radiation delegate:
        - added comments
    
    python:
        - radiate.py:       todo comments
        - accelerator.py:   start implementing radiation objs
        - closed_orbit.py:  pythonized closed orbit calculations
        - linear_optics:    todo comment for function renaming: fix point instead of origin

--- python/examples/radiate.py
+++ python/examples/radiate.py
@@ -81,6 +81,11 @@ else:
 #    them before calculation starts (sum it up afterwards
 
 
+# real part damping time
+# compute eigenvalues and thus A too (similar as for finding twiss
+# r = closed_orbit
+# use r.one_turn_map
+
 print(ps)
 acc.propagate(calc_config, ps, 0, 2000)
 print(ps)
