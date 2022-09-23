678942d9db7412db9058bcdc2da09aed02a3f226
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-09-23 20:05:12 +0200

    cavity check: energy defaults to NaN in config
    cavity / test_cavity : check that energy is defined if cavity action is used

--- src/thor_scsi/elements/test_cavity.cc
+++ src/thor_scsi/elements/test_cavity.cc
@@ -16,8 +16,18 @@ static double compute_delta(const double volt,  const double frequency, const do
 {
 	auto c0 = tse::speed_of_light;
 
+	/*
+	std::cout << "Frequency " << frequency/1e6 << " MHz"
+		  << " voltage  " << volt/1e6 << " MV "
+		  << " phase "  << phase
+		  << " energy "  << energy/1e9 << " GeV "
+		  << " ct / phase difference "  << ct;
+	*/
 	const double delta = - volt / (energy)
 			*sin(2e0*M_PI*frequency/c0*ct+phase);
+	/*
+	std::cout << " resuts in delta " << std::scientific << delta  << std::endl;
+	*/
 	return delta;
 }
 
@@ -25,18 +35,61 @@ static double compute_delta(const double volt,  const double frequency, const do
 BOOST_AUTO_TEST_CASE(test20_cavity)
 {
 
-	const double frequency=500e6, voltage=0.500e5;
+	const double frequency=500e6, voltage=500e3;
 	const double ct = 1e-3;
 	Config C;
 	C.set<std::string>("name", "test");
 	C.set<double>("Frequency", frequency);
 	C.set<double>("Voltage", voltage);
+	C.set<double>("harnum", 1);
 
 	tsc::ConfigType calc_config;
 	tse::CavityType cavity(C);
 
 	calc_config.Cavity_on = true;
+	calc_config.Energy = 1.7e9;
 
+	std::cout << "energy " << calc_config.Energy << std::endl;
+	const double delta = compute_delta(voltage, frequency, 0.0, calc_config.Energy, ct);
+
+	BOOST_CHECK_CLOSE(cavity.getVoltage(),   voltage,   1e-12);
+	BOOST_CHECK_CLOSE(cavity.getFrequency(), frequency, 1e-12);
+
+	const ss_vect<double> ps_orig = {0, 0, 0, 0, 0, ct};
+	BOOST_CHECK_CLOSE(ps_orig[ct_],   ct,   1e-12);
+
+
+	{
+		ss_vect<double> ps = ps_orig;
+		cavity.pass(calc_config, ps);
+
+		BOOST_CHECK_CLOSE(ps[ct_],    ct, 1e-14);
+		BOOST_CHECK_CLOSE(ps[delta_], delta, 1e-14);
+
+		BOOST_CHECK_SMALL(ps[x_],     1e-14);
+		BOOST_CHECK_SMALL(ps[y_],     1e-14);
+		BOOST_CHECK_SMALL(ps[px_],    1e-14);
+		BOOST_CHECK_SMALL(ps[py_],    1e-14);
+	}
+
+}
+
+BOOST_AUTO_TEST_CASE(test21_cavity_delta)
+{
+
+	const double frequency=499.036e6, voltage=1.5e6;
+	const double ct = 1e-3;
+	Config C;
+	C.set<std::string>("name", "test");
+	C.set<double>("Frequency", frequency);
+	C.set<double>("Voltage", voltage);
+	C.set<double>("harnum", 538);
+
+	tsc::ConfigType calc_config;
+	tse::CavityType cavity(C);
+
+	calc_config.Cavity_on = true;
+	calc_config.Energy = 2.5e9;
 	std::cout << "energy " << calc_config.Energy << std::endl;
 	const double delta = compute_delta(voltage, frequency, 0.0, calc_config.Energy, ct);
 
