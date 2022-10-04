aeff77cc48882211c3767907fc85f65e4bab74c9
J.B <jbngtssn@gmail.com>
2022-10-04 17:17:30 +0200

    Radiation: documentation work in progress
    radiation delegate: diffusion coefficient computation being looked into
                        using Energy in GeV for constant computation
    
    radiation_delegate_api: format beautify
    thor scsi utils.h:      fixed comment
    
    python thor scsi:
    	 output:         clamp small numerical values to zero
    	 closed orbit:   adapted logging messages to requirements of todays debugging
    	 linear optics:  blackified code
    
    example:
           radiate: used for debugging purposes

--- src/thor_scsi/elements/radiation_delegate_api.h
+++ src/thor_scsi/elements/radiation_delegate_api.h
@@ -23,7 +23,7 @@ namespace thor_scsi::elements {
 	public:
 		virtual ~RadiationDelegateKickInterface(void){}
 		virtual void view(const FieldKickAPI& kick, const ss_vect<double> &ps, const enum ObservedState, const int cnt) = 0;
-		virtual void view(const FieldKickAPI& kick, const ss_vect<tps>    &ps, const enum ObservedState, const int cnt)  = 0;
+		virtual void view(const FieldKickAPI& kick, const ss_vect<tps>    &ps, const enum ObservedState, const int cnt) = 0;
 		virtual void show(std::ostream& strm, int level) const{
 			strm << "RadiationDelegateKickInterface";
 		}
