4cb304b5b85d56934c89709d1fa772e3883031a9
J.B <jbngtssn@gmail.com>
2022-10-04 17:16:04 +0200

    Code clean-up for radiation computation:   ../examples/radiate.py   ../utils/radiate.py   ../utils/linear_optics.py   ../utils/linalg.py   ../utils/output.py
    radiate: kept clean up

--- python/examples/radiate.py
+++ python/examples/radiate.py
@@ -26,114 +26,7 @@ import thor_scsi.lib as tslib
 # from thor_scsi.utils.linalg import match_eigenvalues_to_plane_orig
 from thor_scsi.utils.closed_orbit import compute_closed_orbit
 from thor_scsi.utils.output import vec2txt, mat2txt
-from thor_scsi.utils.linear_optics import compute_dispersion, compute_A_CS
-from thor_scsi.utils.linalg import compute_A, omega_block_matrix
-
-
-X_ = 0
-Y_ = 1
-Z_ = 2
-
-
-def chop_vec(vec, eps):
-    for k in range(vec.size):
-        if np.abs(vec[k]) < eps:
-            vec[k] = 0e0
-    return vec
-
-
-def chop_mat(mat, eps):
-    for k in range(mat[:, 0].size):
-        chop_vec(mat[k, :], eps)
-    return mat
-
-
-def chop_cmplx_vec(vec, eps):
-    for k in range(vec.size):
-        [x, y] = [vec[k].real, vec[k].imag]
-        if np.abs(x) < eps:
-            x = 0e0
-        if np.abs(y) < eps:
-            y = 0e0
-        vec[k] = complex(x, y)
-    return vec
-
-
-def chop_cmplx_mat(mat, eps):
-    for k in range(mat[:, 0].size):
-        chop_cmplx_vec(mat[k, :], eps)
-    return mat
-
-
-def acos2(sin, cos):
-    # Calculate the normalised phase advance from the Poincaré map:
-    #   Tr{M} = 2 cos(2 pi nu)
-    # i.e., assuming mid-plane symmetry.
-    # The sin part is used to determine the quadrant.
-    mu = np.arccos(cos)
-    if sin < 0e0:
-        mu = 2e0*np.pi - mu
-    return mu
-
-
-def calculate_nu(M):
-    tr = M.trace()
-    # Check if stable.
-    if tr < 2e0:
-        calculate_nu(tr/2e0,  M[0][1])/(2e0*np.pi)
-        return nu
-    else:
-        print("\ncalculate_nu: unstable\n")
-        return float('nan')
-
-
-def calculate_nus(n_dof, M):
-    nus = np.zeros(n_dof, float)
-    for k in range(n_dof):
-        nus[k] = calculate_nu(M[2*k:2*k+2, 2*k:2*k+2])/(2e0*np.pi)
-        if n_dof == 3:
-            nus[2] = 1e0 - nus[2]
-    return nus
-
-
-def calculate_nu_symp(n_dof, M):
-    # Calculate normalised phase advance from a symplectic periodic matrix.
-    n = 2*dof
-    I = np.identity(4)
-    tr = np.zeros(n_dof, float)
-    for k in range(n_dof):
-        tr[k] = np.trace(M[2*k:2*k+2, 2*k:2*k+2])
-    M4b4 = M[0:4, 0:4]
-    [p1, pm1] = [np.linalg.det(M4b4-I), np.linalg.det(M4b4+I)]
-    [po2, q] = [(p1-pm1)/16e0, (p1+pm1)/8e0 - 1e0]
-    if tr[X_] > tr[Y_]:
-        sgn = 1
-    else:
-        sgn = -1
-    [x, y] = [-po2+sgn*np.sqrt(po2**2-q), -po2-sgn*np.sqrt(po2**2-q)]
-    nu = []
-    nu.extend([acos2(M[0][1], x)/(2e0*np.pi), acos2(M[2][3], y)/(2e0*np.pi)])
-    if n_dof == 3:
-        nu.append(1e0-acos2(M[4][5], tr[Z_]/2e0)/(2e0*np.pi))
-    return np.array(nu)
-
-
-def find_closest_nu(nu, w):
-    min = 1e30
-    for k in range(w.size):
-        nu_k = acos2(w[k].imag, w[k].real)/(2e0*np.pi)
-        diff =  np.abs(nu_k-nu)
-        if diff < min:
-            [ind, min] = [k, diff]
-    return ind
-
-
-def sort_eigen_vec(dof, nu, w):
-    order = np.zeros(2*dof, int)
-    for k in range(dof):
-        order[2*k]   = find_closest_nu(nu[k], w)
-        order[2*k+1] = find_closest_nu(1e0-nu[k], w)
-    return order
+from thor_scsi.utils.linear_optics import compute_M_diag
 
 
 t_dir = os.path.join(os.environ["HOME"], "Nextcloud", "thor_scsi")
@@ -183,70 +76,18 @@ if calc_config.Cavity_on == True:
     dof = 3
 else:
     dof = 2
-n = 2 * dof
 
 r = compute_closed_orbit(acc, calc_config, delta=0e0)
 M = r.one_turn_map[:6, :6]
 print("\nM:\n"+mat2txt(M))
 
-nu_symp = calculate_nu_symp(dof, M)
-# Diagonalise M.
-[w, u] = np.linalg.eig(M[:n, :n])
+compute_M_diag(dof, M)
 
-nu_eig = np.zeros(n)
-for k in range(n):
-    nu_eig[k] = acos2(w[k].imag, w[k].real)/(2e0*np.pi)
-
-if debug_prt:
-    print("\nu:\n"+mat2txt(u))
-    print("\nnu_symp:\n"+vec2txt(nu_symp))
-    print("\nnu_eig:\n"+vec2txt(nu_eig))
-    print("\nlambda:\n"+vec2txt(w))
-    # print("\nu:\n"+mat2txt(u))
-
-order = sort_eigen_vec(dof, nu_symp, w)
-
-w_ord = np.zeros(n, complex)
-u_ord = np.zeros((n, n), complex)
-nu_eig_ord = np.zeros(n, float)
-for k in range(n):
-    w_ord[k]      = w[order[k]]
-    u_ord[:, k]   = u[:, order[k]]
-    nu_eig_ord[k] = acos2(w_ord[k].imag, w_ord[k].real)/(2e0*np.pi)
-
-if debug_prt:
-    print("\norder:\n", order)
-    print("\nnu_eig_ord:\n"+vec2txt(nu_eig_ord))
-    print("\nlambda_ord:\n"+vec2txt(w_ord))
-    # print("\nu_ord:\n"+mat2txt(u_ord))
-    print("\nu_ord^-1.M.u_ord:\n"+mat2txt(
-        chop_cmplx_mat(np.linalg.inv(u_ord) @ M[:n, :n] @ u_ord, 1e-15)))
-
-eta = compute_dispersion(M)
-
-if debug_prt:
-    print("\neta:\n", eta)
-
-[A, A_inv, u1] = compute_A(dof, eta, u_ord)
-
-if debug_prt:
-    print("\nu1:\n"+mat2txt(u1))
-    print("\nu1^-1.M.u1:\n"+mat2txt(
-        chop_cmplx_mat(np.linalg.inv(u1) @ M[:n, :n] @ u1, 1e-13)))
-    print("\nu1^T.omega.u1:\n"+mat2txt(
-        chop_cmplx_mat(u1.T @ omega_block_matrix(dof) @ u1, 1e-13)))
-    print("\nA:\n"+mat2txt(chop_mat(A_inv, 1e-13)))
-    print("\nA^T.omega.A:\n"+mat2txt(chop_mat(
-        A[:n, :n].T @ omega_block_matrix(dof) @ A[:n, :n], 1e-13)))
-
-print("\nA_CS:\n"+mat2txt(chop_mat(compute_A_CS(dof, A)[0], 1e-10)))
-print("\nR:\n"+mat2txt(chop_mat(A_inv @ M @ A, 1e-10)))
+exit()
 
 r = calculate_radiation(
     acc, energy=2.5e9, calc_config=calc_config, install_radiators=True
 )
-
-exit()
 
 use_tpsa = True
 if not use_tpsa:
--- python/thor_scsi/utils/radiate.py
+++ python/thor_scsi/utils/radiate.py
@@ -2,21 +2,12 @@
 """
 import thor_scsi.lib as tslib
 from .accelerator import instrument_with_radiators
-from .closed_orbit import compute_closed_orbit
-from . import linalg
-from . import linear_optics as lo
-from .output import vec2txt, mat2txt
-from .phase_advance import compute_nus_for_symplectic_matrix
-from .phase_space_vector import ss_vect_tps2ps_jac, array2ss_vect_tps
 import numpy as np
 from dataclasses import dataclass
 
 import logging
 logger = logging.getLogger("thor-scsi")
 
-X_ = 0
-Y_ = 1
-Z_ = 2
 
 @dataclass
 class RadiationResult:
@@ -25,77 +16,6 @@ class RadiationResult:
     emittance: np.ndarray
     #: for consistency checks
     fractional_tunes: np.ndarray
-
-
-def acos2(sin, cos):
-    # Calculate the normalised phase advance from the trace = 2*2*pi* nu of
-    # the Poincaré map; i.e., assuming mid-plane symmetry.
-    # The sin part is used to determine the quadrant.
-    mu = np.arccos(cos)
-    if sin < 0e0:
-        mu = 2e0*np.pi - mu
-    return mu
-
-
-def calculate_nu(M):
-    tr = M.trace()
-    # Check if stable.
-    if tr < 2e0:
-        calculate_nu(tr/2e0,  M[0][1])/(2e0*np.pi)
-        return nu
-    else:
-        print("\ncalculate_nu: unstable\n")
-        return float('nan')
-
-
-def calculate_nus(n_dof, M):
-    nus = np.zeros(n_dof, float)
-    for k in range(n_dof):
-        nus[k] = calculate_nu(M[2*k:2*k+2, 2*k:2*k+2])/(2e0*np.pi)
-        if n_dof == 3:
-            nus[2] = 1e0 - nus[2]
-    return nus
-
-
-def calculate_nu_symp(M):
-    # Calculate normalised phase advance from a symplectic periodic matrix.
-    dof = 2
-    n = 2*dof
-    I = np.identity(n)
-    tr = np.zeros(3, float)
-    for k in range(3):
-        tr[k] = np.trace(M[2*k:2*k+2, 2*k:2*k+2])
-    M4b4 = M[0:n, 0:n]
-    [p1, pm1] = [np.linalg.det(M4b4-I), np.linalg.det(M4b4+I)]
-    [po2, q] = [(p1-pm1)/16e0, (p1+pm1)/8e0 - 1e0]
-    if tr[X_] > tr[Y_]:
-        sgn = 1
-    else:
-        sgn = -1
-    [x, y] = [-po2+sgn*np.sqrt(po2**2-q), -po2-sgn*np.sqrt(po2**2-q)]
-    nu = \
-        [acos2(M[0][1], x)/(2e0*np.pi), acos2(M[2][3], y)/(2e0*np.pi),
-         1e0-acos2(M[4][5], tr[Z_]/2e0)/(2e0*np.pi)]
-    return np.array(nu)
-
-
-def find_closest_nu(nu, w):
-    min = 1e30
-    for k in range(w.size):
-        nu_k = acos2(w[k].imag, w[k].real)/(2e0*np.pi)
-        diff =  np.abs(nu_k-nu)
-        if diff < min:
-            [ind, min] = [k, diff]
-    return ind
-
-
-def sort_eigen_vec(nu, w):
-    order = []
-    for k in range(nu.size):
-        order.append(find_closest_nu(nu[k], w))
-    for k in range(nu.size):
-        order.append(find_closest_nu(1e0-nu[k], w))
-    return np.array(order)
 
 
 def calculate_radiation(
@@ -147,63 +67,6 @@ def calculate_radiation(
         f"calc_config radiation { calc_config.radiation} emmittance {calc_config.emittance} Cavity on {calc_config.Cavity_on}"
     )
 
-    # Compute the fixed point ... radation is on
-    r = compute_closed_orbit(acc, calc_config, delta=0e0)
-    print("r.one_turn_map")
-    print(mat2txt(r.one_turn_map))
-
-    nu = calculate_nu_symp(r.one_turn_map)
-    print("\nnu = [{:7.5f}, {:7.5f}, {:7.5f}]".format(nu[X_], nu[Y_], nu[Z_]))
-
-    # diagonalise M
-    n = 2 * dof
-    M = r.one_turn_map[:n, :n]
-    M_tp = np.transpose(M)
-
-    # Finds eigen values and eigen vectors ... same functionallity as in
-    # find_phase_space_origin / find_phase_space_fix_point
-    w, v = np.linalg.eig(M_tp)
-    print("\nlambda:")
-    for w_k in w:
-        nu_k = acos2(w_k.imag, w_k.real)/(2e0*np.pi)
-        print(" %7.5f" % (nu_k), end="")
-    print()
-
-    order = sort_eigen_vec(nu, w)
-    print("\norder:\n", order)
-    w_ord= np.array([w[order[0]], w[order[3]], w[order[1]], w[order[4]],
-                     w[order[2]], w[order[5]]])
-    v_ord= np.array([v[order[0]], v[order[3]], v[order[1]], v[order[4]],
-                     v[order[2]], v[order[5]]])
-    print("\nw:")
-    print(vec2txt(w))
-    print("\nw_ord:")
-    print(vec2txt(w_ord))
-    print("\nv:")
-    print(mat2txt(v))
-    print("\nv_ord:")
-    print(mat2txt(v_ord))
-
-    eta = lo.compute_dispersion(M)
-    print(eta)
-    # eta = np.zeros(6, np.float)
-    A, A_inv, v1 = linalg.compute_A(dof, eta, v_ord)
-    print("\nA:")
-    print(mat2txt(A))
-    return
-
-    Atmp = np.zeros([7, 7], dtype=np.float)
-    Atmp[:6, :6] = A
-    print("Atmp ")
-    print(mat2txt(Atmp))
-    # Atmp[[2, 3, 4, 5], :] = Atmp[[4, 5, 2, 3], :]
-    print("Atmp resuffled")
-    print(mat2txt(Atmp))
-    Ap = array2ss_vect_tps(Atmp)
-    return
-
-    print("Ap before calc")
-    print(Ap)
 
     # Diffusion coefficients
     acc.propagate(calc_config, Ap)
