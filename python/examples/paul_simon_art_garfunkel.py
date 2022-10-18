fcabb1f1ced56276c3fa38a6992c3a406c712f08
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-10-18 17:26:51 +0200

    Paul Simon Art: adding eta plots

--- python/examples/paul_simon_art_garfunkel.py
+++ python/examples/paul_simon_art_garfunkel.py
@@ -34,6 +34,7 @@
 
 
 import logging
+
 logging.basicConfig(level="WARNING")
 # Levels: DEBUG, INFO, WARNING, ERROR, and CRITICAL.
 
@@ -48,7 +49,7 @@ from thor_scsi.utils.linear_optics import (
     compute_map,
     compute_nu_symp,
     compute_M_diag,
-    compute_twiss_along_lattice
+    compute_twiss_along_lattice,
 )
 from thor_scsi.utils.radiate import compute_radiation
 from thor_scsi.utils.phase_space_vector import map2numpy
@@ -56,11 +57,7 @@ from thor_scsi.utils.output import mat2txt, vec2txt
 from thor_scsi.utils.twiss_output import twiss_ds_to_df, df_to_tsv
 
 
-X_, Y_, Z_ = [
-    tslib.spatial_index.X,
-    tslib.spatial_index.Y,
-    tslib.spatial_index.Z
-]
+X_, Y_, Z_ = [tslib.spatial_index.X, tslib.spatial_index.Y, tslib.spatial_index.Z]
 
 
 def plt_twiss(ds):
@@ -83,47 +80,48 @@ def plt_twiss(ds):
 def prt_fam(acc, fam_name):
     print("\nprt_fam:")
     for q in acc.elementsWithName(fam_name):
-        print("  {:4s} {:3d} {:6.3f} {:4.2f}".
-              format(q.name, q.index, q.getMultipoles().getMultipole(2).real,
-                     q.getLength()))
+        print(
+            "  {:4s} {:3d} {:6.3f} {:4.2f}".format(
+                q.name, q.index, q.getMultipoles().getMultipole(2).real, q.getLength()
+            )
+        )
 
 
 def set_db_2_fam(acc, fam_name, db_2):
     for q in acc.elementsWithName(fam_name):
         b_2 = q.getMultipoles().getMultipole(2).real
         index = q.index
-        q.getMultipoles().setMultipole(2, b_2+db_2)
+        q.getMultipoles().setMultipole(2, b_2 + db_2)
 
 
 def set_db_2L_fam(acc, fam_name, db_2L):
     q = acc.find(fam_name, 0)
     L = q.getLength()
-    set_db_2_fam(acc, q.name, db_2L/L)
+    set_db_2_fam(acc, q.name, db_2L / L)
 
 
 def compute_dnu_db_2L(acc, calc_config, fam_name, db_2L):
     dof = 2
     nu = np.zeros([2, dof])
     for k in range(-1, 2, 2):
-        set_db_2L_fam(acc, fam_name, k*db_2L)
+        set_db_2L_fam(acc, fam_name, k * db_2L)
         M = map2numpy(compute_map(acc, calc_config))[:6, :6]
-        set_db_2L_fam(acc, fam_name, -k*db_2L)
-        nu[(k+1)//2] = compute_nu_symp(dof, M)
+        set_db_2L_fam(acc, fam_name, -k * db_2L)
+        nu[(k + 1) // 2] = compute_nu_symp(dof, M)
 
-    dnu_db_2 = (nu[1]-nu[0])/(2e0*db_2L)
+    dnu_db_2 = (nu[1] - nu[0]) / (2e0 * db_2L)
     return dnu_db_2
 
 
 def tweak_nu(fam_names, dnu_x, dnu_y):
     n = len(fam_names)
     dnu = np.array([dnu_x, dnu_y])
-    dnu_db_2L = np.zeros((2, n), dtype='float')
+    dnu_db_2L = np.zeros((2, n), dtype="float")
     for k in range(n):
-        dnu_db_2L[:, k] = \
-            compute_dnu_db_2L(acc, calc_config, fam_names[k],   1e-4)
+        dnu_db_2L[:, k] = compute_dnu_db_2L(acc, calc_config, fam_names[k], 1e-4)
     print("\ndnu_db_2L:\n", mat2txt(dnu_db_2L))
     u, s, v_t = np.linalg.svd(dnu_db_2L, full_matrices=False)
-    dnu_db_2L_inv = (v_t.T @ np.diag(s**-1) @ u.T)
+    dnu_db_2L_inv = v_t.T @ np.diag(s ** -1) @ u.T
     db_2L = dnu_db_2L_inv @ dnu
     print("\ndb_2L = ", vec2txt(db_2L))
     for k in range(n):
@@ -135,7 +133,7 @@ t_file = os.path.join(t_dir, "alsu-7ba-20180503c.lat")
 # t_file = os.path.join(t_dir, "b3_sf_40Grad_JB.lat")
 
 acc = accelerator_from_config(t_file, add_marker_at_start=True)
-assert(type(acc[0]) == tslib.Marker)
+assert type(acc[0]) == tslib.Marker
 
 calc_config = tslib.ConfigType()
 
@@ -143,12 +141,15 @@ cav = acc.find("cav", 0)
 cav.setHarmonicNumber(327)
 cav.setPhase(180.0)
 print("\nCavity", repr(cav))
-print(f"""\nCavity info:
+print(
+    f"""\nCavity info:
   f [MHz] {1e-6*cav.getFrequency()}
   V [MV]  {1e-6*cav.getVoltage()}
   h       {cav.getHarmonicNumber()}
   phi     {cav.getPhase()}
-""", end="")
+""",
+    end="",
+)
 
 dof = 2
 
@@ -182,9 +183,31 @@ calc_config.radiation = False
 calc_config.Cavity_on = False
 
 # Tweak the tune using all the quadrupole families.
-b_2 = ['q1', 'q2', 'q3', 'q4', 'mb3h', 'qfh', 'mqfh']
+b_2 = ["q1", "q2", "q3", "q4", "mb3h", "qfh", "mqfh"]
 tweak_nu(b_2, 0.00501, -0.00670)
 M = map2numpy(compute_map(acc, calc_config))[:6, :6]
 nu = compute_nu_symp(dof, M)
 print("\nM:\n" + mat2txt(M))
 print("\nnu = [{:7.5f}, {:7.5f}]".format(nu[X_], nu[Y_]))
+
+fig, axes = plt.subplots(2, 1, sharex=True)
+ax_eta_x, ax_eta_px = axes
+ds.dispersion.sel(phase_coordinate="x").plot(ax=ax_eta_x)
+ds.dispersion.sel(phase_coordinate="px").plot(ax=ax_eta_px)
+ax_eta_x.set_xlabel("s [m]")
+ax_eta_px.set_xlabel("s [m]")
+ax_eta_x.set_ylabel(r"$\eta_x$")
+ax_eta_px.set_ylabel(r"$\eta_{x'}$")
+
+fig, ax = plt.subplots(1, 1, sharex=True)
+ax.plot(
+    ds.dispersion.sel(phase_coordinate="x"),
+    ds.dispersion.sel(phase_coordinate="px"),
+    marker=".",
+    linestyle="--",
+    linewidth=0.25,
+)
+ax.set_xlabel(r"$\eta_x$")
+ax.set_ylabel(r"$\eta_x'$")
+
+plt.show()
