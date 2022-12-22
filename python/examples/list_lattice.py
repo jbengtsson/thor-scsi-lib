12ece7fd511f025353d010f32404cd028fea5bfe
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-12-22 15:24:49 +0100

    removed list_lattices: outdated
    see a_machine on how to read and use lattices

--- python/examples/list_lattice.py
+++
@@ -1,9 +0,0 @@
-import thor.lib as scsi
-
-file_name = "tests/lattices/b2_stduser_beamports_blm_tracy_corr"
-lat = scsi.LatticeType()
-lat.Lat_Read(file_name)
-lat.Lat_Init()
-
-for cnt, elem in enumerate(lat.elems):
-    print(f"{cnt:04d} {elem}")
