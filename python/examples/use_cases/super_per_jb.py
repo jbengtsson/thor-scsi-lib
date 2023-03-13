4307fdb123060afd72e130ffbd92773c258f1bd0
J.B <jbngtssn@gmail.com>
2023-03-13 08:59:07 +0100

    Prototyping of use case for general super period optimisation: super_per_jb.py.

--- python/examples/use_cases/super_per_jb.py
+++ python/examples/use_cases/super_per_jb.py
@@ -141,14 +141,20 @@ def compute_periodic_solution(lat, model_state, prt):
     model_state.radiation = False
     model_state.Cavity_on = False
 
-    M, A = lo.compute_map_and_diag(n_dof, lat, model_state, desc=desc)
-    if prt:
-        print("\nM:\n", mat2txt(M.jacobian()))
-    res= cs.compute_Twiss_A(A)
-    Twiss = res[:3]
-    if prt:
-         print_Twiss_params("\nTwiss:\n", Twiss)
-    ds = lo.compute_Twiss_along_lattice(n_dof, lat, model_state, A=A, desc=desc)
+    stable, M, A = lo.compute_map_and_diag(n_dof, lat, model_state, desc=desc)
+    if stable:
+        if prt:
+            print("\nM:\n", mat2txt(M.jacobian()))
+        res= cs.compute_Twiss_A(A)
+        Twiss = res[:3]
+        if prt:
+             print_Twiss_params("\nTwiss:\n", Twiss)
+        ds = \
+            lo.compute_Twiss_along_lattice(
+                n_dof, lat, model_state, A=A, desc=desc)
+    else:
+        ds = np.nan
+        print("\ncompute_periodic_solution: unstable")
 
     return M, A, ds
 
@@ -277,8 +283,8 @@ def opt_super_period(lat, param_list, C, bounds, phi_des, eps_x_des, nu_des,
 
     def compute_nu_cell(data):
         nu_cell = np.zeros(2, dtype=float)
-        loc1 = lat.find("bb_h", 1).index
-        loc2 = lat.find("bb_h", 3).index
+        loc1 = lat.find("sf", 1).index
+        loc2 = lat.find("sf", 3).index
         for k in range(loc1, loc2):
             nu_cell[X_] += data.twiss.sel(plane="x", par="dnu").values[k]
             nu_cell[Y_] += data.twiss.sel(plane="y", par="dnu").values[k]
@@ -312,22 +318,26 @@ def opt_super_period(lat, param_list, C, bounds, phi_des, eps_x_des, nu_des,
                 data.dispersion.sel(phase_coordinate="x").values[0]
             nu_cell = compute_nu_cell(data)
 
-            U_0, J, tau, eps = \
+            stable, U_0, J, tau, eps = \
                 rad.compute_radiation(lat, model_state, 2.5e9, 1e-15, desc=desc)
 
-            n = len(weights)
-            dchi_2 = np.zeros(n, dtype=float)
-            dchi_2[0] = weights["eps_x"] * np.sum((eps[X_] - eps_x_des) ** 2)
-            dchi_2[1] = weights["nu_cell"] * np.sum((nu_cell - nu_des) ** 2)
-            dchi_2[2] = \
-                weights["beta_straight"] \
-                * np.sum((beta_straight - beta_straight_des) ** 2)
-            dchi_2[3] = weights["eta_straight_x"] * eta_straight_x ** 2
-            dchi_2[4] = weights["xi"] * np.sum(xi ** 2)
-            dchi_2[5] = weights["alpha_c"] / alpha_c ** 2
-            chi_2 = 0e0
-            for k in range(n):
-                chi_2 += dchi_2[k]
+            if stable:
+                n = len(weights)
+                dchi_2 = np.zeros(n, dtype=float)
+                dchi_2[0] = \
+                    weights["eps_x"] * np.sum((eps[X_] - eps_x_des) ** 2)
+                dchi_2[1] = weights["nu_cell"] * np.sum((nu_cell - nu_des) ** 2)
+                dchi_2[2] = \
+                    weights["beta_straight"] \
+                    * np.sum((beta_straight - beta_straight_des) ** 2)
+                dchi_2[3] = weights["eta_straight_x"] * eta_straight_x ** 2
+                dchi_2[4] = weights["xi"] * np.sum(xi ** 2)
+                dchi_2[5] = weights["alpha_c"] / alpha_c ** 2
+                chi_2 = 0e0
+                for k in range(n):
+                    chi_2 += dchi_2[k]
+            else:
+                chi_2 = 1e30
         else:
             chi_2 = 1e30
 
@@ -336,17 +346,25 @@ def opt_super_period(lat, param_list, C, bounds, phi_des, eps_x_des, nu_des,
             n_iter_min = n_iter
             prms_min = prms
             print(f"\n{n_iter:4d} chi_2 = {chi_2:9.3e}\n\n  prms   =\n", prms)
-            if stable[0] and stable[1] and stable[2]:
+            if stable:
                 print(f"\n  dchi_2 =\n", dchi_2)
             print(f"\n  phi            = {compute_phi(lat):8.5f}")
+            [b1, b2, b3] = \
+                [param_list[0][0], param_list[1][0], param_list[2][0]]
+            print(f"\n  {b1:5s}          = {get_phi_elem(lat, b1, 0):8.5f}")
+            print(f"  {b2:5s}          = {get_phi_elem(lat, b2, 0):8.5f}")
+            print(f"  {b3:5s}          = {get_phi_elem(lat, b3, 0):8.5f}")
             [b1, b2, b3, b4] = \
-                [param_list[0][0], param_list[1][0], param_list[2][0],
-                 param_list[4][0]]
-            print(f"  {b1:5s}          = {get_phi_elem(lat, b1, 0):8.5f}")
+                [param_list[3][0], param_list[4][0], param_list[5][0],
+                 param_list[6][0]]
+            print(f"\n  {b1:5s}          = {get_phi_elem(lat, b1, 0):8.5f}")
             print(f"  {b2:5s}          = {get_phi_elem(lat, b2, 0):8.5f}")
             print(f"  {b3:5s}          = {get_phi_elem(lat, b3, 0):8.5f}")
             print(f"  {b4:5s}          = {get_phi_elem(lat, b4, 0):8.5f}")
-            if stable[0] and stable[1] and stable[2]:
+            [b1, b2] = [rbend, param_list[8][0]]
+            print(f"\n  {b1:5s}          = {get_phi_elem(lat, b1, 0):8.5f}")
+            print(f"  {b2:5s}          = {get_phi_elem(lat, b2, 0):8.5f}")
+            if stable:
                 print(f"\n  eps_x          = {1e12*eps[X_]:5.3f}")
                 print("  nu_cell        = [{:7.5f}, {:7.5f}]".
                       format(nu_cell[X_], nu_cell[Y_]))
@@ -367,7 +385,11 @@ def opt_super_period(lat, param_list, C, bounds, phi_des, eps_x_des, nu_des,
 
         n_iter += 1
         set_prm(lat, param_list, prms)
-        dphi = (phi - 8*prms[0] - 2*prms[1] - 2*prms[3])/8e0
+        dphi = \
+            (phi
+             - 8*(prms[0] + prms[1] + prms[2])
+             - 2*(prms[3] + prms[4] + prms[5] + prms[6])
+             - 2*prms[8])/8e0
         set_phi_fam(lat, rbend, dphi)
         chi_2 = compute_chi_2(lat, model_state, eps_x_des, nu_des, prms)
         return chi_2
@@ -379,8 +401,8 @@ def opt_super_period(lat, param_list, C, bounds, phi_des, eps_x_des, nu_des,
         global prms_min
 
         M = lo.compute_map(lat, model_state, desc=desc, tpsa_order=tpsa_order)
-        stable = lo.check_if_stable_3D(M.jacobian())
-        if not stable[0] or not stable[1] or not stable[2]:
+        stable = lo.check_if_stable(3, M.jacobian())
+        if not stable:
             print("\nCell unstable:")
             print("  prms_min =", prms_min)
             print("  Setting prs_min for plot.")
@@ -405,13 +427,13 @@ def opt_super_period(lat, param_list, C, bounds, phi_des, eps_x_des, nu_des,
     global rbend
 
     max_iter = 1000
-    f_tol = 1e-4
-    x_tol = 1e-4
+    f_tol    = 1e-4
+    x_tol    = 1e-4
 
     print("\nopt_super_period:\n")
     # Initialise parameters.
     prms1 = prms0 = get_prm(lat, param_list)
-    rbend = param_list[2][0]
+    rbend = param_list[7][0]
     print("\nrbend = ", rbend)
     print("\nprms = ", prms1)
 
@@ -425,8 +447,9 @@ def opt_super_period(lat, param_list, C, bounds, phi_des, eps_x_des, nu_des,
         f_unit_cell,
         prms1,
         method="CG",
-        bounds=bounds,
-        options={"xtol": x_tol, "gtol": f_tol, "maxiter": max_iter},
+        # bounds=bounds,
+        # options={"xtol": x_tol, "maxiter": max_iter},
+        options={"gtol": f_tol, "maxiter": max_iter},
     )
 
     prt_result(f_unit_cell, param_list, prms0, minimum)
@@ -460,7 +483,7 @@ t_dir = os.path.join(os.environ["HOME"], "Nextcloud", "thor_scsi", "JB")
 # t_file = os.path.join(t_dir, 'b3_tst.lat')
 # t_file = os.path.join(t_dir, "b3_sf_40Grad_JB.lat")
 # t_file = os.path.join(t_dir, "b3_sfsf_4Quads_unitcell.lat")
-t_file = os.path.join(t_dir, "b3_sfsf4Q_tracy.lat")
+t_file = os.path.join(t_dir, "b3_sfsf4Q_tracy_jb_2.lat")
 
 # Read in & parse lattice file.
 lat = accelerator_from_config(t_file)
@@ -471,6 +494,16 @@ model_state = tslib.ConfigType()
 n_dof = 2
 model_state.radiation = False
 model_state.Cavity_on = False
+
+if not False:
+    # Set RF cavity phase for negative alpha_c.
+    cav = lat.find("cav", 0)
+    cav.set_phase(180.0)
+    print("\nRF Cavity:")
+    print(f"  f [MHz]   = {1e-6*cav.get_frequency():3.1f}")
+    print(f"  V [MV]    = {1e-6*cav.get_voltage():3.1f}")
+    print(f"  h         = {cav.get_harmonic_number():1d}")
+    print(f"  phi [deg] = {cav.get_phase():3.1f}")
 
 if False:
     print(compute_phi(lat))
@@ -494,7 +527,7 @@ set_b_n_fam(lat, "sd", MultipoleIndex.sextupole, 0e0)
 M = lo.compute_map(lat, model_state, desc=desc, tpsa_order=tpsa_order)
 stable, nu, xi = lo.compute_nu_xi(desc, tpsa_order, M)
 # Compute radiation properties.
-U_0, J, tau, eps = \
+stable, U_0, J, tau, eps = \
     rad.compute_radiation(lat, model_state, 2.5e9, 1e-15, desc=desc)
 
 C = rad.compute_circ(lat)
@@ -515,64 +548,76 @@ beta  = np.array([3.0, 3.0])  # Beta functions at the centre of the straight.
 weights = {
     "eps_x":          1e2*1e15,
     "nu_cell":        1e0,
-    "beta_straight":  1e-4,
+    "beta_straight":  1e-5,
     "eta_straight_x": 1e3,
     "xi":             1e-6,
-    "alpha_c":        1e-5*1e-14
+    "alpha_c":        0*1e-14
 }
 
 # Parameter family names & type.
 param_list = [
-    ("bb_h", "dphi"),         # Dipole.
+    ("bb_h1", "dphi"),        # Dipole.
+    ("bb_h2", "dphi"),        # Dipole.
+    ("bb_h3", "dphi"),        # Dipole.
 
-    ("mbb",   "dphi"),        # Dipole.
+    ("mbb1",  "dphi"),        # Dipole.
+    ("mbb2",  "dphi"),        # Dipole.
+    ("mbb3",  "dphi"),        # Dipole.
+    ("mbb4",  "dphi"),        # Dipole.
 
 #    ("br",   "not used"),     # Focusing reverse bend;
-    ("br",   "b_2"),          # used to set total bend angle.
+    ("br",    "b_2"),          # used to set total bend angle.
 
     ("mbr",   "dphi"),        # Focusing reverse bend.
     ("mbr",   "b_2"),
 
 
-    ("qd",   "b_2"),          # Defocusing quadrupole.
+    ("qd",    "b_2"),          # Defocusing quadrupole.
+    ("mqd",   "b_2"),          # Defocusing quadrupole.
 
-    ("uq1",  "b_2"),          # Triplet for matching section.
-    ("uq2",  "b_2"),
-    ("uq3",  "b_2"),
-    ("uq4",  "b_2"),
+    ("uq1",   "b_2"),          # Triplet for matching section.
+    ("uq2",   "b_2"),
+    ("uq3",   "b_2"),
+    ("uq4",   "b_2"),
 
-    ("ul1",  "L"),
-    ("ul2",  "L"),
-    ("ul3",  "L"),
-    ("ul4",  "L"),
+    ("ul1",   "L"),
+    ("ul2",   "L"),
+    ("ul3",   "L"),
+    ("ul4",   "L")
 ]
 
 # Max parameter range.
 b_2_max = 12.0
-L_min   = 0.1
+L_min   = 0.05
 
 bounds = [
-    (1.5,      2.5),          # bb_h phi.
+    (0.5,      7.0),          # bb phi.
+    (0.5,      7.0),          # bb phi.
+    (0.5,      7.0),          # bb phi.
 
-    (0.0,      b_2_max),      # br b_2.
+    (0.5,      7.0),          # mbb phi.
+    (0.5,      7.0),          # mbb phi.
+    (0.5,      7.0),          # mbb phi.
+    (0.5,      7.0),          # mbb phi.
 
-    (-0.5,     1.0),          # mbr phi.
-    ( 0.0,     b_2_max),      # mbr b_2.
+    # (-0.5,     1.0),          # br phi.
+    ( 0.0,     b_2_max),      # br b_2.
 
-    (3.0,      4.5),          # mbb phi.
-    (-b_2_max, 0.0),          # mbb b_2.
+    (-1.0,     0.0),          # mbr phi.
+    (0.0,      b_2_max),      # mbr b_2.
 
     (-b_2_max, 0.0),          # qd b_2.
+    (-b_2_max, 0.0),          # mqd b_2.
 
     ( 0.0,     b_2_max),      # uq1 b_2.
     (-b_2_max, 0.0),          # uq2 b_2.
     ( 0.0,     b_2_max),      # uq3 b_2.
     (-b_2_max, 0.0),          # uq4 b_2.
 
-    ( L_min,   0.25),         # ul1 L.
-    ( L_min,   0.3),          # ul2 L.
-    ( L_min,   0.25),         # ul3 L.
-    ( L_min,   0.25)          # ul4 L.
+    ( L_min,   0.5),          # ul1 L.
+    ( L_min,   0.5),          # ul2 L.
+    ( L_min,   0.5),          # ul3 L.
+    ( L_min,   0.5)           # ul4 L.
 ]
 
 opt_super_period(lat, param_list, C, bounds, phi, eps_x, nu, beta, weights, phi)
