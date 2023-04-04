409b4170eff7bebd79e114ef6b8dab3d38c7ecb3
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2023-04-04 18:13:58 +0200

    Non linear kicker development: match calculated data using image currents

--- python/examples/nlk/nlk.py
+++ python/examples/nlk/nlk.py
@@ -6,11 +6,49 @@ import matplotlib.pyplot as plt
 from numpy import fft
 
 
-ref_pos = 16.4e-3/2 + 25e-3j
-t_current = 7e2
+# fmt: off
+ref_pos1 =  8e-3 +  7e-3j
+ref_pos2 = 17e-3 + 15e-3j
+# fmt: on
+ref_pos = 8 + 25e-3j
+t_current = -7e2
+
+# fmt: off
+t_current *= 1 - 1 * 0.14 / 2
+ref_pos1  *= 1 - 0.14
+ref_pos2  *= 1 - 0.14
+# fmt: on
+
+
+def compute_mirror_position_plate(ref_pos, mirror_pos, *, y_plane=True):
+    """ """
+    assert y_plane
+    dy = ref_pos.imag - mirror_pos.imag
+    return ref_pos - 2 * dy * 1j
+
+
+plate_position1 = 5e-3j
+mirror_pos1 = compute_mirror_position_plate(ref_pos1, plate_position1)
+print(f"{ref_pos1*1e3=} {mirror_pos1*1e3}")
+
 
 def plot_field(fignum=10):
-    nlkf = NonlinearKickerField(position=ref_pos, current=t_current)
+    binary = True
+    if binary:
+        inner = (ref_pos1.real, ref_pos1.imag,  t_current)
+        outer = (ref_pos2.real, ref_pos2.imag, -t_current)
+        mirror = (mirror_pos1.real, mirror_pos1.imag, -t_current * 0.14)
+        nlkf = tslib.NonlinearKicker([inner, outer, mirror])
+        nlkf.set_scale(1.0)
+        print("nlkf", repr(nlkf))
+    else:
+        nlkf = NonlinearKickerField(positions=[ref_pos1, ref_pos2], current=t_current * (1-0.18))
+
+    nlkfwm = NonlinearKickerField(
+        positions=[ref_pos1, ref_pos2],
+        current=t_current * (1-0.0),
+        mirror_positions=[mirror_pos1, None],
+    )
     left = AirCoilMagneticField(
         positions=np.array([ref_pos, ref_pos.conjugate()]),
         currents=np.array([-t_current, t_current]),
@@ -24,34 +62,94 @@ def plot_field(fignum=10):
     pos = np.zeros([len(x), 2], dtype=np.float)
     pos[:, 0] = x
 
-    fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, num=fignum)
-    ax_x, ax_y = axes
+    if True:
+        fig, axes = plt.subplots(1, 2, sharex=True, sharey=True, num=fignum)
+        ax_x, ax_y = axes
+    else:
+        fig = None
 
-    B = np.zeros([len(x), 2], dtype=np.float)
+    B = np.zeros([len(x), 2], dtype=float)
+    Bwm = np.zeros([len(x), 2], dtype=float)
     xs = x * 1e3
 
-    [nlkf.field_py(tp, tB) for tp, tB in zip(pos, B)]
-    B = B * 1e3
-    ax_x.plot(xs, B[:, 0], "-", label="nlk")
-    ax_y.plot(xs, B[:, 1], "-", label="nlk")
-    ax_x.set_xlabel("x [mm]")
-    ax_y.set_xlabel("y [mm]")
-    ax_x.set_ylabel("B$_x$ [mT]")
-    ax_y.set_ylabel("B$_y$ [mT]")
+    def evaluate_nlkf(pos, nlk=nlkf):
+        return  nlk.field(pos)
+        # print(r, field)
 
-    B = np.zeros([len(x), 2], dtype=np.float)
-    [left.field_py(tp, tB) for tp, tB in zip(pos, B)]
+    B = np.array([nlkf.field(tp) for tp in pos])
+    [nlkfwm.field_py(tp, tB) for tp, tB in zip(pos, Bwm)]
     B = B * 1e3
+    Bwm = Bwm * 1e3
+    if fig:
+        ax_x.plot(xs, B[:, 0], "b-", label="nlk")
+        ax_y.plot(xs, B[:, 1], "b-", label="nlk")
+        ax_x.plot(xs, Bwm[:, 0], "c-", label="nlk with mirror currents")
+        ax_y.plot(xs, Bwm[:, 1], "c-", label="nlk with mirror currents")
+        ax_x.set_xlabel("x [mm]")
+        ax_y.set_xlabel("x [mm]")
+        ax_x.set_ylabel("B$_x$ [mT]")
+        ax_y.set_ylabel("B$_y$ [mT]")
+
+    Bl = np.zeros([len(x), 2], dtype=np.float)
+    [left.field_py(tp, tB) for tp, tB in zip(pos, Bl)]
+    Bl = Bl * 1e3
     # ax_x.plot(xs, B[:, 0], "--", label="left")
     # ax_y.plot(xs, B[:, 1], "--", label="left")
 
-    B = np.zeros([len(x), 2], dtype=np.float)
-    [right.field_py(tp, tB) for tp, tB in zip(pos, B)]
-    B = B * 1e3
+    Br = np.zeros([len(x), 2], dtype=np.float)
+    [right.field_py(tp, tB) for tp, tB in zip(pos, Br)]
+    Br = Br * 1e3
     # ax_x.plot(xs, B[:, 0], "-.", label="right")
     # ax_y.plot(xs, B[:, 1], "-.", label="right")
 
-    fig.savefig("nlk_field_x.pdf")
+    # Comparison to Marc Dirsat's field
+
+    t, x, By = np.loadtxt("By_750ns_2d.csv", skiprows=1, delimiter=",").T
+    # x in mm need to calculate with it
+    x = x / 1000.0
+    x = x - x.mean()
+
+    # ploting quantities
+    px = x * 1000
+    pBy = By * 1000
+    if fig:
+        ax_y.plot(px, pBy, "g.-")
+
+        fig.savefig("nlk_field_x.pdf")
+
+    fig2, ax = plt.subplots(
+        1, 1, figsize=[8, 6], sharex=True, sharey=True, num=fignum + 1
+    )
+    ax.plot(xs, B[:, 1], "b-", label="nlk")
+    ax.plot(xs, Bwm[:, 1], "c:")
+    ax.plot(px, pBy, "g.-")
+    ax.set_xlabel("x [mm]")
+    ax.set_ylabel("B$_y$ [mT]")
+    ax.set_xlim(-12,12)
+    fig2.savefig("nlk_field_y.pdf")
+
+    # Evaluate field at position for difference
+    pos = np.zeros([len(x), 2], dtype=np.float)
+    B_ref = np.zeros([len(x), 2], dtype=np.float)
+    B_ref_wm = np.zeros([len(x), 2], dtype=np.float)
+    pos[:, 0] = x
+
+    B_ref = np.array([nlkf.field(tp) for tp in pos])
+    # [nlkf.field_py(tp, tB) for tp, tB in zip(pos, B_ref)]
+    [nlkfwm.field_py(tp, tB) for tp, tB in zip(pos, B_ref_wm)]
+    px_ref = pos[:, 0] * 1000
+    pBy_ref = B_ref[:, 1] * 1000
+    pBy_ref_wm = B_ref_wm[:, 1] * 1000
+    if fig:
+        ax_y.plot(px_ref, pBy_ref, "r--")
+        ax_y.plot(px_ref, pBy_ref_wm, "m.--")
+
+    fig3, ax = plt.subplots(1, 1, sharex=True, sharey=True, num=fignum + 2)
+    ax.plot(px_ref, pBy - pBy_ref, 'g-')
+    ax.plot(px_ref, pBy - pBy_ref_wm, 'm--')
+    ax.set_xlabel("x [mm]")
+    ax.set_ylabel("B$_y$ [mT]")
+    ax.set_xlim(-12,12)
 
 
 def plot_field_circle(fignum=None):
@@ -82,7 +180,9 @@ def plot_field_circle(fignum=None):
     fig.savefig("nlk_geometry.pdf")
 
     fignum2 = fignum if fignum is None else fignum + 1
-    fig, axes = plt.subplots(1, 2, num=fignum2, figsize=[8/2, 6/2], sharex=True, sharey=True)
+    fig, axes = plt.subplots(
+        1, 2, num=fignum2, figsize=[8 / 2, 6 / 2], sharex=True, sharey=True
+    )
     ax_x, ax_y = axes
 
     B = np.zeros([len(z), 2], dtype=float)
@@ -145,7 +245,6 @@ def main():
             ps = tslib.ss_vect_tps()
             ps.set_identity()
 
-
         ps[0] = x_off
         print(f"propagate offset x={x_off}\n", ps)
         nlk.propagate(config, ps)
@@ -158,8 +257,9 @@ def main():
 
 
 if __name__ == "__main__":
-    # plot_field()
+    plot_field()
     # plot_field_circle()
     # plt.show()
     # print(tslib)
-    main()
+    # main()
+    plt.show()
