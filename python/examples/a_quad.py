8c56417e6a58666723704cf45a0ecfaf685cd28c
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2023-04-26 17:44:44 +0200

    quadrupole response matrix: calculating phase advance iterating over all quads
    Currrent status
    
    examples a_quad:  shows how to use a knobbable quadrupole

--- python/examples/a_quad.py
+++ python/examples/a_quad.py
@@ -16,7 +16,7 @@ quad = Quadrupole(C)
 
 print(quad)
 print(repr(quad))
-quad.get_multipoles().set_multipole(6, 4.2e-4)
+# quad.get_multipoles().set_multipole(6, 4.2e-4)
 
 # Will fail now: need to implement that the coefficients array can be resized
 # quad.getMultipoles().setMultipole(10, 3.552e-4)
@@ -38,12 +38,11 @@ quadK = tslib.QuadrupoleTpsa(C)
 index_dict = dict(x=0, px=1, y=2, py=3, delta=4, ct=5,  K=6, dx=7, dy=8)
 index_mapping = gtpsa.IndexMapping(index_dict, "quadrupole study")
 
-mo = 4
-desc = gtpsa.desc(len(index_dict), mo)
+mo = 2
+# desc = gtpsa.desc(len(index_dict), mo)
+nv = 6
+desc = gtpsa.desc(nv, mo, len(index_dict) - nv)
 
-# Index mapping should not be necessary
-ps = gtpsa.ss_vect_tpsa(desc, 4, len(index_dict), index_mapping)
-ps.set_identity()
 
 # Make the gradient a knob
 # 1. get the value that was set in the initalisation
@@ -56,15 +55,32 @@ k = k.to_object()
 print("K", k, type(k))
 
 # Make K a knob
-print(gtpsa.ctpsa.__init__.__doc__)
+# print(gtpsa.ctpsa.__init__.__doc__)
 b2 = gtpsa.ctpsa(desc, mo, mapping=index_mapping)
-b2.set_variable(k + 0j, "K", 0e0j)
-b2.print("K")
+dx = gtpsa.tpsa(desc, mo, mapping=index_mapping)
+dy = gtpsa.tpsa(desc, mo, mapping=index_mapping)
+if False:
+    b2.set_variable(k + 0j, "K")
+    dx.set_variable(0, "dx")
+    dy.set_variable(0, "dy")
+else:
+    b2.set_knob(k + 0j, "K")
+    dx.set_knob(0, "dx")
+    dy.set_knob(0, "dy")
+
+# b2.print("K")
 
 # this wrapping needs to be avoided ... but not now
 # just requires a bit of rework of the c++ wrapper
-tmp = gtpsa.CTpsaOrComplex(b2)
-quadK.set_main_multipole_strength(tmp)
+# tmp = gtpsa.CTpsaOrComplex(b2)
+# quadK.set_main_multipole_strength(tmp)
+quadK.set_dx(gtpsa.TpsaOrDouble(dx))
+quadK.set_dy(gtpsa.TpsaOrDouble(dy))
+
+
+# Index mapping should not be necessary
+ps = gtpsa.ss_vect_tpsa(desc, mo, nv, index_mapping)
+ps.set_identity()
 
 quadK.propagate(calc_config, ps)
 print(ps)
@@ -78,6 +94,6 @@ print("coeffs\n", pd.DataFrame(coeffs).set_index("index"))
 
 ttmp = ps.x
 print("ttmp", type(ttmp))
-print(ttmp.get(dict(x=3)))
+#print(ttmp.get(dict(x=3)))
 
 print(ps.jacobian())
--- python/examples/use_cases/quadrupole_response_matrix.py
+++ python/examples/use_cases/quadrupole_response_matrix.py
@@ -1,5 +1,8 @@
 # Find the lattice file
 from pathlib import Path
+
+import tqdm
+
 from thor_scsi.factory import accelerator_from_config
 import thor_scsi.lib as tslib
 from thor_scsi.lib import Quadrupole, QuadrupoleTpsa, AcceleratorTpsa, ConfigType
@@ -17,8 +20,11 @@ from dataclasses import dataclass
 named_index_d = dict(x=0, px=1, y=2, py=3, delta=4, ct=5, K=6, dx=7, dy=8)
 named_index = gtpsa.IndexMapping(named_index_d)
 # default_desc = gtpsa.desc(nv=6+3, mo=3)
-mo = 1
-default_desc = gtpsa.desc(nv=6, mo=mo, np=3, po=0)
+# maximum order of variables
+mo = 3
+# maximum order of parameters ... at least one
+po = 2
+default_desc = gtpsa.desc(nv=6, mo=mo, np=3, po=po)
 # default_desc = gtpsa.desc(nv=6, mo=3, np=144*3, po=1)
 
 t_file = (
@@ -52,7 +58,6 @@ def lattice_tpsa(acc):
 def make_quad_knobbable(quad, *, desc=default_desc, named_index=named_index):
     # Augment the quadrupole
     k_orig = quad.get_main_multipole_strength().to_object()
-    po = 0
     k = gtpsa.ctpsa(desc, po, mapping=named_index)
     dx = gtpsa.tpsa(desc, po, mapping=named_index)
     dy = gtpsa.tpsa(desc, po, mapping=named_index)
@@ -80,8 +85,7 @@ def make_quad_knobbable(quad, *, desc=default_desc, named_index=named_index):
 
 
 def make_quad_unknobbable(quad):
-    """Replace knobbed variables with standard values
-    """
+    """Replace knobbed variables with standard values"""
     dx = float(quad.get_dx().to_object().get())
     dy = float(quad.get_dy().to_object().get())
     quad.set_dx(gtpsa.TpsaOrDouble(dx))
@@ -90,18 +94,6 @@ def make_quad_unknobbable(quad):
     quad.get_multipoles().set_multipole(2, gtpsa.CTpsaOrComplex(mul))
     return quad
 
-def tpsa2df(a_tpsa: gtpsa.tpsa) -> pd.DataFrame:
-    coeffs = var.get_coefficients()
-    keys = named_index_d.keys()
-    nrec = gtpsa.utils.tpsa_coefficients_to_nrec(coeffs, keys)
-    df = pd.DataFrame(nrec).set_index("index")
-
-    # add order column
-    order_columns = [item for item in df.columns if item != "value"]
-    order = np.sum(df.loc[:, order_columns], axis=1)
-    ndf = df.reindex(columns=list(df.columns) + ["order"])
-    ndf.loc[:, "order"] = order
-    return ndf
 
 
 def response_of_one_quad(
@@ -193,22 +185,25 @@ observers = instrument_with_standard_observers(bpm_elems, mapping=named_index)
 
 def combine_responses(observers: Sequence[tslib.StandardObserver]):
     def extract_x_y(ob):
-        t_tpsa = ob.get_truncated_power_series_a()
-        x_dx = t_tpsa.x.get(dx=1)
-        y_dy = t_tpsa.x.get(dy=1)
-        x_dy = t_tpsa.x.get(dy=1)
-        y_dx = t_tpsa.x.get(dx=1)
+        ps = ob.get_truncated_power_series_a()
+        data = [
+            (ps.x.get(d), ps.y.get(d))
+            for d in (dict(K=1), dict(K=2), dict(dx=1), dict(dy=1))
+        ]
 
         ob_name = ob.get_observed_name()
         da = xr.DataArray(
-            data=[[(x_dx, x_dy), (y_dx, y_dy)]],
-            dims=["bpm", "plane", "dep"],
-            coords=[[ob_name], ["x", "y"], ["dx", "dy"]],
-            name="effect",
+            data=[data],
+            dims=["bpm", "dep", "plane"],
+            coords=[
+                [ob_name],
+                ["K", "K2", "dx", "dy"],
+                ["x", "y"],
+            ],
+            name="bpm_observation",
         )
 
         idx = ob.get_observed_index()
-        data = [idx]
         index = xr.DataArray(name="index", data=[idx], dims=["bpm"], coords=[[ob_name]])
         ds = xr.Dataset(dict(index=index, effect=da))
         return ds
@@ -218,7 +213,119 @@ def combine_responses(observers: Sequence[tslib.StandardObserver]):
     return ds
 
 
+def abs(v: gtpsa.tpsa):
+    if v.get() < 0e0:
+        return v * -1
+    return v
+
+
+def acos2(sin, cos):
+    """arcos(phi): 0 - 2 * pi.
+    The sin part is used to determine the quadrant.
+    """
+    if abs(cos).get() > 1e0:
+        raise ValueError(f"arg = {cos:22.15e} > 1e0")
+    phi = gtpsa.acos(cos)
+    if sin.get() < 0e0:
+        phi = 2e0 * np.pi - phi
+    return phi
+
+
+def compute_phase_advance(
+    ps: gtpsa.ss_vect_tpsa, canonical_coordinates_names: Sequence[str]
+) -> gtpsa.tpsa:
+    """compute phase advance
+
+    Args:
+        ps: phase space
+        canonical_coordinates_names: name of the two coordinates
+
+    Returns:
+        phase advance as gtpsa.tpsa object
+    """
+    # names of the 2 coordinates
+    c, d = canonical_coordinates_names
+    # compute the trace
+    tr = ps.loc[c].deriv(c) + ps.loc[d].deriv(d)
+    tr_cst = tr.get()
+    if not tr_cst < 2.0:
+        raise ValueError(f"Trace not stable: trace = {tr}")
+    if True:
+        # a bit a lot of calculation just to find the quadrant
+        s2 = ps.loc[c].deriv(d) * ps.loc[d].deriv(c)
+        sgn = s2.get() > 0e0 or -1e0
+        sin = gtpsa.sqrt(abs(s2))
+        phase_advance = acos2(sgn * sin, tr / 2.0) / (2 * np.pi)
+    else:
+        # ignoring quadrant
+        phase_advance = gtpsa.acos(tr / 2.0) / (2 * np.pi)
+    return phase_advance
+
+
 calc_config = ConfigType()
 acc_tpsa.propagate(calc_config, ps3)
+compute_phase_advance(ps3, ["x", "px"])
 response_one_quad = combine_responses(observers)
-print(response_one_quad)
+print(response_one_quad.effect.sel(plane="x", dep="dx"))
+
+all_responses = []
+
+
+def process_one_quad(quad):
+    nps = response_of_one_quad(acc_tpsa, quad)
+    acc_tpsa.propagate(calc_config, nps)
+    response_one_quad = combine_responses(observers)
+    response_one_quad["name"] = quad.name
+    # list comprehension as currently iloc not active?
+    ps = [
+        np.array(
+            [
+                nps.x.get(d),
+                nps.px.get(d),
+                nps.y.get(d),
+                nps.py.get(d),
+                nps.delta.get(d),
+                nps.ct.get(d),
+            ]
+        )
+        for d in [dict(K=1), dict(K=2), dict(dx=1), dict(dy=1)]
+    ]
+    phase_space = xr.DataArray(
+        data=ps,
+        name="phase_space",
+        dims=["dep", "coor"],
+        coords=[["K", "K2", "dx", "dy"], ["x", "px", "y", "py", "delta", "ct"]],
+    )
+    response_one_quad["phase_space"] = phase_space
+
+    pa_x = compute_phase_advance(nps, ["x", "px"])
+    pa_y = compute_phase_advance(nps, ["y", "py"])
+
+    data = [
+        np.array(
+            [
+                pa_x.get(d),
+                pa_y.get(d),
+            ]
+        )
+        for d in [dict(K=1), dict(K=2), dict(dx=1), dict(dy=1)]
+    ]
+    phase_advance = xr.DataArray(
+        data=data,
+        dims=["dep", "plane"],
+        coords=[["K", "K2", "dx", "dy"], ["x", "y"]],
+    )
+
+    response_one_quad["phase_advance"] = phase_advance
+
+    return response_one_quad
+
+
+all_responses = {
+    elem.name: process_one_quad(elem)
+    for elem in tqdm.tqdm(acc_tpsa, total=len(acc_tpsa))
+    if isinstance(elem, QuadrupoleTpsa)
+}
+tmp = [(key, item) for key, item in all_responses.items()]
+db = xr.concat([t[1] for t in tmp], pd.Index([t[0] for t in tmp], name="quadrupole"))
+db.to_netcdf("quadrupole_response_matrix2.nc")
