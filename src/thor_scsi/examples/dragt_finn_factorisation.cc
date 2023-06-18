a60278e6089457204c7f40d6372435f5531eae11
Johan <jbngtssn@gmail.com>
2023-06-19 00:54:51 +0200

    Implemementation of Dragt-Finn factorisation.

--- src/thor_scsi/examples/dragt_finn_factorisation.cc
+++ src/thor_scsi/examples/dragt_finn_factorisation.cc
@@ -8,31 +8,30 @@ namespace tse = thor_scsi::elements;
 
 int main(int argc, char *argv[])
 {
-	const int mo = 4, nv = 6;
-	auto desc = std::make_shared<gtpsa::desc>(nv, mo);
-	//const auto t =
-	Config C;
-	C.set<std::string>("name", "test");
-	C.set<double>("K", 0.0);
-	C.set<double>("L", 0e0);
-	C.set<double>("N", 1);
+  const int mo = 4, nv = 6;
+  auto desc = std::make_shared<gtpsa::desc>(nv, mo);
+  //const auto t =
+  Config C;
+  C.set<std::string>("name", "test");
+  C.set<double>("K", 1e0);
+  C.set<double>("L", 0e0);
+  C.set<double>("N", 1);
 
-	tsc::ConfigType calc_config;
-	auto sext = tse::SextupoleType(C);
+  tsc::ConfigType calc_config;
+  auto sext = tse::SextupoleType(C);
 
-	auto ps = gtpsa::ss_vect<gtpsa::tpsa>(desc, 4);
-	ps.set_identity();
-	sext.propagate(calc_config, ps);
-
-
-	std::cout << "after sextupole" << ps << std::endl;
-
-	for (int i = 0; i < ps.size(); i++) {
-	  ps[i].print();
-	}
-
-	auto lie_factors =  gtpsa::M_to_h_DF(ps);
-	std::cout << "lie factors" << lie_factors << std::endl;
+  auto ps = gtpsa::ss_vect<gtpsa::tpsa>(desc, 4);
+  ps.set_identity();
+  sext.propagate(calc_config, ps);
 
 
+  std::cout << "after sextupole\n" << ps << std::endl;
+
+  for (int i = 0; i < ps.size(); i++) {
+    ps[i].print();
+  }
+
+  auto lie_factors = gtpsa::M_to_h_DF(ps);
+  std::cout << "lie factors\n" << lie_factors << std::endl;
+  lie_factors.print();
 }
