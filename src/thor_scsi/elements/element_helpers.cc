37d992a824f7cddcd88af8f2c5b34034ae4f1f19
Johan <jbngtssn@gmail.com>
2023-06-24 14:13:48 +0200

    Minor editing to improve code readability.

--- src/thor_scsi/elements/element_helpers.cc
+++ src/thor_scsi/elements/element_helpers.cc
@@ -14,230 +14,251 @@ namespace tsc = thor_scsi::core;
 double tse::get_psi(const double irho, const double phi, const double gap)
 {
 
-	double psi;
-	const double phir = degtorad(phi);
+  double psi;
+  const double phir = degtorad(phi);
 
-	const double k1 = 0.5e0, k2 = 0e0;
+  const double k1 = 0.5e0, k2 = 0e0;
 
-	/* replace with local approximation */
-	if (phi == 0e0){
-		psi = 0e0;
-	} else {
-		psi = k1*gap*irho*(1e0+sqr(sin(phir)))/cos(phir)
-			*(1e0 - k2*gap*irho*tan(phir));
-	}
-	return psi;
+  /* replace with local approximation */
+  if (phi == 0e0){
+    psi = 0e0;
+  } else {
+    psi = k1*gap*irho*(1e0+sqr(sin(phir)))/cos(phir)
+      *(1e0 - k2*gap*irho*tan(phir));
+  }
+  return psi;
 }
 
 
 
 namespace thor_scsi::elements{
-	template<typename T>
-	void get_twoJ(const int n_DOF, const gtpsa::ss_vect<double> &ps, const gtpsa::ss_vect<T> &A,
-		      double twoJ[])
-	{
-		int             j, no;
-		long int        jj[ps_dim];
-		const double unused=0e0;
-		gtpsa::ss_vect<double> z(unused);
+  template<typename T>
+  void get_twoJ
+  (const int n_DOF, const gtpsa::ss_vect<double> &ps,
+   const gtpsa::ss_vect<T> &A, double twoJ[])
+  {
+    int             j, no;
+    long int        jj[ps_dim];
+    const double unused=0e0;
+    gtpsa::ss_vect<double> z(unused);
 
-		//throw std::runtime_error("get_twoJ needs to be ported");
-		// no = no_tps;
-		// sets the truncation order of calculation
-		// danot_(1);
+    //throw std::runtime_error("get_twoJ needs to be ported");
+    // no = no_tps;
+    // sets the truncation order of calculation
+    // danot_(1);
 
-		for (j = 0; j < ps_dim; j++)
-			jj[j] = (j < 2*n_DOF)? 1 : 0;
+    for (j = 0; j < ps_dim; j++)
+      jj[j] = (j < 2*n_DOF)? 1 : 0;
 
-		// inspect if mad_tpsa_pminv can be used?
-		// z = (PInv(A, jj)*ps).cst();
+    // inspect if mad_tpsa_pminv can be used?
+    // z = (PInv(A, jj)*ps).cst();
 
-		for (j = 0; j < n_DOF; j++)
-			twoJ[j] = sqr(z[2*j]) + sqr(z[2*j+1]);
+    for (j = 0; j < n_DOF; j++)
+      twoJ[j] = sqr(z[2*j]) + sqr(z[2*j+1]);
 
-		// danot_(no);
-	}
+    // danot_(no);
+  }
 
-	template<typename T>
-	double get_curly_H(const gtpsa::ss_vect<T> &A){
-		int             j;
-		double          curly_H[2], unused=0e0;
-		gtpsa::ss_vect<double> eta(unused);
+  template<typename T>
+  double get_curly_H(const gtpsa::ss_vect<T> &A){
+    int             j;
+    double          curly_H[2], unused=0e0;
+    gtpsa::ss_vect<double> eta(unused);
 
-		arma::mat jac = A.jacobian();
-		eta.set_zero();
-		for (j = 0; j < 4; j++)
-			eta[j] = jac(j, delta_);
+    arma::mat jac = A.jacobian();
+    eta.set_zero();
+    for (j = 0; j < 4; j++)
+      eta[j] = jac(j, delta_);
 
-		get_twoJ(2, eta, A, curly_H);
+    get_twoJ(2, eta, A, curly_H);
 
-		return curly_H[X_];
-	}
+    return curly_H[X_];
+  }
 
-	double get_curly_H(const gtpsa::ss_vect<tps> &A)
-	{
+  double get_curly_H(const gtpsa::ss_vect<tps> &A)
+  {
 
-		int             j;
-		double          curly_H[2], unused=0e0;
-		gtpsa::ss_vect<double> eta(unused);
+    int             j;
+    double          curly_H[2], unused=0e0;
+    gtpsa::ss_vect<double> eta(unused);
 
-		eta.set_zero();
-		for (j = 0; j < 4; j++)
-			eta[j] = A[j][delta_];
+    eta.set_zero();
+    for (j = 0; j < 4; j++)
+      eta[j] = A[j][delta_];
 
-		get_twoJ(2, eta, A, curly_H);
+    get_twoJ(2, eta, A, curly_H);
 
-		return curly_H[X_];
-	}
+    return curly_H[X_];
+  }
 
-	double get_curly_H(const gtpsa::ss_vect<double> &x){
-		// THOR_SCSI_LOG(THOR_SCSI_WARN) << "get_curly_H: operation not defined for double\n";
-		throw std::domain_error("get_curly_H: operation not defined for double");
-		return 0e0;
-	}
-
-
-	template<typename T, typename T2>
-	void drift_propagate(const tsc::ConfigType &conf, const T2& L, gtpsa::ss_vect<T> &ps)
-	{
-	        T u(ps[0]);
-
-		if (!conf.H_exact) {
-			// Small angle axproximation.
-			u = L/(1e0+ps[delta_]);
-			ps[x_]  += u*ps[px_];
-			ps[y_]  += u*ps[py_];
-			ps[ct_] += u*(sqr(ps[px_])+sqr(ps[py_]))/(2e0*(1e0+ps[delta_]));
-		} else {
-			u = L/tse::get_p_s(conf, ps);
-			ps[x_]  += u*ps[px_]; ps[y_] += u*ps[py_];
-			ps[ct_] += u*(1e0+ps[delta_]) - L;
-		}
-		if (conf.pathlength){
-			ps[ct_] += L;
-		}
-	}
-
-	/**
-	 *
-	 * The vector potential for the combined-function sector bend is from:
-	 *  C. Iselin "Lie Transformations and Transport Equations for Combined-
-	 * Function Dipoles" Part. Accel. 17, 143-155 (1985).
-	 *
-	 * Args:
-	 *   order: optimisation for number of coefficients that require to be evaulatued
-	 *          should go to field interpolation
-	 *
-	 *   h_bend: 1/rho_bend (radius of the the bend, magnetic rigidty of the dipoles)
-	 *   h_ref: $1 / \rho$ for the reference curve / for the comoving frame (curvature of the design or reference orbit)
-	 *
-	 *   ps0 : initial condistins (when start propagation trough the element)
-	 *   ps  : final condistions
-	 *
-	 * Todo:
-	 *     Review kick and radiate code organisation ....
-	 *
-	 *     Split up function in different parts or functions
-	 *     E.g.: one for dipoles and one for anything else...
-	 */
-	template<typename T>
-	void thin_kick(const tsc::ConfigType &conf, const T BxoBrho, const T ByoBrho,
-			    const double L, const double h_bend, const double h_ref,
-			    const gtpsa::ss_vect<T> &ps0,  gtpsa::ss_vect<T> &ps)
-	{
-		int        j;
-		// T          BxoBrho, ByoBrho, ByoBrho1, B[3],
-		T u(ps0[0]), p_s(ps[0]);
+  double get_curly_H(const gtpsa::ss_vect<double> &x){
+    // THOR_SCSI_LOG(THOR_SCSI_WARN) << "get_curly_H:
+    // operation not defined for double\n";
+    throw std::domain_error("get_curly_H: operation not defined for double");
+    return 0e0;
+  }
 
 
-		const int debug = false;
+  template<typename T, typename T2>
+  void drift_propagate
+  (const tsc::ConfigType &conf, const T2& L, gtpsa::ss_vect<T> &ps)
+  {
+    T u(ps[0]);
 
-		// remains of calculation optimisation
-		const int Order = 1;
+    if (!conf.H_exact) {
+      // Small angle axproximation.
+      u = L/(1e0+ps[delta_]);
+      ps[x_]  += u*ps[px_];
+      ps[y_]  += u*ps[py_];
+      ps[ct_] += u*(sqr(ps[px_])+sqr(ps[py_]))/(2e0*(1e0+ps[delta_]));
+    } else {
+      u = L/tse::get_p_s(conf, ps);
+      ps[x_]  += u*ps[px_]; ps[y_] += u*ps[py_];
+      ps[ct_] += u*(1e0+ps[delta_]) - L;
+    }
+    if (conf.pathlength){
+      ps[ct_] += L;
+    }
+  }
 
-		if(debug){
-		    std::cerr << "Length " << L << " Order " << Order << " h_bend " << h_bend << " h_ref " << h_ref << std::endl
-			      << "ps in " << ps;
-		}
-		// Todo: what kind of bend is that?
-		if ((h_bend != 0e0) || ((1 <= Order) //... needs to be removed
+  /**
+   *
+   * The vector potential for the combined-function sector bend is from:
+   *  C. Iselin "Lie Transformations and Transport Equations for Combined-
+   * Function Dipoles" Part. Accel. 17, 143-155 (1985).
+   *
+   * Args:
+   *   order: optimisation for number of coefficients that require to be
+   *           evaulatued
+   *          should go to field interpolation
+   *
+   *   h_bend: 1/rho_bend (radius of the the bend, magnetic rigidty of the
+   *                       dipoles)
+   *   h_ref: $1 / \rho$ for the reference curve
+   *   / for the comoving frame (curvature of the design or reference orbit)
+   *
+   *   ps0 : initial condistins (when start propagation trough the element)
+   *   ps  : final condistions
+   *
+   * Todo:
+   *     Review kick and radiate code organisation ....
+   *
+   *     Split up function in different parts or functions
+   *     E.g.: one for dipoles and one for anything else...
+   */
+  template<typename T>
+  void thin_kick(const tsc::ConfigType &conf, const T BxoBrho, const T ByoBrho,
+		 const double L, const double h_bend, const double h_ref,
+		 const gtpsa::ss_vect<T> &ps0,  gtpsa::ss_vect<T> &ps)
+  {
+    int        j;
+    // T          BxoBrho, ByoBrho, ByoBrho1, B[3],
+    T u(ps0[0]), p_s(ps[0]);
+
+
+    const int debug = false;
+
+    // remains of calculation optimisation
+    const int Order = 1;
+
+    if(debug){
+      std::cerr << "Length " << L << " Order " << Order << " h_bend "
+		<< h_bend << " h_ref " << h_ref << std::endl << "ps in " << ps;
+    }
+    // Todo: what kind of bend is that?
+    if ((h_bend != 0e0) || ((1 <= Order) //... needs to be removed
 #if 0
-					//  optimisation of number
-		    && (Order <= HOMmax)
+			    //  optimisation of number
+			    && (Order <= HOMmax)
 #endif
 			    )) {
-			{
-				// Warning: machinery there for non double
-				// Need to understand
-				if(debug){
-					std::cerr << "interpolated field " << BxoBrho << ", "  << ByoBrho << std::endl;
-				}
+      {
+	// Warning: machinery there for non double
+	// Need to understand
+	if(debug){
+	  std::cerr << "interpolated field " << BxoBrho << ", "  << ByoBrho
+		    << std::endl;
+	}
 
-			}
+      }
 
-			if (h_ref != 0e0) {
-				// Sector bend.
-				if (true) {
-					// std::cerr << "h_bend - h_ref" << h_bend - h_ref << std::endl;
-					ps[px_] -= L*(ByoBrho+(h_bend-h_ref)/2e0+h_ref*h_bend*ps0[x_]
-						      -h_ref*ps0[delta_]);
-					ps[ct_] += L*h_ref*ps0[x_];
-					// std::cerr << "h_ref ps[px_]" <<  ps[px_] << " ps[ct_] " << ps[ct_]
-					//	  << "ps0[delta_] " << ps0[delta_] << std::endl;
-				} else {
-				        /*
-					 * Code idea  following Teapot symplectic integrator
-					 *
-					 * Currently not used ....
-					 */
+      if (h_ref != 0e0) {
+	// Sector bend.
+	if (true) {
+	  // std::cerr << "h_bend - h_ref" << h_bend - h_ref << std::endl;
+	  ps[px_] -= L*(ByoBrho+(h_bend-h_ref)/2e0+h_ref*h_bend*ps0[x_]
+			-h_ref*ps0[delta_]);
+	  ps[ct_] += L*h_ref*ps0[x_];
+	  // std::cerr << "h_ref ps[px_]" <<  ps[px_] << " ps[ct_] " << ps[ct_]
+	  //	  << "ps0[delta_] " << ps0[delta_] << std::endl;
+	} else {
+	  /*
+	   * Code idea  following Teapot symplectic integrator
+	   *
+	   * Currently not used ....
+	   */
 
-					// The Hamiltonian is split into: H_d + H_k; with [H_d, H_d] = 0.
-					p_s = get_p_s(conf, ps0); u = L*h_ref*ps0[x_]/p_s;
-					ps[x_]  += u*ps0[px_];
-					ps[y_]  += u*ps0[py_];
-					ps[ct_] += u*(1e0+ps0[delta_]);
-					// ps[px_] -= L*(h_bend*(1e0+h_ref*ps0[x_])-h_ref*p_s);
+	  // The Hamiltonian is split into: H_d + H_k; with [H_d, H_d] = 0.
+	  p_s = get_p_s(conf, ps0); u = L*h_ref*ps0[x_]/p_s;
+	  ps[x_]  += u*ps0[px_];
+	  ps[y_]  += u*ps0[py_];
+	  ps[ct_] += u*(1e0+ps0[delta_]);
+	  // ps[px_] -= L*(h_bend*(1e0+h_ref*ps0[x_])-h_ref*p_s);
 
 #if 1
-					// #warning "field interpolation for sector bends missing"
+	  // #warning "field interpolation for sector bends missing"
 #else
-					// Field expansion up to sextupole like terms.
-					//const double B2 = MB[Quad+HOMmax];
-					// need to understand it
-					T Gy, Gx;
-					intp.gradient(ps[x_], ps[y_], &Gx, &Gy);
-					ByoBrho += h_bend - Gy *h_ref*sqr(ps0[y_])/2e0;
-					ps[px_] -= L*((1e0+h_ref*ps0[x_])*ByoBrho-h_ref*p_s);
-					ps[py_] += L*(1e0+h_ref*ps0[x_])*BxoBrho;
+	  // Field expansion up to sextupole like terms.
+	  //const double B2 = MB[Quad+HOMmax];
+	  // need to understand it
+	  T Gy, Gx;
+	  intp.gradient(ps[x_], ps[y_], &Gx, &Gy);
+	  ByoBrho += h_bend - Gy *h_ref*sqr(ps0[y_])/2e0;
+	  ps[px_] -= L*((1e0+h_ref*ps0[x_])*ByoBrho-h_ref*p_s);
+	  ps[py_] += L*(1e0+h_ref*ps0[x_])*BxoBrho;
 #endif
-				}
-			} else {
-				// std::cerr << "Calculating horizontal thin kick: By"  << ByoBrho <<  " h_bend=1/rho_bend " << h_bend;
-				// Cartesian bend.
-				ps[px_] -= L*(h_bend+ByoBrho);
-				// std::cerr << "ps[px_] " << ps[px_]  << std::endl;
-
-			}
-			ps[py_] += L*BxoBrho;
-		}
 	}
+      } else {
+	// std::cerr << "Calculating horizontal thin kick: By"  << ByoBrho <<  " h_bend=1/rho_bend " << h_bend;
+	// Cartesian bend.
+	ps[px_] -= L*(h_bend+ByoBrho);
+	// std::cerr << "ps[px_] " << ps[px_]  << std::endl;
+
+      }
+      ps[py_] += L*BxoBrho;
+    }
+  }
 
 }
 
-template void tse::drift_propagate(const tsc::ConfigType &conf, const double&, gtpsa::ss_vect<double>      &);
-template void tse::drift_propagate(const tsc::ConfigType &conf, const double&, gtpsa::ss_vect<tps>         &);
-template void tse::drift_propagate(const tsc::ConfigType &conf, const double&, gtpsa::ss_vect<gtpsa::tpsa> &);
+template void tse::drift_propagate
+(const tsc::ConfigType &conf, const double&, gtpsa::ss_vect<double>      &);
+template void tse::drift_propagate
+(const tsc::ConfigType &conf, const double&, gtpsa::ss_vect<tps>         &);
+template void tse::drift_propagate
+(const tsc::ConfigType &conf, const double&, gtpsa::ss_vect<gtpsa::tpsa> &);
 
 
-template void tse::thin_kick(const tsc::ConfigType &conf, const double       BxoBrho, const double     ByoBrho,
-			     const double L, const double h_bend, const double h_ref, const gtpsa::ss_vect<double>      &ps0, gtpsa::ss_vect<double>      &ps);
-template void tse::thin_kick(const tsc::ConfigType &conf, const tps          BxoBrho, const tps        ByoBrho,
-			     const double L, const double h_bend, const double h_ref, const gtpsa::ss_vect<tps>         &ps0, gtpsa::ss_vect<tps>         &ps);
-template void tse::thin_kick(const tsc::ConfigType &conf, const gtpsa::tpsa BxoBrho, const gtpsa::tpsa ByoBrho,
-			     const double L, const double h_bend, const double h_ref, const gtpsa::ss_vect<gtpsa::tpsa> &ps0, gtpsa::ss_vect<gtpsa::tpsa> &ps);
+template void tse::thin_kick
+(const tsc::ConfigType &conf, const double BxoBrho, const double ByoBrho,
+ const double L, const double h_bend, const double h_ref,
+ const gtpsa::ss_vect<double> &ps0, gtpsa::ss_vect<double> &ps);
+template void tse::thin_kick
+(const tsc::ConfigType &conf, const tps BxoBrho, const tps ByoBrho,
+ const double L, const double h_bend, const double h_ref,
+ const gtpsa::ss_vect<tps> &ps0, gtpsa::ss_vect<tps> &ps);
+template void tse::thin_kick
+(const tsc::ConfigType &conf, const gtpsa::tpsa BxoBrho,
+ const gtpsa::tpsa ByoBrho, const double L, const double h_bend,
+ const double h_ref, const gtpsa::ss_vect<gtpsa::tpsa> &ps0,
+ gtpsa::ss_vect<gtpsa::tpsa> &ps);
 
-template void tse::get_twoJ(const int n_DOF, const gtpsa::ss_vect<double> &ps, const gtpsa::ss_vect<gtpsa::tpsa> &A, double twoJ[]);
-template void tse::get_twoJ(const int n_DOF, const gtpsa::ss_vect<double> &ps, const gtpsa::ss_vect<tps>         &A, double twoJ[]);
+template void tse::get_twoJ
+(const int n_DOF, const gtpsa::ss_vect<double> &ps,
+ const gtpsa::ss_vect<gtpsa::tpsa> &A, double twoJ[]);
+template void tse::get_twoJ
+(const int n_DOF, const gtpsa::ss_vect<double> &ps,
+ const gtpsa::ss_vect<tps> &A, double twoJ[]);
 
 template double tse::get_curly_H(const gtpsa::ss_vect<gtpsa::tpsa> &A);
 template double tse::get_curly_H(const gtpsa::ss_vect<tps>         &A);
--- src/thor_scsi/elements/element_helpers.h
+++ src/thor_scsi/elements/element_helpers.h
@@ -5,7 +5,7 @@
    Contains functions not part of the global API but rather
    code common to the elemetns.
 
- */
+*/
 #include <vector>
 #include <string>
 // #include <tps/ss_vect.h>
@@ -23,7 +23,8 @@
 // #include <thor_scsi/process/t2ring_common.h>
 
 
-extern double q_fluct; /// < Track down if it is a constant or a global variable reflecting current status
+extern double q_fluct; /* < Track down if it is a constant or a global
+			  variable reflecting current status                  */
 
 namespace thor_scsi::elements {
 
@@ -78,9 +79,9 @@ namespace thor_scsi::elements {
  * \endverbatim
  *
  */
-template<typename T>
-void get_twoJ(const int n_DOF, const gtpsa::ss_vect<double> &ps,
-	      const gtpsa::ss_vect<T> &A, double twoJ[]);
+	template<typename T>
+	void get_twoJ(const int n_DOF, const gtpsa::ss_vect<double> &ps,
+		      const gtpsa::ss_vect<T> &A, double twoJ[]);
 
 /**
  *
@@ -96,25 +97,29 @@ void get_twoJ(const int n_DOF, const gtpsa::ss_vect<double> &ps,
 /**
  *  @brief Compute longitudinal momentum
  */
-template<typename T>
-inline T get_p_s(const thor_scsi::core::ConfigType &conf, const gtpsa::ss_vect<T> &ps)
-{
-	T p_s(ps[0]), p_s2(ps[0]);
+	template<typename T>
+	inline T get_p_s
+	(const thor_scsi::core::ConfigType &conf, const gtpsa::ss_vect<T> &ps)
+	{
+		T p_s(ps[0]), p_s2(ps[0]);
 
-	if (!conf.H_exact) {
-		// Small angle axproximation.
-		p_s = 1e0 + ps[delta_];
-	} else {
-		p_s2 = sqr(1e0+ps[delta_]) - sqr(ps[px_]) - sqr(ps[py_]);
-		if (p_s2 >= 0e0){
-			p_s = sqrt(p_s2);
-		}  else {
-			throw PhysicsViolation("Speed of light exceeded");
-			p_s = NAN;
+		if (!conf.H_exact) {
+			// Small angle axproximation.
+			p_s = 1e0 + ps[delta_];
+		} else {
+			p_s2 =
+				sqr(1e0+ps[delta_]) - sqr(ps[px_])
+				- sqr(ps[py_]);
+			if (p_s2 >= 0e0){
+				p_s = sqrt(p_s2);
+			}  else {
+				throw PhysicsViolation
+					("Speed of light exceeded");
+				p_s = NAN;
+			}
 		}
+		return(p_s);
 	}
-	return(p_s);
-}
 
 
 	template<typename T>
@@ -136,143 +141,162 @@ inline T get_p_s(const thor_scsi::core::ConfigType &conf, const gtpsa::ss_vect<T
 	}
 
 	static inline double get_dI_eta(const gtpsa::ss_vect<double> &A){
-		std::cout << "get_dI_eta: operation not defined for double" << std::endl;
-		throw std::domain_error("get_dI_eta: operation not defined for tps");
+		std::cout << "get_dI_eta: operation not defined for double"
+			  << std::endl;
+		throw std::domain_error
+			("get_dI_eta: operation not defined for tps");
 		return 0e0;
 	}
 
 // partial template-class specialization
 // primary version
-template<typename T>
-class is_tps { };
+	template<typename T>
+	class is_tps { };
 
 /**
  * partial specialization
  *
  * Todo: check if moved to oberver
  */
-template<>
-class is_tps<double> {
-public:
-	/**
-	 *  @brief Compute phase space
-	 */
-	/*
-	static inline void get_ps(const ss_vect<double> &x, thor_scsi::core::CellType *Cell)
-		{ Cell->BeamPos = pstostl(x); }
-	*/
-	static inline double set_prm(const int k) { return 1e0; }
+	template<>
+	class is_tps<double> {
+	public:
+		/**
+		 *  @brief Compute phase space
+		 */
+		/*
+		static inline void get_ps
+		(const ss_vect<double> &x, thor_scsi::core::CellType *Cell)
+			{ Cell->BeamPos = pstostl(x); }
+		*/
+		static inline double set_prm(const int k) { return 1e0; }
 
-	/**
-	 * @brief Compute (linear) dispersion action
-	 */
-	/**
-	 * @brief Synchrotron integral
-	 *
-	 * Todo: Check which one
-	 */
-	static inline double get_dI_eta(const gtpsa::ss_vect<tps> &A){
-		std::cout << "get_dI_eta: operation not defined for double" << std::endl;
-		throw std::domain_error("get_dI_eta: operation not defined for tps");
-		return 0e0;
-	}
-	static inline double get_dI_eta(const gtpsa::ss_vect<gtpsa::tpsa> &A){
-		std::cout << "get_dI_eta: operation not defined for gtpsa::tpsa" << std::endl;
-		throw std::domain_error("get_dI_eta: operation not defined for gtpsa::tpsa");
-		return 0e0;
-	}
+		/**
+		 * @brief Compute (linear) dispersion action
+		 */
+		/**
+		 * @brief Synchrotron integral
+		 *
+		 * Todo: Check which one
+		 */
+		static inline double get_dI_eta(const gtpsa::ss_vect<tps> &A){
+			std::cout << "get_dI_eta: operation not defined for"
+				" double" << std::endl;
+			throw std::domain_error
+				("get_dI_eta: operation not defined for tps");
+			return 0e0;
+		}
+		static inline double get_dI_eta
+		(const gtpsa::ss_vect<gtpsa::tpsa> &A){
+			std::cout << "get_dI_eta: operation not defined for"
+				" gtpsa::tpsa" << std::endl;
+			throw std::domain_error
+				("get_dI_eta: operation not defined for"
+				 " gtpsa::tpsa");
+			return 0e0;
+		}
 
 
-	static inline void diff_mat(const double B2, const double u,
-				    const double ps0, const gtpsa::ss_vect<double> &xp) { }
+		static inline void diff_mat
+		(const double B2, const double u, const double ps0,
+		 const gtpsa::ss_vect<double> &xp) { }
 
-};
+	};
 
 // partial specialization
-template<>
-class is_tps<tps> {
-public:
-	/*
-	static inline void get_ps(const ss_vect<tps> &x, thor_scsi::core::CellType *Cell)
-		{ Cell->BeamPos = pstostl(x.cst()); Cell->A = maptostlmat(x); }
-	*/
+	template<>
+	class is_tps<tps> {
+	public:
+		/*
+		static inline void get_ps
+		(const ss_vect<tps> &x, thor_scsi::core::CellType *Cell)
+			{ Cell->BeamPos = pstostl(x.cst());
+				Cell->A = maptostlmat(x); }
+		*/
 
-	static inline tps set_prm(const int k) { return tps(0e0, k); }
+		static inline tps set_prm(const int k) { return tps(0e0, k); }
 
-	/*
-	 *  dN<u^2>/E^2 =
-	 * 3*C_U*C_gamma*h_bar*c*E_0^5*(1+delta)^4*(B_perp/(Brho))^3/(4*pi*m_e^3 [eV/c^2])
-	 */
-	/**
-	 *
-	 *
-	 * @f[
-	 *  \frac{dN<u^2>}{E^2} =
-	 *    3 \ C_U \ C_\gamma \hbar \ c \sqrt{E_0} (1+\delta)^4
-	 *       \frac{\left(\frac{B_{perp}}{B\rho}\right)^3}{(4*pi*m_e^3 [eV/c^2])}
-	 * @f]
-	 * A contains the eigenvectors.
-	 *
-	 * \verbatim embed:rst:leading-asterisk
-	 * see [Sands]_
-	 *
-	 * .. [Sands] M. Sands "The Physics of Electron Storage Rings" SLAC-121, Eq. (5.20),
-	 *            p. 118
-	 *
-	 * \endverbatim
-	 */
-
-	static inline void diff_mat(const tps &B2_perp, const tps &ds,
-				    const tps &p_s0, gtpsa::ss_vect<tps> &x)
-		{ }
-
-
-};
-		/**
-		 * @brief forward a phase space with a drift
-		 *
-		 *
-		 * Required as helper for drift -- kick -- drift implementationa
-		 * See drift.h for DriftType, which is an implementation as separate lattice lement
+		/*
+		 * dN<u^2>/E^2 =
+		 *	3*C_U*C_gamma*h_bar*c*E_0^5*(1+delta)^4
+		 *	*(B_perp/(Brho))^3/(4*pi*m_e^3 [eV/c^2])
 		 */
-		template<typename T, typename T2>
-		void drift_propagate(const thor_scsi::core::ConfigType &conf, const T2& L, gtpsa::ss_vect<T> &ps);
-
 		/**
-		 * @brief implementation of the thin kick (thin lens approximation)
 		 *
-		 *   @param h_bend: 1/rho_bend (radius of the the bend, magnetic rigidty of the dipoles)
-		 *   @param h_ref:  1/rho which is the curvature of the design / reference orbit
+		 @f[
+			 \frac{dN<u^2>}{E^2} =
+			 3 \ C_U \ C_\gamma \hbar \ c \sqrt{E_0} (1+\delta)^4
+			 \frac{\left(\frac{B_{perp}}{B\rho}\right)^3}
+			 {(4*pi*m_e^3 [eV/c^2])}
+		 * @f]
+		 * A contains the eigenvectors.
 		 *
 		 * \verbatim embed:rst:leading-asterisk
+		 * see [Sands]_
 		 *
-		 * .. Todo::
+		 * .. [Sands] M. Sands "The Physics of Electron Storage Rings"
+		 *    SLAC-121, Eq. (5.20), p. 118
 		 *
-		 *     * Split up function in different parts or functions:
-		 *       E.g.: one for dipoles and one for anything else.
-		 *     * Consider to combine interpolation with length L and thus not using field interpolation
-		 *       but field integral interpolation
-		 *     * consider renaming function to thin lens
-		 *
-		 *
-		 * .. Warning::
-		 *
-		 *    * cartesion bend with evaluation of gradients not supported curently (code not
-		 *      active
-		 *
-		 * The vector potential for the combined-function sector bend is from: [Iselin:85]_
-		 *
-		 * .. [Iselin:85]  C. Iselin "Lie Transformations and Transport Equations for Combined-
-		 *                 Function Dipoles" Part. Accel. 17, 143-155 (1985).
 		 * \endverbatim
 		 */
-		template<typename T>
-		void thin_kick(const thor_scsi::core::ConfigType &conf,
-			       // const thor_scsi::core::Field2DInterpolation& intp,
-			       const T BxoBrho, const T ByoBrho,
-			       const double L,
-			       const double h_bend, const double h_ref,
-			       const gtpsa::ss_vect<T> &ps0, gtpsa::ss_vect<T> &ps);
+
+		static inline void diff_mat
+		(const tps &B2_perp, const tps &ds, const tps &p_s0,
+		 gtpsa::ss_vect<tps> &x)
+			{ }
+	};
+	/**
+	 * @brief forward a phase space with a drift
+	 *
+	 *
+	 * Required as helper for drift -- kick -- drift implementationa
+	 * See drift.h for DriftType, which is an implementation as separate
+	 * lattice lement
+	 */
+	template<typename T, typename T2>
+	void drift_propagate
+	(const thor_scsi::core::ConfigType &conf, const T2& L,
+	 gtpsa::ss_vect<T> &ps);
+
+	/**
+	 * @brief implementation of the thin kick (thin lens approximation)
+	 *
+	 *   @param h_bend: 1/rho_bend (radius of the the bend, magnetic
+	 *                              rigidty of the dipoles)
+	 *   @param h_ref:  1/rho which is the curvature of the design
+	 *                  / reference orbit
+	 *
+	 * \verbatim embed:rst:leading-asterisk
+	 *
+	 * .. Todo::
+	 *
+	 *     * Split up function in different parts or functions:
+	 *       E.g.: one for dipoles and one for anything else.
+	 *     * Consider to combine interpolation with length L and thus not
+	 *       using field interpolation but field integral interpolation
+	 *     * consider renaming function to thin lens
+	 *
+	 *
+	 * .. Warning::
+	 *
+	 *    * cartesion bend with evaluation of gradients not supported
+	 *      curently (code not active
+	 *
+	 * The vector potential for the combined-function sector bend is from:
+	 * [Iselin:85]_
+	 *
+	 * .. [Iselin:85]  C. Iselin "Lie Transformations and Transport
+	 *    Equations for Combined-Function Dipoles" Part. Accel. 17,
+	 *    143-155 (1985).
+	 * \endverbatim
+	 */
+	template<typename T>
+	void thin_kick(const thor_scsi::core::ConfigType &conf,
+		       // const thor_scsi::core::Field2DInterpolation& intp,
+		       const T BxoBrho, const T ByoBrho,
+		       const double L,
+		       const double h_bend, const double h_ref,
+		       const gtpsa::ss_vect<T> &ps0, gtpsa::ss_vect<T> &ps);
 
 }// namespace thor_scsi::elements
 #endif /*  _THOR_SCSI_CORE_ELEMENTS_HELPERS_H_  */
