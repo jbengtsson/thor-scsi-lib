21baa38e30ee1e1f909f49ccdcb874b564ef8493
Johan <jbngtssn@gmail.com>
2023-07-21 01:40:28 +0200

    Debugging Poincaré map with sextupoles.

--- python/examples/use_cases/sext_resp_mat.py
+++ python/examples/use_cases/sext_resp_mat.py
@@ -1,3 +1,13 @@
+"""Use Case:
+     Sextupole response matrix analysis.
+
+     J. Bengtsson 
+     𝑇ℎ𝑒 𝑆𝑒𝑥𝑡𝑢𝑝𝑜𝑙𝑒 𝑆𝑐ℎ𝑒𝑚𝑒 𝑓𝑜𝑟 𝑡ℎ𝑒 𝑆𝑤𝑖𝑠𝑠 𝐿𝑖𝑔ℎ𝑡 𝑆𝑜𝑢𝑟𝑐𝑒 (𝑆𝐿𝑆) – 𝐴𝑛 𝐴𝑛𝑎𝑙𝑦𝑡𝑖𝑐 𝐴𝑝𝑝𝑟𝑜𝑎𝑐ℎ
+     See SLS Tech Note 09/97
+
+     https://ados.web.psi.ch/slsnotes/sls0997.pdf
+"""
+
 import enum
 import logging
 
@@ -29,7 +39,9 @@ from thor_scsi.utils.accelerator import instrument_with_standard_observers
 from thor_scsi.utils.twiss_output import twiss_ds_to_df, df_to_tsv
 
 from thor_scsi.utils.extract_info import accelerator_info
-from thor_scsi.utils import linear_optics as lo, courant_snyder as cs
+from thor_scsi.utils import linear_optics as lo, courant_snyder as cs, \
+     radiate as rad
+
 
 # from thor_scsi.utils.phase_space_vector import map2numpy
 from thor_scsi.utils.output import prt2txt, mat2txt, vec2txt
@@ -43,16 +55,15 @@ X_, Y_, Z_ = [
 ]
 
 
+class MultipoleIndex(enum.IntEnum):
+    quadrupole = 2
+    sextupole  = 3
+
+
 def prt_Twiss(str, Twiss):
     """
-
-    todo:
-        rename str e.g. header? prefix?
-
     """
-    # eta, alpha, beta = Twiss[0], Twiss[1], Twiss[2]
-    # that way I also check that Twiss has exactly three parameters
-    eta, alpha, beta = Twiss
+    eta, alpha, beta = Twiss[0], Twiss[1], Twiss[2]
     print(str, end="")
     print(f"  eta    = [{eta[X_]:9.3e}, {eta[Y_]:9.3e}]")
     print(f"  alpha  = [{alpha[X_]:9.3e}, {alpha[Y_]:9.3e}]")
@@ -61,11 +72,8 @@ def prt_Twiss(str, Twiss):
 
 def compute_periodic_solution(lat, model_state, named_index, desc, no):
     """
-    Todo:
-        model_state: rename to calculation_configuration or calc_config
     """
-    # Compute the periodic solution for a super period.
-    # Degrees of freedom - RF cavity is off; i.e., coasting beam.
+    # Deegrees of freedom.
     n_dof = 2
     model_state.radiation = False
     model_state.Cavity_on = False
@@ -85,7 +93,30 @@ def compute_periodic_solution(lat, model_state, named_index, desc, no):
     return M, A, ds
 
 
+def get_phi_elem(lat, fam_name, n_kid):
+    elem = lat.find(fam_name, n_kid)
+    return elem.get_length() * elem.get_curvature() * 180e0 / np.pi
+
+
+def compute_phi(lat):
+    """Compute total bend angle.
+    """
+    prt = False
+    phi = 0e0
+    for k in range(len(lat)):
+        if type(lat[k]) == tslib.Bending:
+            dphi = get_phi_elem(lat, lat[k].name, 0)
+            phi += dphi
+            if prt:
+                print("{:8s} {:5.3f} {:6.3f}".
+                      format(lat[k].name, lat[k].get_length(), dphi))
+    return phi
+
+
 def prt_map(str, map):
+    """
+    # 
+    """
     print(str)
     map.x.print()
     map.px.print()
@@ -104,11 +135,23 @@ def read_lattice(t_file):
     model_state = tslib.ConfigType()
 
     n_dof = 2
+    energy = 2.5e9
+    model_state.Energy    = energy
     model_state.radiation = False
     model_state.Cavity_on = False
 
     print("-> read_lattice")
     return n_dof, lat, model_state
+
+
+def set_b_n_elem(lat, fam_name, kid_num, n, b_n):
+    mp = lat.find(fam_name, kid_num)
+    mp.get_multipoles().set_multipole(n, b_n)
+
+
+def set_b_n_fam(lat, fam_name, n, b_n):
+    for mp in lat.elements_with_name(fam_name):
+        mp.get_multipoles().set_multipole(n, b_n)
 
 
 corresponding_types = {
@@ -135,6 +178,7 @@ def mult_prm(elems, mult_family, n, desc):
                 elem, po=1, desc=desc, named_index=named_index,
                 multipole_number=n, offset=True
             )
+        print("\nmult_prm:", elems[index])
         # While the RHS pointer can be recasted to:
         #   CellVoid
         #   ElemTypeKnobbed
@@ -171,26 +215,44 @@ def propagate(lat, model_state, desc, no, nv, named_index):
 # Number of phase-space coordinates.
 nv = 6
 # Variables max order.
-no = 4
+no = 2
 # Number of parameters.
 nv_prm = 0
 # Parameters max order.
 no_prm = 0
 
-t_dir = os.path.join(os.environ["HOME"], "Nextcloud", "thor_scsi", "JB",
-                     "BESSY-III", "ipac_2023")
+t_dir = os.path.join(
+    os.environ["HOME"], "Nextcloud", "thor_scsi", "JB", "BESSY-III",
+    "ipac_2023")
 t_file = os.path.join(t_dir, "b3_cf425cf_thor_scsi.lat")
 
 n_dof, lat, model_state = read_lattice(t_file)
 
-if False:
+# Zero sextopoles.
+print("\nZeroing sextupoles.")
+set_b_n_fam(lat, "sf", MultipoleIndex.sextupole, 0e0)
+set_b_n_fam(lat, "sd", MultipoleIndex.sextupole, 0e0)
+set_b_n_fam(lat, "sd2", MultipoleIndex.sextupole, 0e0)
+
+if not False:
     named_index = gtpsa.IndexMapping(dict(x=0, px=1, y=2, py=3, delta=4, ct=5))
     desc = gtpsa.desc(nv, no, nv_prm, no_prm)
     M, A, data = \
         compute_periodic_solution(lat, model_state, named_index, desc, no)
 
-nv_prm = 3
-no_prm = 1
+    # Compute linear chromaticity.
+    M = lo.compute_map(lat, model_state, desc=desc, tpsa_order=no)
+    stable, nu, xi = lo.compute_nu_xi(desc, no, M)
+
+    C = rad.compute_circ(lat)
+
+    print("\n  C [m]     = {:9.7f}".format(C))
+    print("  phi [deg] = {:5.3f}".format(compute_phi(lat)))
+    print("  nu        = [{:7.5f}, {:7.5f}]".format(nu[X_], nu[Y_]))
+    print("  xi        = [{:7.5f}, {:7.5f}]".format(xi[X_], xi[Y_]))
+
+nv_prm = 0
+no_prm = 0
 
 named_index = gtpsa.IndexMapping(
     dict(x=0, px=1, y=2, py=3, delta=4, ct=5, K=6, dx=7, dy=8)
@@ -198,12 +260,19 @@ named_index = gtpsa.IndexMapping(
 
 desc = gtpsa.desc(nv, no, nv_prm, no_prm)
 
-lat_ptc = lat_mult_prm("uq3", lat, 3, desc)
-
-M = propagate(lat_ptc, model_state, desc, no, nv, named_index)
-print("\nM:\n", M)
 if False:
+    lat_ptc = lat_mult_prm("uq3", lat, 2, desc)
+    M = propagate(lat, model_state, desc, no, nv, named_index)
+
+M = gtpsa.ss_vect_tpsa(desc, no, nv, index_mapping=named_index)
+M.set_identity()
+lat.propagate(model_state, M)
+
+print("\nM:\n", M)
+if not False:
     prt_map("\nM:", M)
+
+assert False
 
 h = tslib.M_to_h_DF(M)
 print("\nh:")
