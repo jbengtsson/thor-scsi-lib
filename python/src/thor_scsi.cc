bf9c22af384abfbebc59eb9e2ed862095e6bc44c
Johan <jbngtssn@gmail.com>
2023-07-28 01:20:58 +0200

    Added integ gtpsa function to Python interface.

--- python/src/thor_scsi.cc
+++ python/src/thor_scsi.cc
@@ -44,5 +44,6 @@ PYBIND11_MODULE(lib, m) {
     m.def("inv", &gtpsa::minv);
     m.def("pinv", &gtpsa::pminv);
     m.def("compose", &gtpsa::compose);
+    // m.def("integ", &gtpsa::integ);
     m.def("M_to_h_DF", &gtpsa::M_to_h_DF);
 }
