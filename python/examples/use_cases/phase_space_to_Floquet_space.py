96b09a3a9b4ab1b6d3bf1b07889650c111712fdf
Johan <jbngtssn@gmail.com>
2023-11-06 08:57:15 -0500

    Code clean-up.

--- python/examples/use_cases/phase_space_to_Floquet_space.py
+++ python/examples/use_cases/phase_space_to_Floquet_space.py
@@ -1,27 +1,17 @@
 """Use Case:
-     Courant & Snyder matrix diagonalisation.
+     Courant & Snyder diagonalisation of an arbitrary matrix M from phase-space
+     to Floquet space:
+
+       M = A R A^-1
 """
 
-import enum
-import logging
-
-# Levels: DEBUG, INFO, WARNING, ERROR, and CRITICAL.
-logging.basicConfig(level="ERROR")
-logger = logging.getLogger("thor_scsi")
-
-
-import math
 import os
-
-import numpy as np
 
 import gtpsa
 import thor_scsi.lib as ts
-
 from thor_scsi.factory import accelerator_from_config
-
-from thor_scsi.utils import linear_optics as lo, courant_snyder as cs, \
-    radiate as rad, closed_orbit as co
+from thor_scsi.utils import linear_optics as lo
+from thor_scsi.utils.output import mat2txt
 
 
 def read_lattice(file_name):
@@ -51,13 +41,20 @@ no_prm = 0
 desc = gtpsa.desc(nv, no, nv_prm, no_prm)
 
 home_dir = os.path.join(os.environ["HOME"], "Nextcloud", "thor_scsi", "JB",
-                     "BESSY-III", "ipac_2023")
+                        "BESSY-III", "ipac_2023")
 file_name = os.path.join(home_dir, "b3_cf425cf_thor_scsi.lat")
 
-print("\nlattice file:  \n", file_name)
+print("\nlattice file:\n ", file_name)
 
 n_dof, lat, model_state = read_lattice(file_name)
 
 M = lo.compute_map(lat, model_state, desc=desc, tpsa_order=no)
 
-print("\nM:", M)
+M_mat = M.jacobian()[:6, :6]
+
+stable, A_mat, A_inv_mat, alpha_rad = lo.compute_M_diag(n_dof, M_mat)
+
+print("\nM:\n", mat2txt(M_mat))
+print("\nA:\n", mat2txt(A_mat))
+print("\nR:\n", mat2txt(A_inv_mat @ M_mat @ A_mat))
+
