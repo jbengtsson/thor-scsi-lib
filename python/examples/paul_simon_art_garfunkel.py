24728b4cdd6de9e0c3a0de1994f5fd59d0a7de00
Johan <jbngtssn@gmail.com>
2023-11-18 18:00:14 -0500

    Refactored demo.

--- python/examples/paul_simon_art_garfunkel.py
+++ python/examples/paul_simon_art_garfunkel.py
@@ -46,13 +46,10 @@ import matplotlib.pyplot as plt
 import thor_scsi.lib as tslib
 
 from thor_scsi.factory import accelerator_from_config
-from thor_scsi.utils.linear_optics import (
-    compute_map,
-    compute_nu_symp,
-    compute_M_diag,
-    compute_Twiss_along_lattice
-)
-from thor_scsi.utils.radiate import compute_radiation
+
+from thor_scsi.utils import linear_optics as lo, courant_snyder as cs, \
+    radiate as rad
+
 from thor_scsi.utils.phase_space_vector import map2numpy
 from thor_scsi.utils.output import mat2txt, vec2txt
 from thor_scsi.utils.twiss_output import twiss_ds_to_df, df_to_tsv
@@ -92,36 +89,36 @@ def plt_curly_H(ds):
     fig.tight_layout()
 
 
-def prt_fam(acc, fam_name):
+def prt_fam(lat, fam_name):
     print("\nprt_fam:")
-    for q in acc.elementsWithName(fam_name):
+    for q in lat.elementsWithName(fam_name):
         print("  {:4s} {:3d} {:6.3f} {:4.2f}".
               format(q.name, q.index, q.getMultipoles().getMultipole(2).real,
                      q.getLength()))
 
 
-def set_db_2_fam(acc, fam_name, db_2):
-    for q in acc.elements_with_name(fam_name):
+def set_db_2_fam(lat, fam_name, db_2):
+    for q in lat.elements_with_name(fam_name):
         b_2 = q.get_multipoles().get_multipole(2).real
         q.get_multipoles().set_multipole(2, b_2+db_2)
 
 
-def set_db_2L_fam(acc, fam_name, db_2L):
-    q = acc.find(fam_name, 0)
+def set_db_2L_fam(lat, fam_name, db_2L):
+    q = lat.find(fam_name, 0)
     L = q.get_length()
-    set_db_2_fam(acc, q.name, db_2L/L)
+    set_db_2_fam(lat, q.name, db_2L/L)
 
 
-def compute_dnu_db_2L(acc, calc_config, fam_name, db_2L):
-    dof = 2
-    nu = np.zeros([2, dof])
+def compute_dnu_db_2L(lat, model_state, fam_name, db_2L):
+    n_dof = 2
+    nu = np.zeros([2, n_dof])
     for k in range(-1, 2, 2):
-        set_db_2L_fam(acc, fam_name, k*db_2L)
-        #M = map2numpy(compute_map(acc, calc_config))[:6, :6]
-        a_map = compute_map(acc, calc_config, desc=desc)
+        set_db_2L_fam(lat, fam_name, k*db_2L)
+        #M = map2numpy(compute_map(lat, model_state))[:6, :6]
+        a_map = lo.compute_map(lat, model_state, desc=desc)
         M = a_map.jacobian()
-        set_db_2L_fam(acc, fam_name, -k*db_2L)
-        nu[(k+1)//2] = compute_nu_symp(dof, M)
+        set_db_2L_fam(lat, fam_name, -k*db_2L)
+        nu[(k+1)//2] = lo.compute_nu_symp(n_dof, M)
 
     dnu_db_2 = (nu[1]-nu[0])/(2e0*db_2L)
     return dnu_db_2
@@ -133,24 +130,25 @@ def tweak_nu(fam_names, dnu_x, dnu_y):
     dnu_db_2L = np.zeros((2, n), dtype='float')
     for k in range(n):
         dnu_db_2L[:, k] = \
-            compute_dnu_db_2L(acc, calc_config, fam_names[k],   1e-4)
+            compute_dnu_db_2L(lat, model_state, fam_names[k],   1e-4)
     print("\ndnu_db_2L:\n", mat2txt(dnu_db_2L))
     u, s, v_t = np.linalg.svd(dnu_db_2L, full_matrices=False)
     dnu_db_2L_inv = (v_t.T @ np.diag(s**-1) @ u.T)
     db_2L = dnu_db_2L_inv @ dnu
     print("\ndb_2L = ", vec2txt(db_2L))
     for k in range(n):
-        set_db_2L_fam(acc, fam_names[k], db_2L[k])
+        set_db_2L_fam(lat, fam_names[k], db_2L[k])
 
 
-t_dir = os.path.join(os.environ["HOME"], "Nextcloud", "thor_scsi")
+t_dir = os.path.join(
+    os.environ["HOME"], "Nextcloud", "thor_scsi", "JB", "BESSY-III")
 t_file = os.path.join(t_dir, "alsu-7ba-20180503c.lat")
 # t_file = os.path.join(t_dir, "b3_sf_40Grad_JB.lat")
 
-acc = accelerator_from_config(t_file)
-calc_config = tslib.ConfigType()
+lat = accelerator_from_config(t_file)
+model_state = tslib.ConfigType()
 
-cav = acc.find("cav", 0)
+cav = lat.find("cav", 0)
 cav.set_harmonic_number(327)
 cav.set_phase(180.0)
 print("\nCavity", repr(cav))
@@ -161,26 +159,35 @@ print(f"""\nCavity info:
   phi     {cav.get_phase()}
 """, end="")
 
-dof = 2
+n_dof = 2
+
+# Number of phase-space coordinates.
+nv = 7
+# Variables max order.
+no = 2
 
 E = 2.0e9
-calc_config.radiation = False
-calc_config.Cavity_on = False
 
+model_state.radiation = False
+model_state.Cavity_on = False
 
-desc = gtpsa.desc(6, 2)
-M = compute_map(acc, calc_config, desc=desc)
-jac = M.jacobian()
-nu = compute_nu_symp(dof, jac)
-print("\nM:\n" + mat2txt(jac))
-print("\nnu = [{:7.5f}, {:7.5f}]".format(nu[X_], nu[Y_]))
+named_index = gtpsa.IndexMapping(dict(x=0, px=1, y=2, py=3, delta=4, ct=5))
 
-A, A_inv, _ = compute_M_diag(dof, jac)
+desc = gtpsa.desc(nv, no)
 
-ds = compute_Twiss_along_lattice(2, acc, calc_config,
-                                 # A
-                                 desc = desc
-                                 )
+# Compute linear chromaticity.
+M = lo.compute_map(lat, model_state, desc=desc, tpsa_order=no)
+stable, nu, xi = lo.compute_nu_xi(desc, no, M)
+
+print("\nM:\n", M)
+print("  nu        = [{:7.5f}, {:7.5f}]".format(nu[X_], nu[Y_]))
+print("  xi        = [{:7.5f}, {:7.5f}]".format(xi[X_], xi[Y_]))
+
+stable, A, A_inv, _ = lo.compute_M_diag(n_dof, M.jacobian())
+
+ds = \
+    lo.compute_Twiss_along_lattice(
+        n_dof, lat, model_state, desc=desc, mapping=named_index)
 df = twiss_ds_to_df(ds)
 # print("\nTwiss - ds:\n", ds)
 # print("\nTwiss - df:\n", df)
@@ -193,18 +200,17 @@ if True:
     plt_twiss(ds)
     plt_curly_H(ds)
 
+rad.compute_radiation(lat, model_state, E, 1e-15, desc=desc)
 
-compute_radiation(acc, calc_config, E, 1e-15, desc=desc)
-
-calc_config.radiation = False
-calc_config.Cavity_on = False
+model_state.radiation = False
+model_state.Cavity_on = False
 
 # Tweak the tune using all the quadrupole families.
 b_2 = ['q1', 'q2', 'q3', 'q4', 'mb3h', 'qfh', 'mqfh']
 tweak_nu(b_2, 0.00501, -0.00670)
-a_map = compute_map(acc, calc_config, desc=desc)
+a_map = lo.compute_map(lat, model_state, desc=desc)
 M = a_map.jacobian()
-nu = compute_nu_symp(dof, M)
+nu = lo.compute_nu_symp(n_dof, M)
 print("\nM:\n" + mat2txt(M))
 print("\nnu = [{:7.5f}, {:7.5f}]".format(nu[X_], nu[Y_]))
 
