01b3d2537eba772a4ac8a3cca62ad4f6c1e2388f
Johan <jbngtssn@gmail.com>
2024-05-14 13:43:52 +0200

    Implementing max_4u_uc.py.

--- python/examples/use_cases/max_4u_sp.py
+++ python/examples/use_cases/max_4u_sp.py
@@ -20,6 +20,8 @@ from thor_scsi.utils import lattice_properties as lp, index_class as ind, \
     linear_optics as lo, prm_class as pc
 from thor_scsi.utils.output import vec2txt
 
+import gtpsa
+
 
 ind = ind.index_class()
 
@@ -30,17 +32,12 @@ b_2_bend_max = 1.0
 b_2_max      = 10.0
 
 
-def opt_sp(lat_prop, prm_list, weight):
+def opt_sp(lat_prop, prm_list, uc_0, uc_1, weight):
     """Use Case: optimise super period.
     """
 
     loc       = lat_prop._lattice.find("sf_h", 2).index
-    # Dispersion at the unit cell end.
-    eta_uc_x  = 0.06255
-    # Beta functions at the unit cell end.
-    beta_uc   = [3.65614, 3.68868]
-    nu_uc     = [0.266, 0.0764]
-    nu_sp     = [2.110-4*nu_uc[ind.X], 0.814-4*nu_uc[ind.Y]]
+    nu_uc     = [1.0/3.0, 1.0/12.0]
 
     chi_2_min = 1e30
     eta       = np.nan
@@ -56,43 +53,38 @@ def opt_sp(lat_prop, prm_list, weight):
                 phi += lat_prop.get_phi_elem(bend_list[k], 0)
             return phi
 
-        b1_list = [
-            "b1_0", "b1_1", "b1_2", "b1_3", "b1_4", "b1_5"]
-        b2_list = [
-            "b2d_6", "b2d_5", "b2d_4", "b2d_3", "b2d_2", "b2d_1", "b2_0",
-            "b2u_1", "b2u_2", "b2u_3", "b2u_4", "b2u_5"]
-        rb = "qf1"
+        b_list = [
+            "b2u_6", "b2u_5", "b2u_4", "b2u_3", "b2u_2", "b2u_1", "b2_0",
+            "b2d_1", "b2d_2", "b2d_3", "b2d_4", "b2d_5"]
+        rb_1 = "qf1"
+        rb_2 = "qf1_e"
 
         phi = lat_prop.compute_phi_lat()
+        phi_b = compute_phi_bend(lat_prop, b_list)
+        phi_rb_1 = lat_prop.get_phi_elem(rb_1, 0)
+        phi_rb_2 = lat_prop.get_phi_elem(rb_2, 0)
+
         # Twiss functions at end of super period.
         _, _, beta_id, _ = lat_prop.get_Twiss(-1)
-        phi_b1 = compute_phi_bend(lat_prop, b1_list)
-        phi_b2 = compute_phi_bend(lat_prop, b2_list)
-        phi_rb = lat_prop.get_phi_elem(rb, 0)
 
         print("\n{:3d} chi_2 = {:11.5e}".format(n_iter, chi_2))
         print("  eps_x [pm.rad] = {:5.3f}".format(1e12*lat_prop._eps[ind.X]))
         print("  U_0 [keV]      = {:5.3f}".format(1e-3*lat_prop._U_0))
-        print("  eta_x [m]      =  {:10.3e} ({:9.3e})".
-              format(eta[ind.x], eta_uc_x))
-        print("  beta_match [m] =  [{:5.3f}, {:5.3f}] ([{:5.3f}, {:5.3f}])".
-              format(beta[ind.X], beta[ind.Y],
-                     beta_uc[ind.X], beta_uc[ind.Y]))
         print("  beta_id        =  [{:5.3f}, {:5.3f}]".
               format(beta_id[ind.X], beta_id[ind.Y]))
-        print("  nu_sp          =  [{:7.5f}, {:7.5f}] ([{:7.5f}, {:7.5f}])".
-              format(nu[ind.X], nu[ind.Y], nu_sp[ind.X], nu_sp[ind.Y]))
+        print("  nu             =  [{:7.5f}, {:7.5f}] ([{:7.5f}, {:7.5f}])".
+              format(nu[ind.X], nu[ind.Y], nu_uc[ind.X], nu_uc[ind.Y]))
         print("  xi             =  [{:5.3f}, {:5.3f}]".
               format(xi[ind.X], xi[ind.Y]))
         print("\n  phi_sp         =  {:8.5f}".format(phi))
         print("  C [m]          =  {:8.5f}".format(lat_prop.compute_circ()))
-        print("\n  phi_b1         =  {:8.5f}".format(phi_b1))
-        print("  phi_b2         =  {:8.5f}".format(phi_b2))
-        print("  phi_rb         =  {:8.5f}".format(phi_rb))
+        print("\n  phi_b          =  {:8.5f}".format(phi_b))
+        print("  phi_rb_1       =  {:8.5f}".format(phi_rb_1))
+        print("  phi_rb_2       =  {:8.5f}".format(phi_rb_2))
         prm_list.prt_prm(prm)
 
     def compute_chi_2(eta, beta, nu, xi):
-        nonlocal loc, eta_uc_x
+        nonlocal loc
 
         prt = not False
 
@@ -106,28 +98,15 @@ def opt_sp(lat_prop, prm_list, weight):
         if prt:
             print("  dchi2(U_0)      = {:10.3e}".format(dchi_2))
 
-        dchi_2 = weight[2]*(eta[ind.x]-eta_uc_x)**2
-        chi_2 += dchi_2
-        if prt:
-            print("  dchi2(eta_uc_x) = {:10.3e}".format(dchi_2))
-
         dchi_2 = \
-            weight[3]*(
-                (beta[ind.X]-beta_uc[ind.X])**2
-                +(beta[ind.Y]-beta_uc[ind.Y])**2)
+            weight[2]*(
+                (nu[ind.X]-nu_uc[ind.X])**2
+                +(nu[ind.Y]-nu_uc[ind.Y])**2)
         chi_2 += dchi_2
         if prt:
-            print("  dchi2(beta)     = {:10.3e}".format(dchi_2))
+            print("  dchi2(nu_uc)    = {:10.3e}".format(dchi_2))
 
-        dchi_2 = \
-            weight[4]*(
-                (nu[ind.X]-nu_sp[ind.X])**2
-                +(nu[ind.Y]-nu_sp[ind.Y])**2)
-        chi_2 += dchi_2
-        if prt:
-            print("  dchi2(nu_sp)    = {:10.3e}".format(dchi_2))
-
-        dchi_2 = weight[5]*(xi[ind.X]**2+xi[ind.Y]**2)
+        dchi_2 = weight[3]*(xi[ind.X]**2+xi[ind.Y]**2)
         chi_2 += dchi_2
         if prt:
             print("  dchi2(xi)       = {:10.3e}".format(dchi_2))
@@ -150,8 +129,16 @@ def opt_sp(lat_prop, prm_list, weight):
                 print("\nf_sp - compute_radiation: unstable")
                 raise ValueError
 
-            eta, _, beta, _ = lat_prop.get_Twiss(loc)
-            _, _, _, nu = lat_prop.get_Twiss(-1)
+            _, _, beta, _ = lat_prop.get_Twiss(-1)
+
+            M = gtpsa.ss_vect_tpsa(lat_prop._desc, 1)
+            M.set_identity()
+            # The 3rd argument is the 1st element index & the 4th the number of
+            # elements to propagate across.
+            lat_prop._lattice.propagate(
+                lat_prop._model_state, M, uc_0, uc_1-uc_0+1)
+            # nu = lo.compute_nus(2, M.jacobian())
+            nu = lo.compute_nu_symp(2, M.jacobian())
 
             M = lo.compute_map(
                 lat_prop._lattice, lat_prop._model_state,
@@ -209,7 +196,7 @@ E_0     = 3.0e9
 
 home_dir = os.path.join(
     os.environ["HOME"], "Nextcloud", "thor_scsi", "JB", "MAX_4U")
-lat_name = "max_4u_sp_jb"
+lat_name = "max_4u_sp_jb_2"
 file_name = os.path.join(home_dir, lat_name+".lat")
 
 lat_prop = \
@@ -242,15 +229,20 @@ else:
 
 if False:
     lat_prop.prt_lat_param()
-    lat_prop.prt_Twiss("max_4u_uc.txt")
+    lat_prop.prt_Twiss(lat_name+".txt")
+
+uc_0 = lat_prop._lattice.find("b1_0", 7).index
+uc_1 = lat_prop._lattice.find("b1_0", 8).index
+print("\nunit cell entrance {:5s} loc = {:d}".
+      format(lat_prop._lattice[uc_0].name, uc_0))
+print("unit cell exit     {:5s} loc = {:d}".
+      format(lat_prop._lattice[uc_1].name, uc_1))
 
 # Weights.
 weight = np.array([
     1e14,  # eps_x.
-    10e-14, # U_0.
-    1e2,   # eta_uc_x.
-    1e-2,  # beta_uc.
-    1e-1,  # nu_sp.
+    1e-16, # U_0.
+    1e-1,  # nu_uc.
     1e-7   # xi.
 ])
 
@@ -276,10 +268,13 @@ prm_list = [
     ("qf1_e",    "b_2"),
     ("qd",       "b_2"),
     ("qf2",      "b_2"),
-    # ("phi_tot",  opt_phi),
+    ("qf1",      "phi"),
+    ("qf1_e",    "phi"),
+    ("phi_tot",  opt_phi),
+    # ("b_1_bend", b1_bend),
     ("b_2_bend", b2_bend)
 ]
 
 prm_list = pc.prm_class(lat_prop, prm_list, b_2_max)
 
-opt_sp(lat_prop, prm_list, weight)
+opt_sp(lat_prop, prm_list, uc_0, uc_1, weight)
