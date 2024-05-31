7d7d13eb136939c9688148a31ff97b2374d5c2c4
Johan <jbngtssn@gmail.com>
2024-05-31 09:12:43 +0200

    Code clean-up.

--- python/examples/use_cases/tracy2at.py
+++ python/examples/use_cases/tracy2at.py
@@ -8,21 +8,14 @@ import at
 
 
 def tracy2at(file_name, harmonic_number):
-    try:
-        ring = at.load_tracy(file_name+".lat", harmonic_number=harmonic_number)
-    except:
-        print("\nload_tracy failed")
-
-    try:
-        at.save_m(ring, filename=file_name+".m")
-    except:
-        print("\nsave_m failed")
+    ring = at.load_tracy(file_name+".lat", harmonic_number=harmonic_number)
+    at.save_m(ring, filename=file_name+".m")
 
 
 home_dir = os.path.join(
     os.environ["HOME"], os.environ["TRACY_LIB"], "projects", "in", "lattice",
     "MAX_4U")
-lat_name = "max_iv_ref"
+lat_name = "max_iv_sp_matched_2"
 file_name = os.path.join(home_dir, lat_name)
 
 tracy2at(file_name, 176)
