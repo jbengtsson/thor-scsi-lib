23ffce456fa032591a0d085857247004bb9e1233
Johan <jbngtssn@gmail.com>
2024-06-07 14:02:03 +0200

    Generalising use case for matching.

--- python/examples/use_cases/max_4u_match.py
+++ python/examples/use_cases/max_4u_match.py
@@ -35,6 +35,11 @@ phi_max      = 0.85
 b_2_bend_max = 1.0
 b_2_max      = 10.0
 
+eta_des   = [0.0, 0.0]   # [eta_x, eta'_x] at the centre of the straight.
+alpha_des = [0.0, 0.0]   # Alpha_x,y at the centre of the straight.
+beta_des  = [9.2, 2.0]   # Beta_x,y at the centre of the straight.
+dnu_des   = [0.5, 0.25]  # Phase advance across the straight.
+
 
 def compute_periodic_cell(lat_prop, uc_0, uc_1):
     M = gtpsa.ss_vect_tpsa(lat_prop._desc, 1)
@@ -53,12 +58,13 @@ def compute_periodic_cell(lat_prop, uc_0, uc_1):
     return Twiss, A
 
 
-def match_straight(lat_prop, prm_list, uc_0, uc_1, sp_1, beta, weight, phi_lat):
+def match_straight(
+        lat_prop, prm_list, uc_0, uc_1, sp_1, weight, phi_lat, Twiss_des):
     chi_2_min = 1e30
     n_iter    = 0
     A0        = gtpsa.ss_vect_tpsa(lat_prop._desc, 1)
 
-    def prt_iter(prm, chi_2, Twiss_k):
+    def prt_iter(prm, chi_2, Twiss_des, Twiss_k):
 
         def compute_phi_bend(lat_prop, bend_list):
             phi = 0e0
@@ -76,48 +82,94 @@ def match_straight(lat_prop, prm_list, uc_0, uc_1, sp_1, beta, weight, phi_lat):
         phi_rb_2 = lat_prop.get_phi_elem(rb_2, 0)
 
         print("\n{:4d} chi_2 = {:9.3e}".format(n_iter, chi_2))
-        lat_prop.prt_Twiss_param(Twiss_k[:3])
-        print("\n    phi_sp         = {:8.5f}".format(phi))
-        print("    C [m]          = {:8.5f}".format(lat_prop.compute_circ()))
-        print("\n    phi_b1         = {:8.5f}".format(phi_b1))
-        print("    phi_b2         = {:8.5f}".format(phi_b2))
-        print("    phi_rb_1       = {:8.5f}".format(phi_rb_1))
-        print("    phi_rb_2       = {:8.5f}".format(phi_rb_2))
+        print("    [eta_x, eta'_x] = "+
+              "[{:10.3e}, {:10.3e}] ([{:10.3e}, {:10.3e}])".
+              format(Twiss_k[0][ind.x], Twiss_k[0][ind.px],
+                     Twiss_des[0][ind.x], Twiss_des[0][ind.px]))
+        print("    alpha           = "
+              +"[{:10.3e}, {:10.3e}] ([{:10.3e}, {:10.3e}])".
+              format(Twiss_k[1][ind.X], Twiss_k[1][ind.Y],
+                     Twiss_des[1][ind.X], Twiss_des[1][ind.Y]))
+        print("    beta            = "+
+              "[{:8.5f}, {:10.5f}]   ([{:8.5f}, {:8.5f}])".
+              format(Twiss_k[2][ind.X], Twiss_k[2][ind.Y],
+                     Twiss_des[2][ind.X], Twiss_des[2][ind.Y]))
+        print("    dnu             = "+
+              "[{:8.5f}, {:10.5f}]   ([{:8.5f}, {:8.5f}])".
+              format(Twiss_k[3][ind.X], Twiss_k[3][ind.Y],
+                     Twiss_des[3][ind.X], Twiss_des[3][ind.Y]))
+        print("\n    phi_sp          = {:8.5f}".format(phi))
+        print("    C [m]           = {:8.5f}".format(lat_prop.compute_circ()))
+        print("\n    phi_b1          = {:8.5f}".format(phi_b1))
+        print("    phi_b2          = {:8.5f}".format(phi_b2))
+        print("    phi_rb_1        = {:8.5f}".format(phi_rb_1))
+        print("    phi_rb_2        = {:8.5f}".format(phi_rb_2))
         prm_list.prt_prm(prm)
 
-    def compute_chi_2_Twiss(Twiss_k):
+    def compute_chi_2_Twiss(Twiss_des, Twiss_k):
         prt = False
-        chi_2 = 0e0
+
+        dchi_2 = weight[0]*(Twiss_k[0][ind.x]-Twiss_des[0][ind.x])**2
         if prt:
-            print()
-        for j in range(3):
-            for k in range(2):
-                dchi_2 = weight[j] * ((Twiss_1[j][k] - Twiss_k[j][k]) ** 2)
-                if prt:
-                    print(" dchi_2 = {:9.3e}".format(dchi_2))
-                chi_2 += dchi_2
+            print("\n  dchi2(eta_x)    = {:9.3e}".format(dchi_2))
+        chi_2 = dchi_2
+
+        dchi_2 = weight[0]*(Twiss_k[0][ind.px]-Twiss_des[0][ind.px])**2
+        if prt:
+            print("  dchi2(eta'_x)   = {:9.3e}".format(dchi_2))
+        chi_2 += dchi_2
+
+        dchi_2 = weight[1]*(Twiss_k[1][ind.X]-Twiss_des[1][ind.X])**2
+        if prt:
+            print("  dchi2(alpha_x)  = {:9.3e}".format(dchi_2))
+        chi_2 += dchi_2
+
+        dchi_2 = weight[1]*(Twiss_k[1][ind.Y]-Twiss_des[1][ind.Y])**2
+        if prt:
+            print("  dchi2(alpha_y)  = {:9.3e}".format(dchi_2))
+        chi_2 += dchi_2
+
+        dchi_2 = weight[2]*(Twiss_k[2][ind.X]-Twiss_des[2][ind.X])**2
+        if prt:
+            print("  dchi2(beta_x)   = {:9.3e}".format(dchi_2))
+        chi_2 += dchi_2
+
+        dchi_2 = weight[3]*(Twiss_k[2][ind.Y]-Twiss_des[2][ind.Y])**2
+        if prt:
+            print("  dchi2(beta_y)   = {:9.3e}".format(dchi_2))
+        chi_2 += dchi_2
+
+        dchi_2 = weight[4]*(Twiss_k[3][ind.X]-Twiss_des[3][ind.X])**2
+        if prt:
+            print("  dchi2(dnu_x)    = {:9.3e}".format(dchi_2))
+        chi_2 += dchi_2
+
+        dchi_2 = weight[5]*(Twiss_k[3][ind.Y]-Twiss_des[3][ind.Y])**2
+        if prt:
+            print("  dchi2(dnu_y)    = {:9.3e}".format(dchi_2))
+        chi_2 += dchi_2
+
         return chi_2
 
-    def compute_chi_2():
-        nonlocal A0
+    def compute_chi_2(A0, Twiss_des):
 
         A1 = _copy.copy(A0)
         lat_prop._lattice.propagate(
             lat_prop._model_state, A1, uc_1+1, sp_1-uc_1)
         Twiss_k = cs.compute_Twiss_A(A1.jacobian())
-        chi_2 = compute_chi_2_Twiss(Twiss_k)
+        chi_2 = compute_chi_2_Twiss(Twiss_des, Twiss_k)
         return chi_2, Twiss_k
 
     def f_match(prm):
-        nonlocal chi_2_min, n_iter
+        nonlocal chi_2_min, n_iter, A0, Twiss_des
 
         n_iter += 1
         prm_list.set_prm(prm)
         phi_lat.set_phi_lat()
 
-        chi_2, Twiss_1 = compute_chi_2()
+        chi_2, Twiss_k = compute_chi_2(A0, Twiss_des)
         if chi_2 < chi_2_min:
-            prt_iter(prm, chi_2, Twiss_1)
+            prt_iter(prm, chi_2, Twiss_des, Twiss_k)
             pc.prt_lat(lat_prop, "match_lat_k.txt", prm_list, phi_lat=phi_lat)
             chi_2_min = min(chi_2, chi_2_min)
 
@@ -131,12 +183,9 @@ def match_straight(lat_prop, prm_list, uc_0, uc_1, sp_1, beta, weight, phi_lat):
     x_tol    = 1e-4
 
     Twiss_0, A = compute_periodic_cell(lat_prop, uc_0, uc_1)
-    Twiss_1 = np.array([[0e0, 0e0], [0e0, 0e0], beta])
 
     print("\nmatch_straight:\n\nEntrance:")
     lat_prop.prt_Twiss_param(Twiss_0)
-    print("\nExit:")
-    lat_prop.prt_Twiss_param(Twiss_1)
 
     A0.set_zero()
     A_7x7 = np.zeros((7, 7))
@@ -177,6 +226,7 @@ E_0     = 3.0e9
 
 home_dir = os.path.join(
     os.environ["HOME"], "Nextcloud", "thor_scsi", "JB", "MAX_4U")
+# lat_name = "max_iv_sp_matched"
 lat_name = "max_4u_d_0"
 file_name = os.path.join(home_dir, lat_name+".lat")
 
@@ -196,13 +246,16 @@ print("unit cell exit     {:5s} loc = {:d}".
 print("super perioc exit  {:5s} loc = {:d}".
       format(lat_prop._lattice[sp_1].name, sp_1))
 
-beta = np.array([9.2, 2.0])
+Twiss_des = np.array([eta_des, alpha_des, beta_des, dnu_des])
 
 # Weights.
 weight = np.array([
-    1e9,  # eta.
-    1e4,  # alpha.
-    0*1e0 # beta.
+    1e1, # eta_x at the centre of the straight.
+    1e0, # alpha at the centre of the straight.
+    0e0, # beta_x at the centre of the straight.
+    0e0, # beta_y at the centre of the straight.
+    0e0, # dnu_x across the straight.
+    0e0  # dnu_y across the straight.
 ])
 
 b1_list = ["b1_0", "b1_1", "b1_2", "b1_3", "b1_4", "b1_5"]
@@ -226,6 +279,7 @@ prm_list = [
     ("qf2",      "b_2"),
 
     ("b_2_bend", b2_bend),
+
     ("phi_bend", b1_bend),
     ("phi_bend", b2_bend)
 ]
@@ -234,4 +288,4 @@ prm_list = pc.prm_class(lat_prop, prm_list, b_2_max)
 
 phi_lat = pc.phi_lat_class(lat_prop, "qf1_e")
 
-match_straight(lat_prop, prm_list, uc_0, uc_1, sp_1, beta, weight, phi_lat)
+match_straight(lat_prop, prm_list, uc_0, uc_1, sp_1, weight, phi_lat, Twiss_des)
