201fb2cb3f200eb105121b65b4b33063f2beafd3
Johan <jbngtssn@gmail.com>
2024-06-24 17:25:41 +0200

    Script clean-up.

--- build.sh
+++ build.sh
@@ -12,45 +12,48 @@ if false; then
 fi
 
 # Pull all changes in the repository:
-if ! false; then
+if false; then
     git pull --recurse-submodules
 fi
 
 # Checkout local branches for gtpsa from CERN.
-if ! false; then
+if false; then
     cd src/gtpsa
     git checkout gtpsa_jb
     cd mad-ng
     git checkout 11fcbfeb
-    cd $home_dir
 fi
 
-if ! false; then
-    # Create the build directory.
+# Create the build directory.
+if false; then
     mkdir build
 fi
 
-cd build
-
 # Make, test, and install the thor-scsi & gtpsa libraries.
-if ! false; then
+if false; then
+    cd $home_dir/build
     cmake ..
     make -j8
     make test
     cmake --install . --prefix ../local
 fi
 
+# Build the Python interfaces.
 if ! false; then
-    # Build the Python interfaces.
     export THOR_SCSI_LIB=$home_dir
     export gtpsa_PREFIX=$THOR_SCSI_LIB/local
     export thor_scsi_PREFIX=$THOR_SCSI_LIB/local
     echo "\n\$THOR_SCSI_LIB set to: " $THOR_SCSI_LIB
-    echo "\$gtpsa_PREFIX set to:    " $gtpsa_PREFIX
-    echo "\$thor_scsi_PREFIX set to:" $thor_scsi_PREFIX
-    cd python
+    echo "\$gtpsa_PREFIX set to:      " $gtpsa_PREFIX
+    echo "\$thor_scsi_PREFIX set to:  " $thor_scsi_PREFIX
+    cd $home_dir/src/gtpsa/python
     pip install .
-    cd home_dir/python
+    cd $home_dir/python
     pip install .
 fi
-  
+
+# Create a local Python environment.
+if false; then
+    python -m venv $home_dir
+    source $home_dir/bin/activate
+fi
