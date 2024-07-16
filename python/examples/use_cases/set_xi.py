5919f0076b1b54c08dcd846d8b47cb3fe37d6409
Johan <jbngtssn@gmail.com>
2024-07-16 12:55:29 +0200

    Code refactoring.

--- python/examples/use_cases/set_xi.py
+++ python/examples/use_cases/set_xi.py
@@ -32,6 +32,48 @@ corresponding_types = {
 }
 
 
+def compute_nu_xi(lat_prop, M):
+    planes = ["x", "y"]
+    try:
+        nu, xi = [np.zeros(2), np.zeros(2)]
+        M_delta = gtpsa.tpsa(lat_prop._desc, lat_prop._no)
+        for k in range(2):
+            tr_k = np.trace(M.jacobian()[2*k:2*(k+1)])
+            if tr_k >= 2e0:
+                print("\ncompute_nu_xi: unstable in plane {:s}".
+                      format(planes[k]))
+                raise ValueError
+            M_delta.clear()
+            if k == 0:
+                M_delta += M.x.get(lo.ind_1(2*k))
+                M_delta.set(
+                    lo.ind_1(ind.delta), 0e0, M.x.get(lo.ind_2(2*k, ind.delta)))
+                # m_22 + delta * m_26.
+                M_delta += M.px.get(lo.ind_1(2*k+1))
+                M_delta.set(
+                    lo.ind_1(ind.delta), 1e0,
+                    M.px.get(lo.ind_2(2*k+1, ind.delta)))
+            elif k == 1:
+                M_delta += M.y.get(lo.ind_1(2*k))
+                M_delta.set(
+                    lo.ind_1(ind.delta), 0e0,
+                    M.y.get(lo.ind_2(2*k, ind.delta)))
+                # m_22 + delta * m_26.
+                M_delta += M.py.get(lo.ind_1(2*k+1))
+                M_delta.set(
+                    lo.ind_1(ind.delta), 1e0,
+                    M.py.get(lo.ind_2(2*k+1, ind.delta)))
+            # M_delta = m_11 + m_22.
+            nu_tpsa = \
+                lo.acos2_tpsa(M.jacobian()[2*k][2*k+1], M_delta/2e0)/(2e0*np.pi)
+            nu[k], xi[k] = [nu_tpsa.get(), nu_tpsa.get(lo.ind_1(ind.delta))]
+    except ValueError:
+        print("\ncompute_nu_xi: unstable =", stable)
+        return False, np.nan, np.nan
+    else:
+        return True, nu, xi
+
+
 def prt_map(map, str, *, eps: float=1e-30):
     print(str)
     map.x.print("x", eps)
@@ -49,7 +91,6 @@ def convert_magnet_to_knobbable(a_magnet: ts.Mpole) -> ts.MpoleTpsa:
 
 
 def mult_prm(lat_prop, elems, mult_family, mpole_n):
-    print("\nmult_prm ->")
     for k in range(len(mult_family)):
         index = mult_family[k].index
         elem = convert_magnet_to_knobbable(elems[index])
@@ -59,7 +100,6 @@ def mult_prm(lat_prop, elems, mult_family, mpole_n):
                 named_index=lat_prop._named_index, multipole_number=mpole_n,
                 offset=False
             )
-        print("\nmult_prm:", elems[index])
         # While the RHS pointer can be recasted to:
         #   CellVoid
         #   ElemTypeKnobbed
@@ -71,12 +111,10 @@ def mult_prm(lat_prop, elems, mult_family, mpole_n):
         # will lead to an assert on line 158 in:
         #   thor_scsi/std_machine/accelerator.cc
         #
-    print("<- mult_prm")
     return elems
 
 
 def lat_mult_prm(lat_prop, mult_prm_name, mpole_n):
-    print(lat_prop)
     elems = [elem for elem in lat_prop._lattice]
     mult_family = lat_prop._lattice.elements_with_name(mult_prm_name)
     elems = mult_prm(lat_prop, elems, mult_family, mpole_n)
@@ -86,13 +124,13 @@ def lat_mult_prm(lat_prop, mult_prm_name, mpole_n):
 # Work-around for C++ virtual function -> Python mapping issue.
 # See function above:
 #   mult_prm
-def propagate(lat_ptc, lat):
+def compute_map(lat_prop, lat):
     M = gtpsa.ss_vect_tpsa(
-        lat_ptc._desc, lat_ptc._no, lat_ptc._nv,
-        index_mapping=lat_ptc._named_index)
+        lat_prop._desc, lat_prop._no, lat_prop._nv,
+        index_mapping=lat_prop._named_index)
     M.set_identity()
     for k in range(len(lat)):
-        lat[k].propagate(lat_ptc._model_state, M)
+        lat[k].propagate(lat_prop._model_state, M)
     return M
 
 
@@ -103,64 +141,15 @@ def zero_sext(lat_prop, b3_list):
         lat_prop.set_b_n_fam(b3_name, MultipoleIndex.sextupole, 0e0)
 
 
-def param_dep(lat_prop):
-    b3_list = ["sf_h", "sd1", "sf_e_h", "sd2"]
-    zero_sext(lat_prop, b3_list)
-
-    if False:
-        lat_prop._named_index = gtpsa.IndexMapping(
-            dict(x=0, px=1, y=2, py=3, delta=4, ct=5))
-        desc = gtpsa.desc(nv, no, nv_prm, no_prm)
-
-        print("\n  C [m]     = {:9.7f}".format(C))
-        print("  phi [deg] = {:5.3f}".format(compute_phi(lat)))
-        print("  nu        = [{:7.5f}, {:7.5f}]".format(nu[X_], nu[Y_]))
-        print("  xi        = [{:7.5f}, {:7.5f}]".format(xi[X_], xi[Y_]))
-
+def param_dep(lat_prop, prm_name):
     lat_prop._named_index = gtpsa.IndexMapping(
         dict(x=0, px=1, y=2, py=3, delta=4, ct=5, K=6))
 
-    if False:
-        nv_prm = 0
-        no_prm = 0
-        lat_prop._desc = gtpsa.desc(lat_prop._nv, lat_prop._no, nv_prm, no_prm)
-        lat_ptc = lat_mult_prm("", lat_prop._lattice, 0)
-    else:
-        nv_prm = 1
-        no_prm = no
-        print(no, nv, nv_prm, no_prm)
-        lat_prop._desc = gtpsa.desc(lat_prop._nv, lat_prop._no, nv_prm, no_prm)
-        print(lat_prop._desc)
-        if True:
-            lat_ptc = lat_mult_prm(lat_prop, b3_list[0], 3)
-        else:
-            lat_ptc = lat_mult_prm(lat_prop, "uq3", 2)
-
-    M = propagate(lat_prop, lat_ptc)
-
-    print("\nM:", M)
-    if not False:
-        prt_map(M, "\nM:")
-
-    if False:
-        M2 = M
-        # Busted.
-        M2.getOrder(M2, 2)
-        print("\n:\n", M2)
-
-    if False:
-        M_inv = ts.inv(M)
-
-        M_M_inv = gtpsa.ss_vect_tpsa(
-            desc, no, nv, index_mapping=lat_prop_named_index)
-        M_M_inv.rcompose(M, M_inv)
-        print("\nM:", M)
-        print("\nM^-1:", M_inv)
-        print("\nM*M^-1:", M_M_inv)
-        print("\nM.x:\n", M.x)
-        print("\nM_M_inv.x:\n", M_M_inv.x)
-        # print("\nM*M^-1:", M_M_inv[0])
-        assert False
+    nv_prm = 1
+    no_prm = no
+    lat_prop._desc = gtpsa.desc(lat_prop._nv, lat_prop._no, nv_prm, no_prm)
+    lat_ptc = lat_mult_prm(lat_prop, prm_name, 3)
+    return lat_ptc
 
 
 ind = ind.index_class()
@@ -177,11 +166,25 @@ cod_eps = 1e-15
 E_0     = 3.0e9
 
 home_dir = os.path.join(
-    os.environ["HOME"], "Nextcloud", "thor_scsi", "JB", "MAX_IV", "max_4u")
-lat_name = "max_4u_g_2"
+    os.environ["HOME"], "Nextcloud", "thor_scsi", "JB", "MAX_IV", "max_iv")
+lat_name = "ake_2"
 file_name = os.path.join(home_dir, lat_name+".lat")
 
 lat_prop = \
     lp.lattice_properties_class(nv, no, nv_prm, no_prm, file_name, E_0, cod_eps)
 
-param_dep(lat_prop)
+b3_list = ["sfoh", "sfmh", "sd"]
+
+zero_sext(lat_prop, b3_list)
+
+lat_ptc = param_dep(lat_prop, b3_list[0])
+
+M = compute_map(lat_prop, lat_ptc)
+
+if False:
+    prt_map(M, "\nM:")
+
+stable, nu, xi = compute_nu_xi(lat_prop, M)
+
+print("\nnu = [{:7.5f}, {:7.5f}]".format(nu[ind.X], nu[ind.Y]))
+print("xi = [{:7.5f}, {:7.5f}]".format(xi[ind.X], xi[ind.Y]))
