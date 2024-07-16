47a1d5c2e6606c92a27fa0273357c26e4079138b
Johan <jbngtssn@gmail.com>
2024-07-16 10:41:21 +0200

    Added use case set_ksi.py.

---
+++ python/examples/use_cases/set_ksi.py
@@ -0,0 +1,15 @@
+"""Use Case:
+     Set the linear chromaticity.
+"""
+
+
+import numpy as np
+
+from thor_scsi.utils import lattice_properties as lp, index_class as ind, \
+    linear_optics as lo, prm_class as pc
+from thor_scsi.utils.output import mat2txt, vec2txt
+
+import gtpsa
+
+
+ind = ind.index_class()
