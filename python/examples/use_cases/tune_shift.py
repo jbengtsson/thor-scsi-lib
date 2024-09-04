1aaf8d4a4d3f9a75f0ae82f5e3cfabc4252a568d
Johan <jbngtssn@gmail.com>
2024-09-04 13:16:00 +0200

    Debugging amplite dependent tune shift formula.

--- python/examples/use_cases/tune_shift.py
+++ python/examples/use_cases/tune_shift.py
@@ -187,23 +187,98 @@ def compute_R(desc, map, A_0, A_1, g, K, gtpsa_prop):
     prt_map(M, "M:", eps=1e-10)
 
 
-def compute_s_ijklm(lat_prop, i, j, k, l, m):
-    eta = \
-        np.array(lat_prop._Twiss.dispersion.sel(phase_coordinate="x").values)
-    alpha = \
-        np.array([lat_prop._Twiss.twiss.sel(plane="x", par="alpha").values,
-                  lat_prop._Twiss.twiss.sel(plane="y", par="alpha").values]
+def get_h_ijklm(g, i, j, k, l, m):
+    return g.get([i, j, k, l, m, 0, 0])
+
+
+def compute_h_ijklm(lat_prop, i, j, k, l, m):
+    eta = np.array(lat_prop._Twiss.dispersion.sel(phase_coordinate="x").values)
+    alpha = np.array(
+        [lat_prop._Twiss.twiss.sel(plane="x", par="alpha").values,
+         lat_prop._Twiss.twiss.sel(plane="y", par="alpha").values]
     )
-    beta = \
-        np.array([lat_prop._Twiss.twiss.sel(plane="x", par="beta").values,
-                  lat_prop._Twiss.twiss.sel(plane="y", par="beta").values]
+    beta = np.array([lat_prop._Twiss.twiss.sel(plane="x", par="beta").values,
+                     lat_prop._Twiss.twiss.sel(plane="y", par="beta").values]
     )
     dnu = np.array([lat_prop._Twiss.twiss.sel(plane="x", par="nu").values,
                     lat_prop._Twiss.twiss.sel(plane="y", par="nu").values])
+    dmu = 2e0*np.pi*dnu
     nu = np.array([lat_prop._Twiss.twiss.sel(plane="x", par="nu").values[-1],
                    lat_prop._Twiss.twiss.sel(plane="y", par="nu").values[-1]])
 
-    s_ijklm = 0e0
+    h_re = 0e0
+    h_im = 0e0
+    for n in range(len(lat_prop._lattice)):
+        if type(lat_prop._lattice[n]) == ts.Sextupole:
+            m_x = i + j
+            m_y = k + l
+            n_x = i - j
+            n_y = k - l
+            L = lat_prop._lattice[n].get_length()
+            b3xL = lat_prop._lattice[n].get_multipoles(). \
+                get_multipole(MpoleInd.sext).real*L
+            if b3xL != 0e0:
+                phi = n_x*dmu[ind.X, n-1] + n_y*dmu[ind.Y, n-1]
+                h_abs = b3xL*beta[ind.X, n-1]**(m_x/2e0) \
+                    *beta[ind.Y, n-1]**(m_y/2e0)
+                dh_re = h_abs*np.cos(phi)
+                dh_im = h_abs*np.sin(phi)
+                h_re += h_abs*np.cos(phi)
+                h_im -= h_abs*np.sin(phi)
+
+    return np.array([h_re, h_im])
+
+
+def compute_hh(lat_prop, h):
+    h_re = new.tpsa()
+    h_im = new.tpsa()
+
+    h = h.get_mns_1(1, 3)
+    h.CtoR(h_re, h_im)
+    h_re.print("h_re", 1e-11)
+    h_im.print("h_im", 1e-11)
+
+    h_21000 = -1e0/8e0*compute_h_ijklm(lat_prop, 2, 1, 0, 0, 0)
+    h_30000 = -1e0/24e0*compute_h_ijklm(lat_prop, 3, 0, 0, 0, 0)
+    h_10110 =  1e0/4e0*compute_h_ijklm(lat_prop, 1, 0, 1, 1, 0)
+    h_10200 =  1e0/8e0*compute_h_ijklm(lat_prop, 1, 0, 2, 0, 0)
+    h_10020 =  1e0/8e0*compute_h_ijklm(lat_prop, 1, 0, 0, 2, 0)
+
+    h_21000_ref = [get_h_ijklm(h_re, 2, 1, 0, 0, 0),
+                   get_h_ijklm(h_im, 2, 1, 0, 0, 0)]
+    h_30000_ref = [get_h_ijklm(h_re, 3, 0, 0, 0, 0),
+                   get_h_ijklm(h_im, 3, 0, 0, 0, 0)]
+    h_10110_ref = [get_h_ijklm(h_re, 1, 0, 1, 1, 0),
+                   get_h_ijklm(h_im, 1, 0, 1, 1, 0)]
+    h_10200_ref = [get_h_ijklm(h_re, 1, 0, 2, 0, 0),
+                   get_h_ijklm(h_im, 1, 0, 2, 0, 0)]
+    h_10020_ref = [get_h_ijklm(h_re, 1, 0, 0, 2, 0),
+                   get_h_ijklm(h_im, 1, 0, 0, 2, 0)]
+
+    print(f"\n  h_21000 = [{h_21000[0]:12.5e}, {h_21000[1]:12.5e}] ([{h_21000_ref[0]/2e0:12.5e}, {h_21000_ref[1]/2e0:12.5e}]))")
+    print(f"  h_30000 = [{h_30000[0]:12.5e}, {h_30000[1]:12.5e}] ([{h_30000_ref[0]/2e0:12.5e}, {h_30000_ref[1]/2e0:12.5e}]))")
+    print(f"  h_10110 = [{h_10110[0]:12.5e}, {h_10110[1]:12.5e}] ([{h_10110_ref[0]/2e0:12.5e}, {h_10110_ref[1]/2e0:12.5e}]))")
+    print(f"  h_10200 = [{h_10200[0]:12.5e}, {h_10200[1]:12.5e}] ([{h_10200_ref[0]/2e0:12.5e}, {h_10200_ref[1]/2e0:12.5e}]))")
+    print(f"  h_10020 = [{h_10020[0]:12.5e}, {h_10020[1]:12.5e}] ([{h_10020_ref[0]/2e0:12.5e}, {h_10020_ref[1]/2e0:12.5e}]))")
+
+
+def compute_g_ijklm(lat_prop, i, j, k, l, m):
+    eta = np.array(lat_prop._Twiss.dispersion.sel(phase_coordinate="x").values)
+    alpha = np.array(
+        [lat_prop._Twiss.twiss.sel(plane="x", par="alpha").values,
+         lat_prop._Twiss.twiss.sel(plane="y", par="alpha").values]
+    )
+    beta = np.array([lat_prop._Twiss.twiss.sel(plane="x", par="beta").values,
+                     lat_prop._Twiss.twiss.sel(plane="y", par="beta").values]
+    )
+    dnu = np.array([lat_prop._Twiss.twiss.sel(plane="x", par="nu").values,
+                    lat_prop._Twiss.twiss.sel(plane="y", par="nu").values])
+    dmu = 2e0*np.pi*dnu
+    nu = np.array([lat_prop._Twiss.twiss.sel(plane="x", par="nu").values[-1],
+                   lat_prop._Twiss.twiss.sel(plane="y", par="nu").values[-1]])
+
+    g_re_ijklm = 0e0
+    g_im_ijklm = 0e0
     for n in range(len(lat_prop._lattice)):
         if type(lat_prop._lattice[n]) == ts.Sextupole:
             m_x = i + j
@@ -214,28 +289,39 @@ def compute_s_ijklm(lat_prop, i, j, k, l, m):
             b3xL = \
                 lat_prop._lattice[n].get_multipoles(). \
                 get_multipole(MpoleInd.sext).real*L
-            s_ijklm += \
-                b3xL*beta[ind.X, n]**(m_x/2e0)*beta[ind.Y, n]**(m_y/2e0) \
-                *np.cos(abs(n_x*dnu[ind.X, n]+n_y*dnu[ind.Y, n] \
-                         -np.pi*(n_x*nu[ind.X]+n_y*nu[ind.Y]))) \
+            g_abs = b3xL*beta[ind.X, n]**(m_x/2e0)*beta[ind.Y, n]**(m_y/2e0)
+            phi = n_x*dnu[ind.X, n] + n_y*dnu[ind.Y, n]
+            g_re_ijklm += g_abs*np.cos(phi) \
                 /np.sin(np.pi*(n_x*nu[ind.X]+n_y*nu[ind.Y]))
-    return s_ijklm
+            g_im_ijklm += g_abs*np.sin(phi) \
+                /np.sin(np.pi*(n_x*nu[ind.X]+n_y*nu[ind.Y]))
+    return g_im_ijklm, g_re_ijklm
 
+def compute_g(lat_prop, g):
+    g_re = new.tpsa()
+    g_im = new.tpsa()
 
-def compute_ampl_dep_orbit(lat_prop):
-    s_21000 =  3e0/8e0*compute_s_ijklm(lat_prop, 2, 1, 0, 0, 0)
-    s_30000 =  1e0/8e0*compute_s_ijklm(lat_prop, 3, 0, 0, 0, 0)
-    s_10110 =  1e0/8e0*compute_s_ijklm(lat_prop, 1, 0, 1, 1, 0)
-    s_10200 =  1e0/8e0*compute_s_ijklm(lat_prop, 2, 1, 0, 0, 0)
-    s_10020 = -1e0/8e0*compute_s_ijklm(lat_prop, 2, 1, 0, 0, 0)
+    g.CtoR(g_re, g_im)
 
-    print("\n  s_21000 = {:12.5e}".format(s_21000))
-    print("  s_30000 = {:12.5e}".format(s_30000))
-    print("  s_10110 = {:12.5e}".format(s_10110))
-    print("  s_10200 = {:12.5e}".format(s_10200))
-    print("  s_10020 = {:12.5e}".format(s_10020))
+    g_21000 =  1e0/8e0*compute_g_ijklm(lat_prop, 2, 1, 0, 0, 0)[0]
+    g_30000 =  1e0/48e0*compute_g_ijklm(lat_prop, 3, 0, 0, 0, 0)[0]
+    g_10110 = -1e0/8e0*compute_g_ijklm(lat_prop, 1, 0, 1, 1, 0)[0]
+    g_10200 = -1e0/16e0*compute_g_ijklm(lat_prop, 1, 0, 2, 0, 0)[0]
+    g_10020 = -1e0/16e0*compute_g_ijklm(lat_prop, 1, 0, 0, 2, 0)[0]
 
-    return s_21000, s_30000, s_10110, s_10200, s_10020
+    g_21000_ref = get_h_ijklm(g_im, 2, 1, 0, 0, 0)
+    g_30000_ref = get_h_ijklm(g_im, 3, 0, 0, 0, 0)
+    g_10110_ref = get_h_ijklm(g_im, 1, 0, 1, 1, 0)
+    g_10200_ref = get_h_ijklm(g_im, 1, 0, 2, 0, 0)
+    g_10020_ref = get_h_ijklm(g_im, 1, 0, 0, 2, 0)
+
+    print(f"\n  g_21000 = {g_21000:12.5e} ({g_21000_ref:12.5e})")
+    print(f"  g_30000 = {g_30000:12.5e} ({g_30000_ref:12.5e})")
+    print(f"  g_10110 = {g_10110:12.5e} ({g_10110_ref:12.5e})")
+    print(f"  g_10200 = {g_10200:12.5e} ({g_10200_ref:12.5e}")
+    print(f"  g_10020 = {g_10020:12.5e} ({g_10020_ref:12.5e}")
+
+    return g_21000, g_30000, g_10110, g_10200, g_10020
 
 
 def chk_types():
@@ -274,17 +360,27 @@ def chk_Poisson_bracket():
     Id = new.ss_vect_tpsa()
     Id.set_identity()
 
-    (1e0*Id.x).poisbra(1e0*Id.px, 6).print("[x, p_x]")
-    (1e0*Id.px).poisbra(1e0*Id.x, 6).print("[p_x, x]")
-    (1e0*Id.x).poisbra(1e0*Id.x, 6).print("[x, x]")
-    (1e0*Id.px).poisbra(1e0*Id.px, 6).print("[p_x, p_x]")
+    ps_dim = 6
 
-    (1e0*Id.y).poisbra(1e0*Id.py, 6).print("[y, p_y]")
-    (1e0*Id.y).poisbra(1e0*Id.y, 6).print("[y, y]")
-    (1e0*Id.py).poisbra(1e0*Id.py, 6).print("[p_y, p_y]")
+    Id.x.to_tpsa().poisbra(Id.px.to_tpsa(), ps_dim).print("[x, p_x]")
+    Id.px.to_tpsa().poisbra(Id.x.to_tpsa(), ps_dim).print("[p_x, x]")
+    Id.x.to_tpsa().poisbra(Id.x.to_tpsa(), ps_dim).print("[x, x]")
+    Id.px.to_tpsa().poisbra(Id.px.to_tpsa(), ps_dim).print("[p_x, p_x]")
 
-    (1e0*Id.x).poisbra(1e0*Id.y, 6).print("[x, y]")
-    (1e0*Id.x).poisbra(1e0*Id.py, 6).print("[x, p_y]")
+    Id.y.to_tpsa().poisbra(Id.py.to_tpsa(), ps_dim).print("[y, p_y]")
+    Id.y.to_tpsa().poisbra(Id.y.to_tpsa(), ps_dim).print("[y, y]")
+    Id.py.to_tpsa().poisbra(Id.py.to_tpsa(), ps_dim).print("[p_y, p_y]")
+
+    Id.x.to_tpsa().poisbra(Id.y.to_tpsa(), ps_dim).print("[x, y]")
+    Id.x.to_tpsa().poisbra(Id.py.to_tpsa(), ps_dim).print("[x, p_y]")
+
+    pow(Id.x.to_tpsa(), 3).poisbra(Id.x.to_tpsa(), ps_dim). \
+        print("[x^3, x]", 1e-11)
+    pow(Id.x.to_tpsa(), 3).poisbra(Id.px.to_tpsa(), ps_dim). \
+        print("[x^3, p_x]", 1e-11)
+    pow(Id.x.to_tpsa(), 3).poisbra(pow(Id.x.to_tpsa(), 2)
+                                   *Id.px.to_tpsa(), ps_dim). \
+        print("[x^3,  x*p_x^2]", 1e-11)
 
 
 def chk_compose():
@@ -308,8 +404,126 @@ def chk_compose():
     print("\nmap_2", map_2)
 
 
+def pb(f, g):
+    a = new.tpsa()
+    ps_dim = 6
+    for k in range(0, 1):
+        print("k =", k)
+        fp = f.deriv(2*k+2)
+        gp = g.deriv(2*k+1)
+        print(fp.get_description())
+        print(gp.get_description())
+        fp.print("fp", 1e-11)
+        gp.print("gp", 1e-11)
+        fxg = fp*gp
+        fxg.print("fxg", 1e-11)
+        a += f.deriv(2*k+1)*g.deriv(2*k+2) - f.deriv(2*k+2)*g.deriv(2*k+1)
+    return a
+
+
+def compute_ampl_dep_orbit_tpsa(g):
+    x_avg       = new.tpsa()
+    x_avg_re    = new.tpsa()
+    x_avg_im    = new.tpsa()
+    x3_avg      = new.tpsa()
+    x3_avg_re   = new.tpsa()
+    x3_avg_im   = new.tpsa()
+    x_y2_avg    = new.tpsa()
+    x_y2_avg_re = new.tpsa()
+    x_y2_avg_im = new.tpsa()
+
+    Id          = new.ss_vect_tpsa()
+
+    ps_dim = 6
+
+    Id.set_identity()
+
+    g = g.get_mns_1(1, 3)
+    g.print("g", 1e-11)
+
+    x_avg = g.poisbra(Id.x.to_tpsa(), ps_dim)
+    x_avg = compose_bs(x_avg, A_1)
+    x_avg = x_avg.get_mns_1(1, 2)
+    x_avg.CtoR(x_avg_re, x_avg_im)
+    x_avg_re.print("<x_re>", 1e-11)
+    s_11000 = x_avg_re.get([1, 1, 0, 0, 0, 0, 0])
+    s_00110 = x_avg_re.get([0, 0, 1, 1, 0, 0, 0])
+
+    x3_avg = g.poisbra(pow(Id.x.to_tpsa(), 3), ps_dim)
+    x3_avg.print("<x3>", 1e-11)
+    x3_avg = compose_bs(x3_avg, A_1)
+    x3_avg.print("<x3>", 1e-11)
+    x3_avg.CtoR(x3_avg_re, x3_avg_im)
+    x3_avg_re.print("<x^3_re>")
+    s_22000 = x3_avg_re.get([2, 2, 0, 0, 0, 0, 0])
+    s_11110_1 = x3_avg_re.get([1, 1, 1, 1, 0, 0, 0])
+
+    x_y2_avg = g.poisbra(Id.x.to_tpsa()*pow(Id.y.to_tpsa(), 2), ps_dim)
+    x_y2_avg = compose_bs(x_y2_avg, A_1)
+    x_y2_avg.CtoR(x_y2_avg_re, x_y2_avg_im)
+    x_y2_avg_re.print("<x*y^2_re>")
+    s_11110_2 = x_y2_avg_re.get([1, 1, 1, 1, 0, 0, 0])
+    s_00220 = x_y2_avg_re.get([0, 0, 2, 2, 0, 0, 0])
+
+    print(f"\n  s_11000   = {s_11000:10.3e}")
+    print(f"  s_00110   = {s_00110:10.3e}")
+    print(f"  s_22000   = {s_22000:10.3e}")
+    print(f"  s_11110_1 = {s_11110_1:10.3e}")
+    print(f"  s_11110_2 = {s_11110_2:10.3e}")
+    print(f"  s_00220   = {s_00220:10.3e}")
+
+
+def compute_ampl_dep_orbit_tpsa_2(g):
+    x_avg       = new.tpsa()
+    x_avg_re    = new.tpsa()
+    x_avg_im    = new.tpsa()
+    x3_avg      = new.tpsa()
+    x3_avg_re   = new.tpsa()
+    x3_avg_im   = new.tpsa()
+    x_y2_avg    = new.tpsa()
+    x_y2_avg_re = new.tpsa()
+    x_y2_avg_im = new.tpsa()
+
+    Id          = new.ss_vect_tpsa()
+
+    Id.set_identity()
+
+    x_avg = pb(g, Id.x.to_tpsa())
+    x_avg = compose_bs(x_avg, A_1)
+    x_avg.CtoR(x_avg_re, x_avg_im)
+    x_avg_re.print("<x_re>", 1e-11)
+    s_11000 = x_avg_re.get([1, 1, 0, 0, 0, 0, 0])
+    s_00110 = x_avg_re.get([0, 0, 1, 1, 0, 0, 0])
+
+    f = pb(g, pow(Id.x.to_tpsa(), 3))
+    f.print("f", 1e-11)
+    assert False
+    x3_avg = pb(g, pow(Id.x.to_tpsa(), 3))
+    x3_avg.print("<x3>", 1e-11)
+    x3_avg = compose_bs(x3_avg, A_1)
+    x3_avg.print("<x3>", 1e-11)
+    x3_avg.CtoR(x3_avg_re, x3_avg_im)
+    x3_avg_re.print("<x^3_re>")
+    s_22000 = x3_avg_re.get([2, 2, 0, 0, 0, 0, 0])
+    s_11110_1 = x3_avg_re.get([1, 1, 1, 1, 0, 0, 0])
+
+    x_y2_avg = pb(g, Id.x.to_tpsa()*pow(Id.y.to_tpsa(), 2))
+    x_y2_avg = compose_bs(x_y2_avg, A_1)
+    x_y2_avg.CtoR(x_y2_avg_re, x_y2_avg_im)
+    x_y2_avg_re.print("<x*y^2_re>")
+    s_11110_2 = x_y2_avg_re.get([1, 1, 1, 1, 0, 0, 0])
+    s_00220 = x_y2_avg_re.get([0, 0, 2, 2, 0, 0, 0])
+
+    print(f"\n  s_11000   = {s_11000:10.3e}")
+    print(f"  s_00110   = {s_00110:10.3e}")
+    print(f"  s_22000   = {s_22000:10.3e}")
+    print(f"  s_11110_1 = {s_11110_1:10.3e}")
+    print(f"  s_11110_2 = {s_11110_2:10.3e}")
+    print(f"  s_00220   = {s_00220:10.3e}")
+
+
 gtpsa_prop = gtpsa_prop_class()
-gtpsa_prop_class.no = 4
+gtpsa_prop_class.no = 5
 gtpsa_prop_class.desc = gtpsa.desc(gtpsa_prop.nv, gtpsa_prop.no)
 
 cod_eps = 1e-15
@@ -357,85 +571,25 @@ if False:
 M = compute_map(lat_prop)
 print("\nM:", M)
 
-if False:
-    h_re = new.tpsa()
-    h_im = new.tpsa()
-
-    h = compute_h(desc, M)
-    h.CtoR(h_re, h_im)
-    h_re = compose_bs(h_re, Id_scl)
-    h_im = compose_bs(h_im, Id_scl)
+if not False:
+    h = compute_h(M)
+    h.print("h", 1e-20)
 
 if not False:
     A_0, A_1, R, g, K = compute_map_normal_form(M)
+    print("\nA_0:", A_0)
+    print("\nA_1:", A_1)
+    g.print("g", 1e-10)
+    K.print("K", 1e-10)
 
 if False:
     compute_M(K, A_0, A_1, g)
 if False:
     compute_R(M, A_0, A_1, g, K)
 
-g_re = new.tpsa()
-g_im = new.tpsa()
-K_re = new.tpsa()
-K_im = new.tpsa()
+compute_hh(lat_prop, h)
+compute_g(lat_prop, g)
 
-# g_re is zero.
-g.CtoR(g_re, g_im)
-# K_im is zero.
-K.CtoR(K_re, K_im)
+# compute_ampl_dep_orbit_tpsa_2(g)
 
-if False:
-    g_re = compose_bs(g_re, Id_scl)
-    g_im = compose_bs(g_im, Id_scl)
-    K_re = compose_bs(K_re, Id_scl)
-    K_im = compose_bs(K_im, Id_scl)
-
-# g.print("g")
-# K_re.print("K_re", 1e-10)
-
-x_avg = new.tpsa()
-x_avg_re = new.tpsa()
-x_avg_im = new.tpsa()
-x3_avg = new.tpsa()
-x3_avg_re = new.tpsa()
-x3_avg_im = new.tpsa()
-x_y2_avg = new.tpsa()
-x_y2_avg_re = new.tpsa()
-x_y2_avg_im = new.tpsa()
-Id = new.ss_vect_tpsa()
-
-Id.set_identity()
-
-g = g.get_mns_1(1, 4)
-g.print("g", 1e-9)
-
-x_avg = g.poisbra(Id.x.to_tpsa(), 4)
-x_avg = compose_bs(x_avg, A_1)
-x_avg = x_avg.get_mns_1(1, 2)
-x_avg.CtoR(x_avg_re, x_avg_im)
-x_avg_re.print("<x_re>")
-s_11000 = x_avg_re.get([1, 1, 0, 0, 0, 0, 0])
-s_00110 = x_avg_re.get([0, 0, 1, 1, 0, 0, 0])
-
-x3_avg = g.poisbra(Id.x.to_tpsa()*Id.x.to_tpsa()*Id.x.to_tpsa(), 4)
-x3_avg = compose_bs(x3_avg, A_1)
-x3_avg.CtoR(x3_avg_re, x3_avg_im)
-x3_avg_re.print("<x^3_re>")
-s_22000 = x3_avg_re.get([2, 2, 0, 0, 0, 0, 0])
-s_11110_1 = x3_avg_re.get([1, 1, 1, 1, 0, 0, 0])
-
-x_y2_avg = g.poisbra(Id.x.to_tpsa()*Id.y.to_tpsa()*Id.y.to_tpsa(), 4)
-x_y2_avg = compose_bs(x_y2_avg, A_1)
-x_y2_avg.CtoR(x_y2_avg_re, x_y2_avg_im)
-x_y2_avg_re.print("<x*y^2_re>")
-s_11110_2 = x_y2_avg_re.get([1, 1, 1, 1, 0, 0, 0])
-s_00220 = x_y2_avg_re.get([0, 0, 2, 2, 0, 0, 0])
-
-print(f"\n  s_11000   = {s_11000:10.3e}")
-print(f"  s_00110   = {s_00110:10.3e}")
-print(f"  s_22000   = {s_22000:10.3e}")
-print(f"  s_11110_1 = {s_11110_1:10.3e}")
-print(f"  s_11110_2 = {s_11110_2:10.3e}")
-print(f"  s_00220   = {s_00220:10.3e}")
-
-compute_ampl_dep_orbit(lat_prop)
+# compute_ampl_dep_orbit(lat_prop)
