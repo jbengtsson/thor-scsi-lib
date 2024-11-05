7c2326f5ca32b806bd95fb99ec681bb2225af9f8
Johan <jbngtssn@gmail.com>
2024-11-05 16:22:18 +0100

    Added use case ampl_orb.py.

---
+++ python/examples/use_cases/ampl_orb.py
@@ -0,0 +1,96 @@
+"""Use Case:
+     Compute, print, and display the global linear optics properties for a
+     lattice.
+"""
+
+
+import os
+import sys
+from dataclasses import dataclass
+from typing import ClassVar
+
+import gtpsa
+
+import numpy as np
+
+from thor_scsi.utils import lattice_properties as lp, nonlin_dyn as nld_cl, \
+    index_class as ind
+
+
+ind = ind.index_class()
+
+
+@dataclass
+class gtpsa_prop:
+    # GTPSA properties.
+    # Number of variables - phase-space coordinates & 1 for parameter
+    #dependence.
+    nv: ClassVar[int] = 6 + 1
+    # Max order.
+    no: ClassVar[int] = 1
+    # Number of parameters.
+    nv_prm: ClassVar[int] = 0
+    # Parameters max order.
+    no_prm: ClassVar[int] = 0
+    # Index.
+    named_index = gtpsa.IndexMapping(dict(x=0, px=1, y=2, py=3, delta=4, ct=5))
+    # Descriptor
+    desc : ClassVar[gtpsa.desc]
+
+
+def compute_optics(lat_prop):
+    try:
+        # Compute Twiss parameters along lattice.
+        if not lat_prop.comp_per_sol():
+            print("\ncomp_per_sol: unstable")
+            raise ValueError
+
+        # Compute radiation properties.
+        stable, stable_rad = lat_prop.compute_radiation()
+        print(stable, stable_rad)
+        if not stable:
+            print("\ncompute_radiation: unstable")
+            raise ValueError
+    except ValueError:
+        assert False
+
+
+# TPSA max order.
+gtpsa_prop.no = 6
+
+cod_eps = 1e-15
+E_0     = 3.0e9
+
+A_max     = np.array([6e-3, 3e-3])
+beta_inj  = np.array([3.0, 3.0])
+delta_max = 3e-2
+
+home_dir = os.path.join(
+    os.environ["HOME"], "Nextcloud", "thor_scsi", "JB", "MAX_IV")
+lat_name = sys.argv[1]
+file_name = os.path.join(home_dir, lat_name+".lat")
+
+lat_prop = lp.lattice_properties_class(gtpsa_prop, file_name, E_0, cod_eps)
+
+lat_prop.prt_lat("lat_prop_lat.txt")
+
+print("\nCircumference [m]      = {:7.5f}".format(lat_prop.compute_circ()))
+print("Total bend angle [deg] = {:7.5f}".format(lat_prop.compute_phi_lat()))
+
+b_3_list = ["s1", "s2", "s3", "s4"]
+nld = nld_cl.nonlin_dyn_class(lat_prop, A_max, beta_inj, delta_max, b_3_list)
+if False:
+    nld.zero_mult(lat_prop, 3)
+    nld.zero_mult(lat_prop, 4)
+
+compute_optics(lat_prop)
+
+lat_prop.prt_lat_param()
+lat_prop.prt_rad()
+lat_prop.prt_M()
+lat_prop.prt_M_rad()
+
+if not False:
+    nld.compute_map(lat_prop, gtpsa_prop.no)
+    nld.compute_nl(lat_prop)
+    nld.prt_nl(lat_prop)
