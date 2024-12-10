1b93a87922483961d6700a5a64dcd6aca2830480
Johan <jbngtssn@gmail.com>
2024-12-10 21:34:00 +0100

    Code refactoring.

--- python/examples/use_cases/compute_orbit.py
+++ python/examples/use_cases/compute_orbit.py
@@ -13,6 +13,8 @@ import sys
 from dataclasses import dataclass
 from typing import ClassVar
 
+import copy as _copy
+
 import math
 import numpy as np
 
@@ -77,6 +79,7 @@ def get_lat(home_dir, lat_name, E_0):
     # Computes element s location: lat_prop._Twiss.s[].
     compute_optics(lat_prop)
     lat_prop._model_state.radiation = False
+    lat_prop._model_state.Cavity_on = False
 
     if False:
         lat_prop.prt_lat_param()
@@ -90,69 +93,100 @@ def get_lat(home_dir, lat_name, E_0):
     return lat_prop
 
 
+def get_phi(el):
+    L = el.get_length()
+    if el.get_curvature() is not None:
+        phi = math.degrees(L*el.get_curvature())
+    else:
+        phi = 0e0
+    return phi
+
+
 def prt_bend(lat_ref, lat_prop):
     file_name = "prt_bend.txt"
     file = open(file_name, "w")
 
     phi_tot_ref = 0e0
     phi_tot = 0e0
-    print("#  k      s        name                  L              phi"
-          "         b_1xL   b_1xL_sum\n"
-          "#        [m]                            [m]            [deg]"
-          "        [mrad]    [mrad]", file=file)
+    dphi_tot = 0e0
     b_1xL_sum = 0e0
+    print("#  k      s        name                  L              phi"
+          "          dphi    b_1xL  b_1xL_sum\n"
+          "#        [m]                            [m]            [deg]"
+          "        [mrad]  [mrad]   [mrad]", file=file)
     for k in range(len(lat_ref._lattice)):
-        if (type(lat_prop._lattice[k]) == ts.Bending) or \
-           (type(lat_prop._lattice[k]) == ts.Quadrupole):
-            b = lat_prop._lattice[k]
-            h = b.get_curvature()
-            if h != 0e0:
-                b_ref = lat_ref._lattice[k]
-                fam_name_ref = b_ref.name
-                index_ref = b_ref.index
-                s = lat_ref._Twiss.s[index_ref]
-                L_ref = b_ref.get_length()
-                phi_ref = math.degrees(L_ref*b_ref.get_curvature())
-                phi_tot_ref += phi_ref
+        el_ref = lat_ref._lattice[k]
+        if (type(el_ref) == ts.Bending) or (type(el_ref) == ts.Quadrupole):
+            fam_name_ref = el_ref.name
+            s = lat_ref._Twiss.s[k]
+            L_ref = el_ref.get_length()
+            phi_ref = get_phi(el_ref)
+            phi_tot_ref += phi_ref
 
-                fam_name = b.name
-                L = b.get_length()
-                phi = math.degrees(L*h)
-                phi_tot += phi
+            el = lat_prop._lattice[k]
+            fam_name = el.name
+            L = el.get_length()
+            phi = get_phi(el)
+            dphi = phi - phi_ref
+            phi_tot += phi
+            dphi_tot += dphi
 
-                b_1xL = math.radians(phi-phi_ref)
-                b_1xL_sum += b_1xL
-                b.get_multipoles().set_multipole(1, b_1xL/L)
+            b_1xL = math.radians(dphi)
+            b_1xL_sum += b_1xL
+            el.get_multipoles().set_multipole(1, b_1xL/L)
 
-                print("  {:3d}  {:6.3f}  {:8s} ({:8s})  {:5.3f} ({:5.3f})" \
-                      "  {:6.3f} ({:6.3f})  {:6.3f}   {:6.3f}".
-                      format(index_ref, s, fam_name_ref, fam_name, L_ref, L,
-                             phi_ref, phi, 1e3*b_1xL, 1e3*b_1xL_sum), file=file)
-    print("\n  phi_tot = {:8.5f} ({:8.5f})".format(phi_tot_ref, phi_tot),
-        file=file)
+            print("  {:3d}  {:6.3f}  {:8s} ({:8s})  {:5.3f} ({:5.3f})" \
+                  "  {:6.3f} ({:6.3f})  {:6.3f}  {:6.3f}   {:6.3f}".
+                  format(k, s, fam_name_ref, fam_name, L_ref, L,
+                         phi_ref, phi, 1e3*dphi*np.pi/180e0, 1e3*b_1xL,
+                         1e3*b_1xL_sum), file=file)
+    print("\n  phi_tot = {:8.5f} ({:8.5f}) dphi = {:9.3e}".
+          format(phi_tot_ref, phi_tot, dphi_tot), file=file)
     file.close()
+
+
+def compute_dx(lat_prop):
+    eps = 1e-10
+    M_0 = gtpsa.ss_vect_tpsa(lat_prop._desc, 1)
+    M_1 = gtpsa.ss_vect_tpsa(lat_prop._desc, 1)
+    p_x = 0e0
+    print("\ncompute_dx:")
+    while True:
+        M_0.set_zero()
+        M_0.px += p_x
+        M_1 = _copy.copy(M_0)
+        lat_prop._lattice.propagate(lat_prop._model_state, M_1)
+        dp_x = (M_1+M_0).cst().px/2e0
+        p_x -= dp_x
+        print("  p_x = {:10.3e}".format(p_x))
+        if abs(dp_x) < eps:
+            break
+    return p_x
 
 
 def prt_orbit(lat_prop):
     file_name = "prt_orbit.txt"
     file = open(file_name, "w")
 
-    # r = co.compute_closed_orbit(
-    #     lat_prop._lattice, lat_prop._model_state, delta=0e0, max_iter=10,
-    #     eps=1e-10, desc=lat_prop._desc)
+    dp_x = compute_dx(lat_prop)
 
-    M = gtpsa.ss_vect_tpsa(lat_prop._desc, 1)
-    M.set_identity()
-    # M.px += -0.448e-3
-    # M += r.x0
-    print("# k               s    type     x       p_x\n"
-          "#                [m]           [mm]    [mrad]", file=file)
+    M_0 = gtpsa.ss_vect_tpsa(lat_prop._desc, 1)
+    M_1 = gtpsa.ss_vect_tpsa(lat_prop._desc, 1)
+    M_1.set_zero()
+    M_1.px += dp_x
+    print("# k               s    type     x       p_x      dp_x\n"
+          "#                [m]           [mm]    [mrad]   [mrad]",
+          file=file)
     for k in range(len(lat_prop._lattice)):
-        lat_prop._lattice.propagate(lat_prop._model_state, M, k, 1)
+        M_0 = _copy.copy(M_1)
+        lat_prop._lattice.propagate(lat_prop._model_state, M_1, k, 1)
         s = lat_ref._Twiss.s[k]
-        print("{:3d}  {:8s}  {:6.3f}  {:4.1f}  {:7.3f}  {:7.3f}".format(
-            k, lat_prop._lattice[k].name, s, lat_prop._type_code[k],
-            1e3*M.cst().x, 1e3*M.cst().px), file=file)
+        el = lat_prop._lattice[k]
+        print("{:3d}  {:8s}  {:6.3f}  {:4.1f}  {:7.3f}  {:7.3f}  {:7.3f}".
+              format(
+                  k, el.name, s, lat_prop._type_code[k],
+                  1e3*M_1.cst().x, 1e3*M_1.cst().px,
+                  1e3*(M_1-M_0).cst().px), file=file)
 
 
 # TPSA max order.
@@ -170,11 +204,12 @@ home_dir = os.path.join(
 home_dir_1 = os.path.join(home_dir, "max_iv")
 home_dir_2 = os.path.join(home_dir, "max_4u")
 lat_ref = get_lat(home_dir_1, "max_iv_baseline", E_0)
-lat_prop = get_lat(home_dir_2, "max_4u_sp_jb_5", E_0)
+lat_prop = get_lat(home_dir_2, "max_4u_sp_jb_5",
+                   E_0)
 
 prt_bend(lat_ref, lat_prop)
 
-b_n_list = ["q1", "q2", "q3", "s1", "s2", "s3", "s4", "o1", "o2", "o3"]
+b_n_list = [ "s1_f1", "s2_f1", "s3_f1", "s4_f1", "o1_f1", "o2_f1", "o3_f1"]
 nld = nld_cl.nonlin_dyn_class(lat_prop, A_max, beta_inj, delta_max, b_n_list)
 nld.zero_mult(lat_prop, 2)
 nld.zero_mult(lat_prop, 3)
