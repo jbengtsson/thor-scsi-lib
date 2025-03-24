a9d54e7c71661293ba84c44049439c9ca2ace1d6
Johan <jbngtssn@gmail.com>
2025-03-24 09:25:35 +0100

    Code refactoring.

--- python/examples/use_cases/compute_orbit_2.py
+++ python/examples/use_cases/compute_orbit_2.py
@@ -106,76 +106,73 @@ def compute_layout(lat_prop):
     p_x_buf = np.array(p_x_buf)
     X_cs = CubicSpline(s_buf, X_buf, bc_type="natural")
     Y_cs = CubicSpline(s_buf, Y_buf, bc_type="natural")
-    return s_buf, X_cs, Y_cs, p_x_buf
+    return s_buf, X_cs, Y_cs
 
 
-def compute_orbit(s_ref, X_ref, Y_ref, p_x_ref, X, Y, k):
-    Dx = X(s_ref[k]) - X_ref(s_ref[k])
-    Dy = Y(s_ref[k]) - Y_ref(s_ref[k])
+def compute_orbit(s, X_ref, Y_ref, X_cs, Y_cs):
+    Dx = X_cs(s) - X_ref(s)
+    Dy = Y_cs(s) - Y_ref(s)
     dxy = np.sqrt(Dx**2+Dy**2)
-    dx = np.sqrt((Dx*np.sin(p_x_ref[k]))**2+(Dy*np.cos(p_x_ref[k]))**2)
-    return Dx, Dy, dxy, dx
+    return Dx, Dy, dxy
 
 
-def prt_layout(lat_prop, file_name, s, X, Y, p_x):
+def prt_layout(lat_prop, file_name, s, X, Y):
     file = open(file_name, "w")
 
-    print("# k      s            X          Y         p_x\n"
-          "#                    [m]        [m]       [rad]", file=file)
+    print("# k      s            X          Y\n"
+          "#                    [m]        [m]", file=file)
     for k in range(len(s)):
         elem = lat_prop._lattice[k]
         name = elem.name
         L = elem.get_length()
         phi = math.radians(get_phi(elem))
-        print(f"{k:4d}  {s[k]:9.5f}  {X(s[k]):9.5f}  {Y(s[k]):9.5f}"
-              f"  {p_x[k]:9.5f}", file=file)
+        print(f"{k:4d}  {s[k]:9.5f}  {X(s[k]):9.5f}  {Y(s[k]):9.5f}", file=file)
 
 
-def prt_orbit(s_ref, X_ref, Y_ref, p_x_ref, X, Y):
+def prt_orbit(s_ref, X_ref, Y_ref, X_cs, Y_cs):
     file_name = "orbit.txt"
     file = open(file_name, "w")
 
-    print("# k      s            DX            DY            Dxy"
-          "            Dx\n"
-          "#                     [m]           [m]           [m]"
-          "            [m]", file=file)
+    print("# k      s            DX            DY            Dxy\n"
+          "#                     [m]           [m]           [m]", file=file)
     for k in range(len(s_ref)):
-        Dx, Dy, dxy, dx = compute_orbit(s_ref, X_ref, Y_ref, p_x_ref, X, Y, k)
-        print(f"{k:4d}  {s_ref[k]:9.5f}  {Dx:12.5e}  {Dy:12.5e}   {dxy:12.5e}"
-              f"   {dx:12.5e}", file=file)
+        Dx, Dy, dxy = compute_orbit(
+            s_ref[k], X_ref, Y_ref, X_cs, Y_cs)
+        print(f"{k:4d}  {s_ref[k]:9.5f}  {Dx:12.5e}  {Dy:12.5e}   {dxy:12.5e}",
+              file=file)
 
 
-def prt_orbit_tab(lat_prop, X_ref, Y_ref, p_x_ref, X_cs, Y_cs):
+def prt_orbit_tab(lat_prop, X_ref, Y_ref, X_cs, Y_cs):
 
     def prt_name():
         if type(elem) == ts.Drift:
-            print("Straight", file=file)
+            print("\tStraight", end="", file=file)
         else:
-            print(f"{elem.name:s}", file=file)
+            print(f"\t{elem.name:s}", end="", file=file)
 
     file_name = "orbit_tab.txt"
     file = open(file_name, "w")
 
     Brho = lat_prop._model_state.Energy/c0
-    print("\n  Brho = {Brho:5.3f}")
     s = 0.0
-    print("\n\n\tLattice type\tstandard\n\n", file=file)
-    print("\tName\tStart [m]\tCentre [m]\t End [m]\tLength [m]"
-          "\tBending angle [deg]\tBy @ x=0 [T]\tx [m]\ty [m]", file=file)
+    print("#\n#\n#\tLattice type\tstandard\n#\n#", file=file)
+    print("#\tName\tStart [m]\tCentre [m]\t End [m]\tLength [m]"
+          "#\tBending angle [deg]\tBy @ x=0 [T]\t\t\tx [m]\ty [m]", file=file)
     for k, elem in enumerate(lat_prop._lattice):
-        L = elem.get_length()
-        phi = math.radians(get_phi(elem))
-        if phi == 0.0:
-            B_y = 0.0
-        else:
-            B_y = Brho*L/phi
-        Dx, Dy, dxy, dx = compute_orbit(
-            s, X_ref, Y_ref, p_x_ref, X_cs, Y_cs, k)
-        prt_name()
-        print(f"\t{s:5.3f}\t{s+L/2.0:5.3f}\t{s+L:5.3f}\t{L:5.3f}"
-              f"\t{phi:9.7f}\t{B_y:11.9f}\t\t\t{Dx:8.6f}\t{Dy:8.6f}",
-              file=file)
-        s += s
+        if type(elem) != ts.Marker:
+            L = elem.get_length()
+            phi = math.radians(get_phi(elem))
+            if L == 0e0:
+                B_y = 0e0
+            else:
+                B_y = -Brho*phi/L
+            Dx, Dy, dxy = compute_orbit(s, X_ref, Y_ref, X_cs, Y_cs)
+            prt_name()
+            print(f"\t{s:6.4f}\t{s+L/2.0:6.4f}\t{s+L:6.4f}\t{L:6.4f}"
+                  f"\t{math.degrees(phi):9.7f}\t{B_y:11.9f}"
+                  f"\t\t\t{-Dx:8.6f}\t{-Dy:8.6f}",
+                  file=file)
+            s += L
 
 
 # TPSA max order.
@@ -191,12 +188,12 @@ file_name_ref = os.path.join(home_dir, "max_iv", "max_iv_baseline_2.lat")
 file_name = os.path.join(home_dir, sys.argv[1]+".lat")
 
 lat_ref = get_lat(file_name_ref, "lattice_ref_lat.txt", E_0)
-s_ref, X_ref, Y_ref, p_x_ref = compute_layout(lat_ref)
-prt_layout(lat_ref, "lattice_ref_layout.txt", s_ref, X_ref, Y_ref, p_x_ref)
+s_ref, X_ref, Y_ref = compute_layout(lat_ref)
+prt_layout(lat_ref, "lattice_ref_layout.txt", s_ref, X_ref, Y_ref)
 
 lat_prop = get_lat(file_name, "lattice_lat.txt", E_0)
-s, X_cs, Y_cs, p_x = compute_layout(lat_prop)
-prt_layout(lat_prop, "lattice_layout.txt", s, X_cs, Y_cs, p_x)
+s, X_cs, Y_cs = compute_layout(lat_prop)
+prt_layout(lat_prop, "lattice_layout.txt", s, X_cs, Y_cs)
 
-prt_orbit(s_ref, X_ref, Y_ref, p_x_ref, X_cs, Y_cs)
-prt_orbit_tab(lat_prop, X_ref, Y_ref, p_x_ref, X_cs, Y_cs)
+prt_orbit(s_ref, X_ref, Y_ref, X_cs, Y_cs)
+prt_orbit_tab(lat_prop, X_ref, Y_ref, X_cs, Y_cs)
