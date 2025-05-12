8678aabedd4bfef26842c4dcfd3cf933cf6dbf74
Johan <jbngtssn@gmail.com>
2025-05-12 06:41:19 +0200

    Code refactoring.

--- python/examples/use_cases/unit_cell_scan.py
+++ python/examples/use_cases/unit_cell_scan.py
@@ -11,8 +11,30 @@ logger = logging.getLogger("thor_scsi")
 
 
 import os
+from dataclasses import dataclass
+from typing import ClassVar
 
 from thor_scsi.utils import lattice_properties as lp
+
+import gtpsa
+
+
+@dataclass
+class gtpsa_prop:
+    # GTPSA properties.
+    # Number of variables - phase-space coordinates & 1 for parameter
+    #dependence.
+    nv: ClassVar[int] = 6 + 1
+    # Max order.
+    no: ClassVar[int] = 1
+    # Number of parameters.
+    nv_prm: ClassVar[int] = 0
+    # Parameters max order.
+    no_prm: ClassVar[int] = 0
+    # Index.
+    named_index = gtpsa.IndexMapping(dict(x=0, px=1, y=2, py=3, delta=4, ct=5))
+    # Descriptor
+    desc : ClassVar[gtpsa.desc]
 
 
 def set_phi_rb_bessy_iii(lat_prop, phi_rb):
@@ -28,14 +50,8 @@ def set_phi_rb_bessy_iii(lat_prop, phi_rb):
     lat_prop.set_phi_fam("b1", 4.25-2.0*phi_rb, True)
 
 
-# Number of phase-space coordinates.
-nv = 7
-# Variables max order.
-no = 2
-# Number of parameters.
-nv_prm = 0
-# Parameters max order.
-no_prm = 0
+# TPSA max order.
+gtpsa_prop.no = 2
 
 cod_eps = 1e-15
 E_0     = 2.5e9
@@ -52,27 +68,22 @@ else:
     file_name = os.path.join(home_dir, "2024Mar.lat")
 
 lat_prop = \
-    lp.lattice_properties_class(nv, no, nv_prm, no_prm, file_name, E_0, cod_eps)
+    lp.lattice_properties_class(gtpsa_prop, file_name, E_0, cod_eps)
 
 # Compute Twiss parameters along lattice.
 stable = lat_prop.comp_per_sol()
-print("\nCircumference [m]      = {:7.5f}".format(lat_prop.compute_circ()))
-print("Total bend angle [deg] = {:7.5f}".
-      format(lat_prop.compute_phi(lat_prop._lattice)))
 lat_prop.prt_M()
 if not stable:
     assert False
-Twiss = lat_prop.get_Twiss(len(lat_prop._lattice)-1)
-lat_prop.prt_Twiss_param(Twiss)
+# Twiss = lat_prop.get_Twiss(len(lat_prop._lattice)-1)
+# lat_prop.prt_Twiss_param(Twiss)
 
 # Compute radiation properties.
 stable = lat_prop.compute_radiation()
 lat_prop.prt_rad()
 lat_prop.prt_M()
 
-types = lat_prop.get_types()
-
-lat_prop.prt_Twiss(types)
+lat_prop.prt_Twiss("lat_prop_Twiss.txt")
 
 if False:
     lat_prop.plt_Twiss("twiss.png", not False)
