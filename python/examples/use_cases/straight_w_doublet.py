8fe099655d95b18b180a9e4c9027e5ec5748879b
Johan <jbngtssn@gmail.com>
2025-12-08 09:41:29 +0100

    Code refactoring.

--- python/examples/use_cases/straight_w_doublet.py
+++ python/examples/use_cases/straight_w_doublet.py
@@ -1,5 +1,5 @@
 """Use Case:
-     Optimising a periodic unit cell.
+     Optimising a straight with a matching section.
 """
 
 
@@ -23,31 +23,6 @@ from thor_scsi.utils.output import vec2txt
 
 ind = ind.index_class()
 
-prms_range = {
-    "phi":       [ -5.0,  5.0],
-    "phi_rbend": [ -5.0,  5.0],
-    "b_2":       [-10.0, 10.0],
-    "b_2_bend":  [-10.0, 10.0]
-}
-
-design_vals = {
-    "eps_x_des" : 50e-12,
-    "eta_x_des" : 1.0,
-    "beta_des"  : [10.0, 2.0]
-}
-
-eps_prms = 1e-4
-
-weights = {
-    "eps_x"   : 1e-1,
-    "dphi"    : 1e-1, 
-    "alpha_c" : 1e-1,
-    "eta_x"   : 1e-1,
-    "beta_x"  : 1e-1,
-    "beta_y"  : 1e0,
-    "xi"      : 1e-6
-}
-
 
 class opt_straight_class:
     # Private.
@@ -67,13 +42,16 @@ class opt_straight_class:
         self._phi_bend     = np.zeros(len(self._bend_list))
 
         self._b_2          = np.nan
-        self._phi_sp_0     = np.nan
-        self._phi_sp       = np.nan
+        self._phi_tot_0    = np.nan
+        self._phi_tot      = np.nan
         self._dphi         = np.nan
 
-        self._nu_uc        = np.nan
         self._alpha_c      = np.nan
+        self._eta_entr     = np.nan
+        self._alpha_entr   = np.nan
+        self._beta_entr    = np.nan
         self._eta_centre   = np.nan
+        self._beta_centre  = np.nan
         self._xi           = np.nan
 
         self._constr       = {}
@@ -107,8 +85,8 @@ class opt_straight_class:
         return True
 
     def compute_prop(self) -> bool:
-        self._phi_sp = self._lat_prop.compute_phi_lat()
-        self._dphi = self._phi_sp - self._phi_sp_0
+        self._phi_tot = self._lat_prop.compute_phi_lat()
+        self._dphi = self._phi_tot - self._phi_tot_0
 
         for k, bend in enumerate(self._bend_list):
             self._phi_bend[k] = bend.compute_bend_phi()
@@ -119,28 +97,39 @@ class opt_straight_class:
 
     def compute_constr(self) -> bool:
         self._alpha_c = self._lat_prop._alpha_c
-        self._eta_centre, _, _, _ = self._lat_prop.get_Twiss(self._uc_centre)
-        Twiss = self._lat_prop.get_Twiss(-1)
-        _, _, beta, _ = Twiss
+        self._eta_centre, _, self._beta_centre, _ = \
+            self._lat_prop.get_Twiss(self._uc_centre)
+        self._eta_entr, self._alpha_entr, self._beta_entr, _ = \
+            self._lat_prop.get_Twiss(-1)
 
         self._constr = {
             "eps_x"   : (self._lat_prop._eps[ind.X]
-                             -self._des_val_list["eps_x_des"])**2,
+                         -self._des_val_list["eps_x_des"])**2,
             "dphi"    : self._dphi**2,
             "alpha_c" : 1e0/self._alpha_c[1]**2,
-            "eta_x"   : (self._eta_centre[ind.x]
-                             -self._des_val_list["eta_x_des"])**2,
-            "beta_x"  : (beta[ind.X]
-                             -self._des_val_list["beta_des"][ind.X])**2,
-            "beta_y"  : (beta[ind.Y]
-                             -self._des_val_list["beta_des"][ind.Y])**2,
-            "xi"      : self._xi[ind.X]**2 + self._xi[ind.Y]**2
+            "eta_x_entr"   : (self._eta_entr[ind.x]
+                              -self._des_val_list["eta_x_entr_des"])**2,
+            "beta_x_entr"  : (self._beta_entr[ind.X]
+                              -self._des_val_list["beta_entr_des"]
+                              [ind.X])**2,
+            "beta_y_entr"  : (self._beta_entr[ind.Y]
+                              -self._des_val_list["beta_entr_des"]
+                              [ind.Y])**2,
+            "eta_x_centre"     : (self._eta_centre[ind.x]
+                                  -self._des_val_list["eta_x_centre_des"])**2,
+            "beta_x_centre"    : (self._beta_centre[ind.X]
+                                  -self._des_val_list["beta_centre_des"]
+                                  [ind.X])**2,
+            "beta_y_centre"    : (self._beta_centre[ind.Y]
+                                  -self._des_val_list["beta_centre_des"]
+                                  [ind.Y])**2,
+            "xi"               : self._xi[ind.X]**2 + self._xi[ind.Y]**2
         }
 
     def compute_chi_2(self) -> float:
         self.compute_constr()
 
-        prt_len = 11
+        prt_len = 13
         prt = not False
 
         if self._first:
@@ -173,16 +162,32 @@ class opt_straight_class:
               f"{1e12*self._lat_prop._eps[ind.X]:5.3f} "
               f"({1e12*self._des_val_list["eps_x_des"]:5.3f})")
 
-        print(f"\n    dphi [deg]     = {self._dphi:9.3e}")
+        print(f"\n    dphi [deg]   = {self._dphi:9.3e}")
 
         print("\n    alpha_c (multipoles zeroed)\n"
               f"                   = [{self._alpha_c[1]:9.3e}, "
               f"{self._alpha_c[2]:9.3e}]")
-        print(f"    eta_x          = {self._eta_centre[ind.x]:9.3e}")
+
+        print(f"    eta_x_entr     = {self._eta_entr[ind.x]:9.3e}"
+              f"              "
+              f" ({self._des_val_list["eta_x_entr_des"]:9.3e})")
+        print(f"    beta_entr      = "
+              f"[{self._beta_entr[ind.X]:5.3f}"
+              f", {self._beta_entr[ind.Y]:5.3f}]          "
+              f"([{self._des_val_list["beta_entr_des"][ind.X]:5.3f}, "
+              f"{self._des_val_list["beta_entr_des"][ind.Y]:5.3f}])")
+        print(f"    eta_x_centre   = {self._eta_centre[ind.x]:9.3e}"
+              f"               "
+              f"({self._des_val_list["eta_x_centre_des"]:9.3e})")
+        print(f"    beta_centre    = "
+              f"[{self._beta_centre[ind.X]:5.3f}"
+              f", {self._beta_centre[ind.Y]:5.3f}]         "
+              f"([{self._des_val_list["beta_centre_des"][ind.X]:5.3f}, "
+              f"{self._des_val_list["beta_centre_des"][ind.Y]:5.3f}])")
         print(f"    xi             = [{self._xi[ind.X]:5.3f}, "
               f"{self._xi[ind.Y]:5.3f}]")
 
-        print(f"\n    phi_sp         = {self._phi_sp:8.5f}")
+        print(f"\n    phi_tot        = {self._phi_tot:8.5f}")
         print(f"    C [m]          = "
               f"{self._lat_prop.compute_circ():8.5f}")
 
@@ -241,7 +246,7 @@ class opt_straight_class:
         g_tol    = 1e-7
 
         prm, bounds = self._prm_list.get_prm()
-        self._phi_sp_0 = self._lat_prop.compute_phi_lat()
+        self._phi_tot_0 = self._lat_prop.compute_phi_lat()
         self.f_sp(prm)
 
         # Methods:
@@ -280,68 +285,123 @@ class opt_straight_class:
         print("\n".join(minimum))
 
 
-no = 2
+def init():
+    # TPSA order - i.e., include linear chromaticity.
+    no = 2
 
-cod_eps   = 1e-15
-E_0       = 2.5e9
+    cod_eps   = 1e-15
+    E_0       = 2.5e9
 
-A_max     = np.array([6e-3, 3e-3])
-delta_max = 3e-2
-beta_inj  = np.array([3.0, 3.0])
+    A_max     = np.array([6e-3, 3e-3])
+    delta_max = 3e-2
+    beta_inj  = np.array([3.0, 3.0])
 
-home_dir = os.path.join(
-    os.environ["HOME"], "Nextcloud", "thor_scsi", "JB")
-file_name = os.path.join(home_dir, sys.argv[1]+".lat")
+    home_dir = os.path.join(
+        os.environ["HOME"], "Nextcloud", "thor_scsi", "JB")
+    file_name = os.path.join(home_dir, sys.argv[1]+".lat")
 
-lat_prop = lp.lattice_properties_class(file_name, E_0, cod_eps, no)
+    lat_prop = lp.lattice_properties_class(file_name, E_0, cod_eps, no)
 
-lat_prop.prt_lat("Twiss_lat.txt")
+    lat_prop.prt_lat("Twiss_lat.txt")
 
-str_centre = lat_prop._lattice.find("D5", 0).index
-print("\nunit cell centre {:5s} loc = {:d}".
-      format(lat_prop._lattice[str_centre].name, str_centre))
+    str_centre = lat_prop._lattice.find("D5", 0).index
+    print("\nunit cell centre {:5s} loc = {:d}".
+          format(lat_prop._lattice[str_centre].name, str_centre))
 
-np.set_printoptions(formatter={"float": "{:10.3e}".format})
+    np.set_printoptions(formatter={"float": "{:10.3e}".format})
 
-b_3_list = []
-nld = nld_class.nonlin_dyn_class(lat_prop, A_max, beta_inj, delta_max, b_3_list)
+    b_3_list = []
+    nld = nld_class.nonlin_dyn_class(
+        lat_prop, A_max, beta_inj, delta_max, b_3_list)
 
-# Compute Twiss parameters along lattice.
-if not lat_prop.comp_per_sol():
-    print("\ncomp_per_sol: unstable")
-    exit(1)
-# Compute radiation properties.
-if not lat_prop.compute_radiation():
-    print("\ncompute_radiation: unstable")
-    exit(1)
-lat_prop.prt_lat_param()
-lat_prop.prt_rad()
-lat_prop.prt_M()
-lat_prop.prt_M_rad()
+    # Compute Twiss parameters along lattice.
+    if not lat_prop.comp_per_sol():
+        print("\ncomp_per_sol: unstable")
+        exit(1)
+    # Compute radiation properties.
+    if not lat_prop.compute_radiation():
+        print("\ncompute_radiation: unstable")
+        exit(1)
+    lat_prop.prt_lat_param()
+    lat_prop.prt_rad()
+    lat_prop.prt_M()
+    lat_prop.prt_M_rad()
 
-bend_lists = [["B2_1", "B2_2"]]
-bend_list = []
-bend_list.append(pc.bend_class(lat_prop, bend_lists[0]))
+    return lat_prop, nld, b_3_list, str_centre
 
-prms = [
-    (bend_list[0], "phi_bend", prms_range["phi"]),
-    (bend_list[0], "b_2_bend", prms_range["b_2_bend"]),
-    ("QF2",        "b_2",      prms_range["b_2"]),
-    ("QF3",        "b_2",      prms_range["b_2"]),
-    ("QD1",        "b_2",      prms_range["b_2"])
-]
 
-prm_list = pc.prm_class(lat_prop, prms)
-dprm_list = np.full(len(prms), eps_prms)
+def define_system(lat_prop, str_centre):
+    # Epsilon for numerical evalutation of the Jacobian. 
+    eps_prms = 1e-4
 
-@dataclass
-class prm_class:
-    lattice:     ClassVar[list] = [lat_prop, nld]
-    s_loc:       ClassVar[list] = str_centre
-    design_vals: ClassVar[dict] = design_vals
-    weights:     ClassVar[list] = weights
-    dipoles:     ClassVar[list] = [bend_list]
-    params:      ClassVar[list] = [prm_list, dprm_list]
+    # Parameter ranges.
+    prms_range = {
+        "phi":       [ -5.0,  5.0],
+        "phi_rbend": [ -5.0,  5.0],
+        "b_2":       [-10.0, 10.0],
+        "b_2_bend":  [-10.0, 10.0]
+    }
 
-opt_straight = opt_straight_class(prm_class)
+    # Design values.
+    design_val_list = {
+        "eps_x_des"        : 150e-12,
+        "eta_x_entr_des"   : 3.88154e-02,
+        "beta_entr_des"    : [4.26224, 2.43395],
+        "eta_x_centre_des" : 1.0,
+        "beta_centre_des"  : [10.0, 2.0]
+    }
+
+    # Varying bend radius dipole.
+    bend_lists = [["B2_1", "B2_2"]]
+
+    bend_list = []
+    bend_list.append(pc.bend_class(lat_prop, bend_lists[0]))
+
+    # Parameters.
+    prms = [
+        (bend_list[0], "phi_bend", prms_range["phi"]),
+        (bend_list[0], "b_2_bend", prms_range["b_2_bend"]),
+        ("QF2",        "b_2",      prms_range["b_2"]),
+        ("QF3",        "b_2",      prms_range["b_2"]),
+        ("QD1",        "b_2",      prms_range["b_2"])
+    ]
+
+    prm_list = pc.prm_class(lat_prop, prms)
+    dprm_list = np.full(len(prms), eps_prms)
+
+    # Weights for least-square minimisation.
+    weight_list = {
+        "eps_x"         : 1e20,
+        "dphi"          : 1e0, 
+        "alpha_c"       : 1e-9,
+        "eta_x_entr"    : 1e3,
+        "beta_x_entr"   : 1e-2,
+        "beta_y_entr"   : 1e-2,
+        "eta_x_centre"  : 0.0,
+        "beta_x_centre" : 1e-2,
+        "beta_y_centre" : 1e-2,
+        "xi"            : 1e-2
+    }
+
+    # Package the system.
+    @dataclass
+    class prm_class:
+        lattice:     ClassVar[list] = [lat_prop, nld]
+        s_loc:       ClassVar[list] = str_centre
+        design_vals: ClassVar[dict] = design_val_list
+        weights:     ClassVar[list] = weight_list
+        dipoles:     ClassVar[list] = [bend_list]
+        params:      ClassVar[list] = [prm_list, dprm_list]
+
+    # And generate the object.
+    opt_straight = opt_straight_class(prm_class)
+
+    return opt_straight
+
+
+# Main program.
+lat_prop, nld, b_3_list, str_centre = init()
+opt_straight = define_system(lat_prop, str_centre)
+
+# Optimise.
 opt_straight.opt_straight()
